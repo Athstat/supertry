9e7fa0fb01900967a06cbfdcc9d103e5f0ef2257
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-04-28 21:10:11 +0200

    adds player aggregate stats provider

---
+++ src/contexts/PlayerAggregateStatsContext.tsx
@@ -0,0 +1,11 @@
+import { createContext} from "react";
+import { AthleteSportsActionAggregated } from "../types/sports_actions";
+
+
+type ContextProps = {
+  aggregateStats?: AthleteSportsActionAggregated[],
+  error?: string
+}
+
+export const PlayerAggregateStatsContext = createContext<ContextProps | undefined>(undefined);
+
