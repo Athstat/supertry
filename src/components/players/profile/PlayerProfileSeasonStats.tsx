447cced27c857ad247aba2ebf634b144e02f631e
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-04-28 21:01:53 +0200

    adds some simple stats

--- src/components/players/profile/PlayerProfileSeasonStats.tsx
+++ src/components/players/profile/PlayerProfileSeasonStats.tsx
@@ -4,6 +4,9 @@ import { GroupedStatsGrid } from "../../shared/GroupedStatsGrid"
 import { useAsync } from "../../../hooks/useAsync"
 import { athleteSportActionsService } from "../../../services/athleteSportsActions"
 import { ErrorState } from "../../ui/ErrorState"
+import { StatCard } from "../../shared/StatCard"
+import { AthleteSportsActionAggregated } from "../../../types/sports_actions"
+import { PlayCircleIcon, WatchIcon } from "lucide-react"
 
 type Props = {
     player: RugbyPlayer
@@ -15,16 +18,42 @@ export const PlayerProfileSeasonStats = forwardRef<HTMLDivElement, Props>(({ pla
         return athleteSportActionsService.getByAthlete(player.tracking_id ?? "");
     }, []);
 
-    const {data, error} = useAsync<any>(fetchData);
+    const { data: aggregatedStats, error } = useAsync<AthleteSportsActionAggregated[]>(fetchData);
 
-    if (error) return <ErrorState error="Something went wrong" message={"Failed to load season stats"} />
+    if (error || !aggregatedStats) return <ErrorState error="Something went wrong" message={"Failed to load season stats"} />
+
+    const getStat = (key: string) => {
+        const filteredList = aggregatedStats.filter(stat => {
+            return stat.action.toLowerCase() === key.toLowerCase();
+        });
+
+        if (filteredList.length > 0) {
+            return filteredList[0];
+        }
+
+        return undefined;
+
+    }
+
+    console.log(aggregatedStats.map(s => s.action));
+
+
+    const minutesPlayed = getStat("MinutesPlayed");
+    const starts = getStat("Starts");
 
     return (
         <GroupedStatsGrid title="Season Stats" ref={ref}>
-            {error && <p>Error loading season stats</p>}
-            {data && 
-                <p>Data Loaded successfully</p>
-            }
+            {minutesPlayed && <StatCard
+                label="Minutes Played"
+                value={minutesPlayed.action_count}
+                icon={<WatchIcon className="text-blue-500" size={20} />}
+            />}
+
+            {starts && <StatCard
+                label="Starts"
+                value={starts.action_count}
+                icon={<PlayCircleIcon className="text-blue-500" size={20} />}
+            />}
         </GroupedStatsGrid>
     )
 }
---
+++ src/types/sports_actions.ts
@@ -0,0 +1,7 @@
+export type AthleteSportsActionAggregated = {
+    id: string,
+    athlete_id: string,
+    action: string,
+    action_count: number,
+    season_id: string
+}
