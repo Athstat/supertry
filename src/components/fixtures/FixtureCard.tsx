44ccbafc01d2e3861950d2c6d675f5253f1330c5
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-05-02 17:20:11 +0200

    adds calendar dash

--- src/components/fixtures/FixtureCard.tsx
+++ src/components/fixtures/FixtureCard.tsx
@@ -1,17 +1,18 @@
-
 // import { Shield } from 'lucide-react'
 import { twMerge } from 'tailwind-merge';
 import { IFixture } from '../../types/games'
+import { format } from 'date-fns';
 
 type Props = {
-    fixture: IFixture
+    fixture: IFixture,
+    showCompetition?: boolean
 }
 
-export default function FixtureCard({ fixture }: Props) {
+export default function FixtureCard({ fixture, showCompetition }: Props) {
 
     console.log(fixture);
 
-    const { team_score, opposition_team_id, result, game_status, opposition_score } = fixture;
+    const { team_score, opposition_team_id, kickoff_time, result, round, game_status, opposition_score } = fixture;
 
     const homeTeamWon = (game_status && team_score && opposition_score) ? team_score > opposition_score : false;
     const awayTeamWon = (game_status && team_score && opposition_score) ? team_score < opposition_score : false;
@@ -19,30 +20,62 @@ export default function FixtureCard({ fixture }: Props) {
 
     return (
         <div
-            className="p-4 flex flex-row text-white hover:bg-gray-50 dark:hover:bg-dark-800 transition-colors"
+            className="p-4 flex flex-row text-white hover:bg-slate-50/50 gap-3 dark:hover:bg-dark-800 transition-colors"
         >
 
-            <div className='flex-1 flex text-slate-700 dark:text-slate-100 flex-col items-start justify-start' >
-                <div className='flex flex-row gap-3 items-center' >
-                    <p className={twMerge(
-                        ' ',
-                        homeTeamWon && "font-bold text-slate-600 dark:text-white"
-                    )} >{fixture.home_team}</p>
-                    {fixture.team_score && <p className='' >{fixture.team_score}</p>}
-                </div>
+            <div className='flex-1 flex text-slate-700 dark:text-white flex-col items-end justify-center' >
 
-                <div className='flex flex-row gap-3 items-center' >
-                    <p className={twMerge(
-                        '',
-                        awayTeamWon && "font-bold text-slate-600 dark:text-white"
-                    )} >{fixture.away_team}</p>
-                    {fixture.team_score && <p className='' >{fixture.team_score}</p>}
+                <div className='flex flex-row gap-2 items-center w-full justify-start' >
+                    <div className='flex flex-row gap-2 items-center w-full justify-start' >
+                        <p className={twMerge(
+                            'text-sm w-fit text-left',
+                            awayTeamWon && ""
+                        )} >{fixture.home_team}</p>
+                    </div>
+
+                    {fixture.team_score !== null && fixture.opposition_score !== null ? (
+                        <div className={twMerge(
+                            'flex items-center justify-start px-2 py-1 rounded-full text-slate-700 dark:text-slate-200 text-sm',
+                            homeTeamWon && "font-bold",
+                        )}>
+                            {fixture.team_score}
+                        </div>
+                    ) : null}
+
                 </div>
             </div>
 
 
-            <div className='flex-1' >
+            <div className='flex-1 text-slate-700 dark:text-slate-400 flex flex-col items-center text-center justify-center' >
+                {/* <p className='text-xs' >{fixture.venue}</p> */}
+                {showCompetition && <p className='text-xs' >{fixture.competition_name}{round && ", Round " + round}</p>}
+                {kickoff_time && <p className='text-xs' >{format(kickoff_time, "dd, MMM yyyy")}</p>}
+                {kickoff_time && <p className='text-sm font-semibold' >{format(kickoff_time, "h:mm a")}</p>}
             </div>
+
+
+            <div className='flex-1 flex text-slate-700 dark:text-white flex-col items-end justify-center' >
+
+                <div className='flex flex-row gap-2 items-center w-full justify-start' >
+                    {fixture.team_score !== null && fixture.opposition_score !== null ? (
+                        <div className={twMerge(
+                            'flex items-center justify-start px-2 py-1 rounded-full text-slate-700 dark:text-slate-200 text-sm',
+                            awayTeamWon && "font-bold",
+                        )}>
+                            {fixture.opposition_score}
+                        </div>
+                    ) : null}
+
+                    <div className='flex flex-row gap-2 items-center w-full justify-end' >
+                        <p className={twMerge(
+                            'text-sm w-fit text-wrap text-right',
+                            awayTeamWon && ""
+                        )} >{fixture.away_team}</p>
+                    </div>
+
+                </div>
+            </div>
+
 
         </div>
     )
--- src/components/league/FixturesList.tsx
+++ src/components/league/FixturesList.tsx
@@ -38,14 +38,15 @@ export function FantasyLeagueFixturesList({ league }: FixturesListProps) {
 
   return (
     <div className="bg-white dark:bg-dark-800/40 rounded-xl shadow-sm dark:shadow-dark-sm ">
-      <div className="p-4 border-b border-gray-700 dark:border-dark-600">
+      
+      <div className="p-4 border-b border-gray-200 dark:border-gray-700 dark:border-dark-600">
         <h2 className="text-xl font-semibold flex items-center gap-2 dark:text-gray-100">
           <Calendar size={24} className="text-primary-500" />
           Upcoming Fixtures
         </h2>
       </div>
 
-      <div className="divide-y divide-gray-700 dark:divide-dark-600 px-3">
+      <div className="divide-y divide-gray-300 dark:divide-slate-800/50 px-3">
         {fixtures.map((fixture, index) => (
           <FixtureCard fixture={fixture} key={index} />
         ))}
