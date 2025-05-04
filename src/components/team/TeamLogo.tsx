0e56404e946665ef7338cf7a0ea9ab30e325a8a4
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-05-04 12:23:05 +0200

    adds team logo component

--- src/components/team/TeamLogo.tsx
+++ src/components/team/TeamLogo.tsx
@@ -1,17 +1,39 @@
+import { Shield } from 'lucide-react'
+import { useState } from 'react';
 import { twMerge } from 'tailwind-merge'
 
 type Props = {
-    url?: string
+    teamId?: string,
     alt?: string
 }
 
-export default function TeamLogo({ url, alt }: Props) {
+export default function TeamLogo({ teamId, alt }: Props) {
 
+    const imageUrl = getTeamLogoUrl(teamId ?? "team_id");
+    const [error, setError] = useState(false);
+
+    console.log("teamId", teamId);
+    console.log("team logo url", imageUrl);
+
+    if (error || !teamId) {
+        return (
+            <Shield className="w-14 h-14 text-slate-300 dark:text-slate-600 rounded-md flex items-center justify-center"/>
+        )
+    }
 
     return (
         <div className={twMerge("w-14 h-14")} >
-            {url && <img src={url} alt={alt ?? "team_logo"} />}
-            {!url && <div className="w-full h-full bg-slate-200 dark:bg-slate-700 rounded-md flex items-center justify-center"></div>}
+            <img 
+                src={imageUrl}
+                alt={alt ?? "team_logo"}
+                onError={(e) => setError(true)} 
+            />
         </div>
     )
+}
+
+
+function getTeamLogoUrl(teamId: string) {
+    const baseUrl = `https://athstat-landing-assets-migrated.s3.amazonaws.com/team_logos`;
+    return `${baseUrl}/${teamId}.png`;
 }
