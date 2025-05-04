784c4af6a2bc9b6e1fb2a8dc0c538e4a2998397e
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-05-04 12:23:28 +0200

    adds fixture list modal

--- src/components/league/FixturesList.tsx
+++ src/components/league/FixturesList.tsx
@@ -5,6 +5,7 @@ import useSWR from 'swr';
 import { LoadingSpinner } from "../team-creation/player-list/LoadingSpinner";
 import { ErrorState } from "../ui/ErrorState";
 import FixtureCard from "../fixtures/FixtureCard";
+import { IFixture } from "../../types/games";
 
 interface FixturesListProps {
   league: IFantasyLeague
@@ -14,31 +15,21 @@ export function FantasyLeagueFixturesList({ league }: FixturesListProps) {
 
   const competitionId = league.official_league_id;
 
-  const { data: allFixtures, error, isLoading } = useSWR(competitionId,gamesService.getGamesByCompetitionId);
+  const { data: allFixtures, error, isLoading } = useSWR(competitionId, gamesService.getGamesByCompetitionId);
 
   if (isLoading) return <LoadingSpinner />
 
   if (error) return <ErrorState message={"Error fetching matches"} />
-  console.log("Error fetching matches", error);
 
   if (!allFixtures) return <div>
     <p>There are no fixtures available</p>
   </div>
 
-  console.log("League Start", league.start_round, league.end_round);
-
-  const fixtures = allFixtures.filter(f => {
-
-    const start_round = league.start_round;
-    const end_round = league.end_round;
-    
-    return start_round && end_round ? f.round >= start_round && f.round <= end_round : true;
-  }).sort((a, b) => new Date(b.kickoff_time ?? new Date()).valueOf() - new Date(a.kickoff_time ?? new Date()).valueOf() )
-
+  const fixtures = filterMatchesForRound(allFixtures, league);
 
   return (
     <div className="bg-white dark:bg-dark-800/40 rounded-xl shadow-sm dark:shadow-dark-sm ">
-      
+
       <div className="p-4 border-b border-gray-200 dark:border-gray-700 dark:border-dark-600">
         <h2 className="text-xl font-semibold flex items-center gap-2 dark:text-gray-100">
           <Calendar size={24} className="text-primary-500" />
@@ -53,4 +44,16 @@ export function FantasyLeagueFixturesList({ league }: FixturesListProps) {
       </div>
     </div>
   );
+}
+
+const filterMatchesForRound = (fixtures: IFixture[], league: IFantasyLeague) => {
+  
+  return fixtures.filter(f => {
+
+    const start_round = league.start_round;
+    const end_round = league.end_round;
+
+    return start_round && end_round ? f.round >= start_round && f.round <= end_round : true;
+  }).sort((a, b) => new Date(b.kickoff_time ?? new Date()).valueOf() - new Date(a.kickoff_time ?? new Date()).valueOf())
+
 }
---
+++ src/components/shared/DialogModal.tsx
@@ -0,0 +1,44 @@
+import { X } from "lucide-react";
+import { twMerge } from "tailwind-merge";
+
+type Props = {
+    children?: React.ReactNode,
+    onClose?: () => void,
+    title?: string,
+    open?: boolean,
+    className?: string
+}
+
+export default function DialogModal({ children, onClose, title, open, className }: Props) {
+
+    const handleOnClose = () => {
+        if (onClose) {
+            onClose();
+        }
+    }
+
+    if (!open) return null;
+
+    return (
+        <div className="fixed inset-0 bg-black/50 backdrop-blur-sm flex items-center justify-center p-4 z-[70] animate-fade-scale-up">
+            <div className="bg-white dark:bg-dark-800 rounded-xl w-full max-w-lg overflow-hidden">
+                <div className="p-4">
+                    <div className="flex items-center justify-between mb-4">
+                        <h2 className="text-xl font-bold dark:text-gray-100">{title}</h2>
+                        <button
+                            onClick={handleOnClose}
+                            className="p-2 hover:bg-gray-100 dark:hover:bg-dark-700 rounded-full text-gray-600 dark:text-gray-400"
+                        >
+                            <X size={20} />
+                        </button>
+                    </div>
+
+                    {/* Tabs */}
+                    <div className={twMerge("mt-4", className)}>
+                        {children}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
