f8ffb92f140ae47647802677ef44d7f923f05a6e
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-05-05 22:48:44 +0200

    adds user rankings

---
+++ src/services/userRankingsService.ts
@@ -0,0 +1,41 @@
+import { UserRanking } from "../types/userRanking";
+import { getAuthHeader, getUri } from "../utils/backendUtils"
+
+export const userRankingsService = {
+
+    getUserRankings: async (start?: number, end?: number) => {
+
+        try {
+            const queryParams = start && end ? `?start=${start}&end=${end}` : "";
+            const url = getUri(`/api/v1/rankings/users${queryParams}`);
+
+            const res = await fetch(url, {
+                headers: getAuthHeader()
+            });
+
+            const json = await res.json();
+
+            return json as UserRanking[];
+        } catch (error) {
+            console.log("Error fetching user rankings", error);
+            return [];
+        }
+    },
+
+    getUserRankingByUserId: async (userId: string) => {
+
+        try {
+            const uri = getUri(`/api/v1/rankings/users/${userId}`);
+            const res = await fetch(uri, {
+                headers: getAuthHeader()
+            });
+
+            const json = await res.json();
+            return json as UserRanking;
+
+        } catch (error) {
+            console.log("Error fetching user rankings ", error);
+            return undefined;
+        }
+    }
+}
