9273d96d7d318979c653baf42ea956258af1f0ac
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-05-06 16:12:35 +0200

    removes unused imports

--- src/screens/JoinLeagueScreen.tsx
+++ src/screens/JoinLeagueScreen.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import { Users, Trophy, ChevronLeft, Loader } from "lucide-react";
+import { useState, useEffect } from "react";
+import { Trophy, Loader } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import { leagueService } from "../services/leagueService";
 import { IFantasyLeague } from "../types/fantasyLeague";
