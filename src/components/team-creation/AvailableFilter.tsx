866bf44a49eb0cc6a5caeeaea711104aa4d966ad
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-05-08 06:40:07 +0200

    adds show for roster players only

--- src/components/team-creation/AvailableFilter.tsx
+++ src/components/team-creation/AvailableFilter.tsx
@@ -9,7 +9,7 @@ export default function AvailableFilter({filterAvailable, toogle} : Props) {
   return (
     <div className="dark:text-slate-300 mt-2 w-full items-center justify-start flex flex-row gap-2 px-2" >
         <input className="h-5 w-5" checked={filterAvailable} onClick={toogle} type="checkbox" />
-        <div className="p-0 m-0  h-fit" >Show Available Players Only</div>
+        <div className="p-0 m-0  h-fit" >Show Roster Players Only</div>
     </div>
   )
 }
