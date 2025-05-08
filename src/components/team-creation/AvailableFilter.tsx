bbb5d59674acc6ce1b6b6c5be8bebece0b4c434e
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-05-08 16:38:25 +0200

    adds padding to checkbox and makes it a lable

--- src/components/team-creation/AvailableFilter.tsx
+++ src/components/team-creation/AvailableFilter.tsx
@@ -6,7 +6,7 @@ type Props = {
 export default function AvailableFilter({filterAvailable, toogle} : Props) {
 
   return (
-    <div className="dark:text-slate-300 p-2 w-full items-center justify-start flex flex-row gap-2 px-2" >
+    <div className="dark:text-slate-300 py-2 px-6 w-full items-center justify-start flex flex-row gap-2" >
         <input id="checkbox_input" className="h-5 w-5" checked={filterAvailable} onClick={toogle} type="checkbox" />
         <label htmlFor="checkbox_input" className="p-0 m-0  h-fit" >Show Confirmed Players Only</label>
     </div>
