8532d289d7dde082c957769226ac19caf4596827
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-05-08 23:26:18 +0200

    adds indicator to player name

--- src/components/player/profile-modal-components/PlayerInfo.tsx
+++ src/components/player/profile-modal-components/PlayerInfo.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { formatPosition } from '../../../utils/athleteUtils';
+import FormIndicator from '../../shared/FormIndicator';
 
 interface PlayerInfoProps {
   player: any;
@@ -10,8 +11,14 @@ export const PlayerInfo: React.FC<PlayerInfoProps> = ({ player }) => {
   return (
     <>
       <div className="px-6 py-4 sticky top-0 bg-white dark:bg-dark-800 border-b border-gray-200 dark:border-gray-700">
-        <h2 className="text-2xl font-bold text-gray-800 dark:text-white">{player.player_name}</h2>
-        <p className="text-gray-600 dark:text-gray-400">{formatPosition(player.position)}</p>
+        
+        <div className='flex flex-row items-center gap-2' >
+          <h2 className="text-2xl font-bold text-gray-800 dark:text-white">{player.player_name}</h2>
+          {player.form && <FormIndicator className='w-5 h-5' form={player.form} />}
+        </div>
+
+        <p className="text-gray-600 dark:text-gray-400">{formatPosition(player.position ?? "")}</p>
+
       </div>
     </>
   );
