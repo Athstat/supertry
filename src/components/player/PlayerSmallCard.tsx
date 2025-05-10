3fd73ee8145555983dba65093662781f38dbbe2a
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-05-10 23:11:44 +0200

    fixes first nam repeat issue

--- src/components/player/PlayerSmallCard.tsx
+++ src/components/player/PlayerSmallCard.tsx
@@ -37,7 +37,7 @@ export default function PlayerBoxScoreSmallCard({children, boxScore: bs, fixture
                 <div className="flex flex-col gap-0" >
 
                     <div>
-                        <p className="font-medium" >{bs.athlete_first_name} {bs.athlete_first_name} {bs.athlete_position ? `· ${fixPosition(bs.athlete_position)}` : ''    }</p>
+                        <p className="font-medium" >{bs.athlete_first_name} {bs.athlete_last_name} {bs.athlete_position ? `· ${fixPosition(bs.athlete_position)}` : ''    }</p>
                     </div>
                     
                     {children}
