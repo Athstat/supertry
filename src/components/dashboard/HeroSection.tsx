7c6a2858c7d2d475f0bee724ff9de66c2ea262cc
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-05-14 22:29:38 +0200

    adds code to hero section to redirect user to correct page

--- src/components/dashboard/HeroSection.tsx
+++ src/components/dashboard/HeroSection.tsx
@@ -1,9 +1,12 @@
-import { ChevronRight } from "lucide-react";
+import { ChevronRight, Trophy } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import { IFantasyLeague } from "../../types/fantasyLeague";
 import { activeLeaguesFilter, isLeagueOnTheClock } from "../../utils/leaguesUtils";
 import { useCountdown } from "../../hooks/useCountdown";
 import { epochDiff } from "../../utils/dateUtils";
+import { useFantasyLeague, useUserFantasyTeam } from "../league/useFantasyLeague";
+import { twMerge } from "tailwind-merge";
+import { useRouter } from "../../hooks/useRoter";
 
 type Props = {
   availableLeagues: IFantasyLeague[],
@@ -14,16 +17,13 @@ export function HeroSection({ availableLeagues, onViewLeague }: Props) {
 
   const navigate = useNavigate();
   const activeLeagues = activeLeaguesFilter(availableLeagues);
-  
+
   let leagueOnTheClock: IFantasyLeague | undefined;
   const twoDays = 1000 * 60 * 60 * 24 * 2;
 
   const leaguesOnTheClock = activeLeagues.filter((l) => {
     return (isLeagueOnTheClock(l, twoDays));
   });
-
-
-  console.log(leaguesOnTheClock);
 
   if (leaguesOnTheClock.length > 0) {
     leagueOnTheClock = leaguesOnTheClock[0];
@@ -65,6 +65,17 @@ function JoinDeadlineCountdown({ league, onViewLeague }: JoinDeadlineCountdownPr
 
   const { days, hours, seconds, minutes } = useCountdown(diff);
 
+  const {userTeam, isLoading, rankedUserTeam} = useUserFantasyTeam(league);
+  const {navigateToMyTeam: navigateToMyTeam} = useRouter();
+
+  const handleCallToAction = () => {
+    if (userTeam) {
+      navigateToMyTeam(userTeam, rankedUserTeam);
+    } else {
+      onViewLeague(league);
+    }
+  }
+
   const timeBlocks = [
     { value: days, label: "Days" },
     { value: hours, label: "Hours" },
@@ -75,7 +86,8 @@ function JoinDeadlineCountdown({ league, onViewLeague }: JoinDeadlineCountdownPr
   return (
     <div className="flex flex-col p-4 gap-4 sm:gap-6">
       <div className="space-y-2 sm:space-y-4">
-        <h1 className="text-xl sm:text-2xl md:text-3xl font-bold tracking-tight">
+        <h1 className="text-xl flex flex-row items-center gap-1 sm:text-2xl md:text-3xl font-bold tracking-tight">
+          <Trophy />
           {league.title}
         </h1>
         <p className="text-primary-100 text-sm sm:text-base md:text-lg">
@@ -85,7 +97,7 @@ function JoinDeadlineCountdown({ league, onViewLeague }: JoinDeadlineCountdownPr
 
       <div className="grid grid-cols-4 sm:flex sm:flex-row gap-2 sm:gap-4 items-center justify-start">
         {timeBlocks.map((block) => (
-          <div 
+          <div
             key={block.label}
             className="p-2 sm:p-3 md:min-w-[80px] items-center justify-center flex flex-col rounded-xl bg-white/10 backdrop-blur-sm border border-white/10 shadow-xl"
           >
@@ -100,11 +112,19 @@ function JoinDeadlineCountdown({ league, onViewLeague }: JoinDeadlineCountdownPr
       </div>
 
       <div className="flex items-center gap-4">
-        <button 
-          onClick={() => onViewLeague(league)}
-          className="w-full sm:w-auto bg-white text-primary-600 px-4 sm:px-6 md:px-8 py-2 sm:py-3 rounded-lg font-semibold hover:bg-opacity-90 transition-all flex items-center justify-center sm:justify-start gap-2 shadow-lg"
+        <button
+          onClick={handleCallToAction}
+          className={twMerge(
+            "w-full sm:w-auto bg-white text-primary-600 px-4 sm:px-6 md:px-8 py-2 sm:py-3 rounded-lg font-semibold hover:bg-opacity-90 transition-all flex items-center justify-center sm:justify-start gap-2 shadow-lg",
+            isLoading && "animate-pulse"
+          )}
         >
-          Pick Your Team <ChevronRight size={20} />
+          {!isLoading && (
+            <>
+              <p>{userTeam ? "View You Team" : "Pick Your Team"}</p>
+              <ChevronRight size={20} />
+            </>
+          )}
         </button>
       </div>
     </div>
