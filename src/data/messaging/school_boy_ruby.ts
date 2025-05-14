362320ccaab174e5f0f5ed509bd9845098b303cc
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-05-14 08:58:26 +0200

    changes sbr chat

--- src/data/messaging/school_boy_ruby.ts
+++ src/data/messaging/school_boy_ruby.ts
@@ -1,2 +1,2 @@
-export const SCHOOL_BOY_RUGBY_CHANNEL_URL = "sendbird_open_channel_44078_dc1f6b9e350fe74e23dbabb86ea608a72ee2598e";
+export const SCHOOL_BOY_RUGBY_CHANNEL_URL = "sendbird_group_channel_335003844_6568f2b99ee409d8be87c8a2c10c232e43d8e9dd";
 export const SCHOOL_BOY_RUGBY_CHANNEL_NAME = "School Boy Rugby";
