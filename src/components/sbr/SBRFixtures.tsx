cf53ad28a3fef1492a7c580d3e3f35857ca81937
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-05-15 16:33:04 +0200

    adds sbr chat screen

---
+++ src/components/sbr/SBRFixtures.tsx
@@ -0,0 +1,9 @@
+export default function SBRFixtures() {
+  return (
+    <div>
+        <div>
+            <h1>Round 1</h1>
+        </div>
+    </div>
+  )
+}
--- src/screens/PageView.tsx
+++ src/screens/PageView.tsx
@@ -10,7 +10,7 @@ export default function PageView({ children, className }: Props) {
 
 
     return (
-        <div className="w-full  h-full flex flex-col items-center justify-start" >
+        <div className="w-full dark:text-white h-full flex flex-col items-center justify-start" >
             <div className={twMerge(
                 "w-full lg:w-[60%]",
                 className
