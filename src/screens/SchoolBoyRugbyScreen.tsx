85e05edc56013a1ca6cf4a6588c1e8192893ee85
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-05-15 21:17:43 +0200

    adds title

--- src/screens/SchoolBoyRugbyScreen.tsx
+++ src/screens/SchoolBoyRugbyScreen.tsx
@@ -1,16 +1,13 @@
-import { useNavigate } from "react-router-dom";
 import PageView from "./PageView";
-import { MessageCircle } from "lucide-react";
 import SBRFixtures from "../components/sbr/SBRFixtures";
-import SbrContextSwitcher from "../components/sbr/SbrContextSwitcher";
 
 export default function SchoolBoyRugbyScreen() {
 
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
-  const handleViewChat = () => {
-    navigate("/sbr/chat");
-  }
+  // const handleViewChat = () => {
+  //   navigate("/sbr/chat");
+  // }
 
   return (
     <PageView className="dark:text-white p-5 flex flex-col gap-3" >
@@ -18,7 +15,7 @@ export default function SchoolBoyRugbyScreen() {
       <div className="flex flex-row items-center justify-between" >
 
         <div className="flex flex-row items-center gap-2" >
-          <h1 className="text-2xl font-bold lg:text-3xl" >SBR</h1>
+          <h1 className="text-2xl font-bold lg:text-3xl" >School Boy Rugby</h1>
           {/* <SbrContextSwitcher /> */}
         </div>
 
