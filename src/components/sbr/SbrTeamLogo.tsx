3a83676c18eb308de32ca5db6606a0ec52d2d32c
Tadiwanashe Shangwa <80280920+Tadiwr@users.noreply.github.com>
2025-05-15 21:11:24 +0200

    Patch  sbr logos (#42)
    * adds sbr logos
    
    * adds logos to voting modal
    
    * adds remaining logos

--- src/components/sbr/SbrTeamLogo.tsx
+++ src/components/sbr/SbrTeamLogo.tsx
@@ -8,6 +8,20 @@ type Props = {
 
 export default function SbrTeamLogo({ teamName, className }: Props) {
 
+    const logoUrl = teamLogoMapper(teamName || "");
+
+    if (logoUrl) {
+        return (
+            <img
+                src={logoUrl}
+                alt={teamName}
+                className={twMerge(
+                    "w-8 h-8 object-contain",
+                    className
+                )}
+            />
+        )
+    }
     
 
     return (
@@ -18,4 +32,48 @@ export default function SbrTeamLogo({ teamName, className }: Props) {
             )} />
         </>
     )
+}
+
+
+function teamLogoMapper(teamName: string): string | undefined {
+  switch (teamName) {
+    case 'CBC':
+      return '/sbr_logos/christian-brothers-college-bulawayo.png.png';
+    case 'Falcon College':
+      return '/sbr_logos/falcon-college.png.png';
+    case 'Gateway High School':
+      return '/sbr_logos/gateway.png.png';
+    case 'Hellenic Academy':
+      return '/sbr_logos/hellenic.png.png';
+    case 'Heritage':
+    case 'Hattie College':
+      return '/sbr_logos/heritage.png.png';
+    case 'Kyle College':
+      return '/sbr_logos/kyle-college-vikings.png.png';
+    case 'Lomagundi College':
+      return '/sbr_logos/lomagundi.png.png';
+    case 'Peterhouse Boys':
+      return '/sbr_logos/peterhouse.png.png';
+    case 'Prince Edward School':
+      return '/sbr_logos/prince-edwards.png.png';
+    case 'St George’s College':
+      return '/sbr_logos/st-georges.png.png';
+    case "St John's College":
+      return '/sbr_logos/st-johns-college.png.png';
+    case 'Watershed College':
+      return '/sbr_logos/watershed.png.png';
+    case 'Nattie College':
+        return '/sbr_logos/nattie_college.webp';
+    case 'Rydings College':
+        return '/sbr_logos/rydings.webp';
+
+    case 'Hillcrest College':
+        return '/sbr_logos/hilcrest.webp';
+    case 'Wise Owl Marondera':
+        return 'public/sbr_logos/wise_owl.webp';
+    case 'Churchill Boys High':
+        return 'public/sbr_logos/church_hill.webp';
+    default:
+      return undefined;
+  }
 }
--- src/components/shared/DialogModal.tsx
+++ src/components/shared/DialogModal.tsx
@@ -21,10 +21,10 @@ export default function DialogModal({ children, onClose, title, open, className 
 
     return (
         <div className="fixed inset-0 bg-black bg-opacity-50 z-[200] flex items-center justify-center ">
-            <div className="bg-black w-[95%] md:w-[80%] lg:w-1/3 max-h-[90vh] my-4 " >
+            <div className="bg-black w-[95%] md:w-[80%] lg:w-1/3 max-h-[90vh] my-4 rounded-lg" >
                 <div className="bg-white dark:bg-gray-900/50 w-full max-h-[90vh]  rounded-lg p-6 shadow-xl overflow-y-auto flex flex-col">
                     <div className="flex items-center justify-between mb-4">
-                        <h2 className="text-xl font-bold dark:text-gray-100">{title}</h2>
+                        <h2 className="text-xl font-bold truncate dark:text-gray-100">{title}</h2>
                         <button
                             onClick={handleOnClose}
                             className="p-2 hover:bg-gray-100 dark:hover:bg-dark-700 rounded-full text-gray-600 dark:text-gray-400"
