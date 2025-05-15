46693e23c32436254928c0b2cb3fee3d58a89c76
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-05-15 20:54:56 +0200

    adds sbr logos

--- src/components/sbr/SbrTeamLogo.tsx
+++ src/components/sbr/SbrTeamLogo.tsx
@@ -8,6 +8,20 @@ type Props = {
 
 export default function SbrTeamLogo({ teamName, className }: Props) {
 
+    const logoUrl = teamLogoMapper(teamName || "");
+
+    if (logoUrl) {
+        return (
+            <img
+                src={logoUrl}
+                alt={teamName}
+                className={twMerge(
+                    "w-8 h-8 object-contain",
+                    className
+                )}
+            />
+        )
+    }
     
 
     return (
@@ -18,4 +32,37 @@ export default function SbrTeamLogo({ teamName, className }: Props) {
             )} />
         </>
     )
+}
+
+
+function teamLogoMapper(teamName: string): string | undefined {
+  switch (teamName) {
+    case 'CBC':
+      return '/sbr_logos/christian-brothers-college-bulawayo.png.png';
+    case 'Falcon College':
+      return '/sbr_logos/falcon-college.png.png';
+    case 'Gateway High School':
+      return '/sbr_logos/gateway.png.png';
+    case 'Hellenic Academy':
+      return '/sbr_logos/hellenic.png.png';
+    case 'Heritage':
+    case 'Hattie College':
+      return '/sbr_logos/heritage.png.png';
+    case 'Kyle College':
+      return '/sbr_logos/kyle-college-vikings.png.png';
+    case 'Lomagundi College':
+      return '/sbr_logos/lomagundi.png.png';
+    case 'Peterhouse Boys':
+      return '/sbr_logos/peterhouse.png.png';
+    case 'Prince Edward School':
+      return '/sbr_logos/prince-edwards.png.png';
+    case 'St George’s College':
+      return '/sbr_logos/st-georges.png.png';
+    case "St John's College":
+      return '/sbr_logos/st-johns-college.png.png';
+    case 'Watershed College':
+      return '/sbr_logos/watershed.png.png';
+    default:
+      return undefined;
+  }
 }
