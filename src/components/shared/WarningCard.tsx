d845b9f9e465f798a90e1bfc9eea2c3c5e37e188
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-05-20 21:38:04 +0200

    adds class name to warning card

--- src/components/shared/WarningCard.tsx
+++ src/components/shared/WarningCard.tsx
@@ -1,12 +1,14 @@
 import { ReactNode } from "react"
+import { twMerge } from "tailwind-merge"
 
 type Props = {
-    children?: ReactNode
+    children?: ReactNode,
+    className?: string
 }
 
-export default function WarningCard({children}: Props) {
+export default function WarningCard({children, className}: Props) {
   return (
-    <div className='bg-yellow-100  dark:bg-yellow-900/40 border border-yellow-300 dark:border-yellow-900 mt-2 rounded-xl px-3 py-1 text-yellow-700 dark:text-yellow-600 flex flex-row items-center gap-1' >
+    <div className={twMerge('bg-yellow-100  dark:bg-yellow-900/40 border border-yellow-300 dark:border-yellow-900 mt-2 rounded-xl px-3 py-1 text-yellow-700 dark:text-yellow-600 flex flex-row items-center gap-1', className)} >
         {children}
     </div>
   )
