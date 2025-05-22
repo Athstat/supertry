af00a8bfb11d58509bb016ba1c16938e33a59466
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-05-22 12:00:21 +0200

    adds icon for sbr page

--- src/components/sbr/SbrScreenHeader.tsx
+++ src/components/sbr/SbrScreenHeader.tsx
@@ -1,4 +1,4 @@
-import { MessageCircle } from 'lucide-react'
+import { Shield, Swords } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
 
 export default function SbrScreenHeader() {
@@ -12,14 +12,10 @@ export default function SbrScreenHeader() {
         <div className="flex flex-row items-center justify-between" >
 
             <div className="flex flex-row items-center gap-2" >
+                <Shield />
+                {/* <Swords /> */}
                 <h1 className="text-xl font-bold lg:text-2xl" >School Boy Rugby</h1>
             </div>
-            <button onClick={handleViewChat} className="h-fit hover:bg-slate-200 dark:hover:bg-slate-800/40 px-3 py-1 rounded-xl w-fit gap-1 flex flex-row items-center justify-end" >
-                <MessageCircle
-                    className="hover:text-primary-600 w-5 h-5 cursor-pointer"
-                />
-                Chat
-            </button>
         </div>
     )
 }
