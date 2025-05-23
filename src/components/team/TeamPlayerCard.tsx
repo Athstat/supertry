32234b79557f3e49cb6dcfb1324e188514c81128
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-05-23 16:04:23 +0200

    adds opacity to cards

--- src/components/team/TeamPlayerCard.tsx
+++ src/components/team/TeamPlayerCard.tsx
@@ -93,7 +93,7 @@ export function AthleteFantasyCard({ player, onClick, className }: Props) {
           className
         )}
       >
-        {!isAvailable && <div className="top-0 left-0 absolute w-full h-full bg-black/30 z-10" ></div>}
+        {!isAvailable && <div className="top-0 left-0 absolute w-full h-full bg-black/50 z-10" ></div>}
         {/* Team Logo */}
         <div className="absolute top-2 right-2 z-10">
           <TeamLogo className="w-8 h-8" url={playerInfo.team_logo} />
