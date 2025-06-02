9ffdf9cd3f0d3c83c7d2e122957dbcf5d43c0fe3
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-06-02 13:47:55 +0200

    adds sbr voting on fixture page for sbr

--- src/components/sbr/SbrFixtureCard.tsx
+++ src/components/sbr/SbrFixtureCard.tsx
@@ -4,11 +4,11 @@ import SbrTeamLogo from "./fixtures/SbrTeamLogo";
 import { twMerge } from "tailwind-merge";
 import { getSbrVotingSummary, sbrFxitureSummary } from "../../utils/sbrUtils";
 import { useSbrFixtureVotes } from "../../hooks/useFxitureVotes";
-import { Check, User, X } from "lucide-react";
 import { Fragment } from "react/jsx-runtime";
 import { useState } from "react";
 import { mutate } from "swr";
 import { sbrService } from "../../services/sbrService";
+import { VotingOptionBar } from "../shared/bars/VotingOptionBar";
 
 type Props = {
     fixture: ISbrFixture,
@@ -20,18 +20,17 @@ type Props = {
 
 export default function SbrFixtureCard({ fixture, showLogos, showCompetition, className }: Props) {
 
-    const { homeVotes, awayVotes, userVote } = useSbrFixtureVotes(fixture);
+    const { homeVotes, awayVotes, userVote, isLoading } = useSbrFixtureVotes(fixture);
     const { home_score, away_score, home_team, away_team } = fixture;
     const hasScores = home_score !== null && away_score !== null;
 
     const navigate = useNavigate();
 
-    // const handleClick = () => {
-    //     navigate(`/sbr/fixtures/${fixture.fixture_id}`);
-    // }
+    const handleClick = () => {
+        navigate(`/sbr/fixtures/${fixture.fixture_id}`);
+    }
 
     const [isVoting, setIsVoting] = useState(false);
-    const [isVoitngHome, setIsVotingHome] = useState(false);
 
     const onVote = async (side: "home_team" | "away_team") => {
         // if user has voted before use put request
@@ -42,8 +41,6 @@ export default function SbrFixtureCard({ fixture, showLogos, showCompetition, cl
         }
 
         setIsVoting(true);
-
-        setIsVotingHome(side === "home_team")
 
         if (!userVote) {
             await sbrService.postSbrFixtureVote(
@@ -81,7 +78,7 @@ export default function SbrFixtureCard({ fixture, showLogos, showCompetition, cl
         <div
             // onClick={handleClick}
             className={twMerge(
-                "   dark:bg-slate-800/40 bg-white rounded-xl border dark:border-slate-800/60 p-4",
+                "   dark:bg-slate-800/40 cursor-pointer bg-white rounded-xl border dark:border-slate-800/60 p-4",
                 className
             )}
         >
@@ -91,6 +88,7 @@ export default function SbrFixtureCard({ fixture, showLogos, showCompetition, cl
             </div>
 
             <div
+                onClick={handleClick}
                 className="flex flex-row"
             >
                 {/* Home Team */}
@@ -117,7 +115,13 @@ export default function SbrFixtureCard({ fixture, showLogos, showCompetition, cl
                 </div>
             </div>
 
-            <div
+            {isLoading && (
+                <div className="w-full h-20 bg-slate-200 dark:bg-slate-800/40 animate-pulse rounded-xl" >
+
+                </div>
+            )}
+
+            {!isLoading && <div
                 className={twMerge(
                     "flex mt-6 flex-col w-full gap-3 items-center justify-center",
                     isVoting && "animate-pulse opacity-60 cursor-progress"
@@ -125,15 +129,15 @@ export default function SbrFixtureCard({ fixture, showLogos, showCompetition, cl
             >
                 {/* Home Team Voting Station */}
 
-               {!hasUserVoted && <div className="flex flex-col w-2/3 gap-2 items-center text-sm justify-center text-slate-700 dark:text-slate-400" >
+               {!hasUserVoted && !hasKickedOff && <div className="flex flex-col w-full gap-2 items-center text-sm justify-center text-slate-700 dark:text-slate-400" >
                     <p>Who you got winning?</p>
 
                     <button onClick={handleClickHomeVoteBar} className="border dark:border-slate-700 w-full px-2 rounded-xl bg-slate-200 py-2 hover:bg-slate-300 dark:bg-slate-800 dark:hover:bg-slate-700" >
-                        {home_team} Win
+                        {home_team}
                     </button>
 
                     <button onClick={handleClickAwayVoteBar} className="border dark:border-slate-700 w-full px-2 rounded-xl bg-slate-200 py-2 hover:bg-slate-300 dark:bg-slate-800 dark:hover:bg-slate-700" >
-                        {away_team} Win
+                        {away_team}
                     </button>
 
                 </div>}
@@ -163,84 +167,8 @@ export default function SbrFixtureCard({ fixture, showLogos, showCompetition, cl
                     />
                 </Fragment>}
 
-            </div>
+            </div>}
         </div>
     )
 }
 
-type VotingOptionBarProps = {
-    isGreen?: boolean,
-    isRed?: boolean,
-    title?: string,
-    voteCount?: number,
-    hasUserVoted?: boolean,
-    votePercentage?: number,
-    onClick?: () => void,
-    className?: string,
-    disable?: boolean
-}
-
-function VotingOptionBar({ isGreen, isRed, title, voteCount = 0, disable, hasUserVoted, votePercentage, onClick, className }: VotingOptionBarProps) {
-
-    const handleClick = () => {
-        if (onClick && !disable) {
-            onClick();
-        }
-    }
-
-    return (
-        <div
-            className={twMerge(
-                "w-full hover:bg-slate-100 dark:hover:bg-slate-800/80 px-2 py-2 rounded-lg flex flex-col items-center justify-center gap-1 cursor-pointer",
-                className
-            )}
-            onClick={handleClick}
-        >
-
-            <div className="w-full flex text-slate-500 dark:text-slate-400 flex-row items-center justify-between" >
-                <p className="text-xs" >{title}</p>
-                <p className="text-xs " >Vote{voteCount > 0 ? "s" : ""} {voteCount}</p>
-            </div>
-
-            <div className="flex flex-row gap-1 rounded-xl w-[100%]">
-                {/* Voting Circle */}
-                <div className={twMerge(
-                    "w-5 h-5 border rounded-xl border-slate-400 dark:border-slate-500",
-                    (hasUserVoted) && "border-none",
-                    !hasUserVoted && ""
-                )} >
-
-                    {hasUserVoted && (
-                        <div className={twMerge(
-                            "w-full h-full flex flex-col items-center justify-center bg-gradient-to-r text-white bg-blue-500 rounded-xl ",
-                            isGreen && "bg-green-400",
-                            isRed && "bg-red-400"
-                        )} >
-                            <User className="w-3 h-3" />
-                        </div>
-                    )}
-
-                </div>
-
-                <div
-                    style={{
-                        width: `${votePercentage}%`
-                    }}
-                    className={twMerge(
-                        "rounded-xl h-5 text-slate-700  dark:text-slate-400 text-[9px] lg:text-[10px] text-center flex flex-row items-center justify-center bg-slate-300 dark:bg-slate-700",
-                        hasUserVoted && "bg-gradient-to-r text-white dark:text-white from-blue-600 to-blue-700",
-                        isGreen && "bg-gradient-to-r from-green-400 to-green-400",
-                        isRed && "bg-gradient-to-r from-red-400 to-red-400 ",
-                    )}
-                >
-                    {/* <p className="truncate" >{voteCount} Votes</p> */}
-                </div>
-
-                <div className=" h-5  flex flex-row items-center justify-center" >
-                    {isGreen && <Check className="w-3 h-3" />}
-                    {isRed && <X className="w-3 h-3" />}
-                </div>
-            </div>
-        </div>
-    )
-}
