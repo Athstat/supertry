19335dc498fe76cbe61239c28650181f396cc1f6
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-06-02 11:56:16 +0200

    adds code to clear country filter on sbr page

--- src/components/sbr/fixtures/SbrFixturesTab.tsx
+++ src/components/sbr/fixtures/SbrFixturesTab.tsx
@@ -1,11 +1,11 @@
-import { Calendar, ChevronLeft, ChevronRight, FileX2 } from "lucide-react"
+import { Calendar, FileX2, X } from "lucide-react"
 import GroupedSbrFixturesList from "./GroupSbrFixtureList";
 import { ISbrFixture } from "../../../types/sbr";
 import PillBar, { PillBarItems } from "../../shared/bars/PillTabBar";
 import { useQueryState } from "../../../hooks/useQueryState";
 import { dateToStrWithoutTime, safeTransformStringToDate } from "../../../utils/dateUtils";
 import { addDays, format, isSameDay, subDays } from "date-fns";
-import { twMerge } from "tailwind-merge";
+import { formatPosition } from "../../../utils/athleteUtils";
 
 type Props = {
     fixtures: ISbrFixture[],
@@ -16,10 +16,14 @@ type Props = {
 /** Renders all the Sbr Fixtures */
 export default function SbrFixturesTab({ fixtures, weekEnd, weekStart }: Props) {
 
-    const [country] = useQueryState('fs', { init: 'all' });
+    const [country, setCoutry] = useQueryState('fs', { init: 'all' });
     const [pivotDateStr, setPivotDateStr] = useQueryState('pivot');
 
     const pivotDate = safeTransformStringToDate(pivotDateStr);
+    const countryFilterIsSet = country && country !== "all";
+    const clearCountryFilter = () => {
+        setCoutry("all");
+    }
 
     const filteredFixtures = fixtures.filter((f) => {
         if (country) {
@@ -112,8 +116,8 @@ export default function SbrFixturesTab({ fixtures, weekEnd, weekStart }: Props) 
                     <h1 className="text-xl font-bold" >Fixtures</h1>
                 </div>
 
-                {<div className="flex flex-row items-center" >
-                    {/* <p className="text-lg font-semibold mr-3" >Week {week}</p> */}
+                {/* <div className="flex flex-row items-center" >
+                    <p className="text-lg font-semibold mr-3" >Week {week}</p>
 
                     <button
                         onClick={onMoveLeft}
@@ -144,20 +148,27 @@ export default function SbrFixturesTab({ fixtures, weekEnd, weekStart }: Props) 
                     >
                         <ChevronRight className="w-7" />
                     </button>
-                </div>}
+                </div> */}
 
             </div>
 
-            {pivotDate && <p className="dark:text-slate-300 text-slate-700 font-medium" >{getDateMessage()}</p>}
+            {/* {pivotDate && <p className="dark:text-slate-300 text-slate-700 font-medium" >{getDateMessage()}</p>} */}
 
             {<GroupedSbrFixturesList
                 fixtures={filteredFixtures}
             />}
 
             {filteredFixtures.length === 0 && (
-                <div className="p-4 w-full flex gap-4 text-slate-700 dark:text-slate-400 flex-col items-center justify-center" >
+                <div className="p-5 w-full flex gap-4 text-slate-700 dark:text-slate-400 flex-col items-center justify-center" >
                     <FileX2 className="w-20 h-20" />
-                    <p>No fixtures we found for this week</p>
+                    <p className="w-4/5 text-center" >You are all caught up, no fixtures were found</p>
+                    {countryFilterIsSet && <div className="flex flex-row items-center gap-2 " >
+                        <p>You have a filter set </p>
+                        <button onClick={clearCountryFilter}  className="bg-slate-200 hover:bg-slate-300 dark:hover:bg-slate-600 flex flex-row items-center gap-1 px-2 py-0.5 text-sm rounded-xl dark:bg-slate-700" >
+                            {formatPosition(country)}
+                            <X className="w-3 h-3" />
+                        </button>
+                    </div>}
                 </div>
             )}
 
