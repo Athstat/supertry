9bd5c22e38356cab0ad59502af0ad7a63f598c55
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-06-04 06:13:32 +0200

    adds changes to profile modal

--- src/components/player/PlayerProfileModal.tsx
+++ src/components/player/PlayerProfileModal.tsx
@@ -28,7 +28,7 @@ export const PlayerProfileModal: React.FC<PlayerProfileModalProps> = ({
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 z-[200] flex items-center justify-center ">
-      <div className="bg-white dark:bg-black w-full max-w-2xl mx-auto my-4 rounded-lg shadow-xl max-h-[90vh] overflow-y-auto flex flex-col">
+      <div className="bg-white dark:bg-black w-[95%] max-w-2xl mx-auto my-4 rounded-lg shadow-xl h-[95vh] lg:h-[95vh] overflow-y-auto flex flex-col">
 
         <div className="bg-white dark:bg-dark-800/60 w-full rounded-lg shadow-xl h-full overflow-y-auto flex flex-col">
           {/* Modal header with player image and close button */}
