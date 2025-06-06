a209b744b218a30800bd82b5ec041261f1672cfa
Tadiwanashe Shangwa <80280920+Tadiwr@users.noreply.github.com>
2025-06-06 13:20:55 +0200

    fixes pr char issue

--- src/components/player/profile-modal-components/tabs/PRChartTab.tsx
+++ src/components/player/profile-modal-components/tabs/PRChartTab.tsx
@@ -35,7 +35,7 @@ export const PRChartTab: React.FC<PRChartTabProps> = ({ player }) => {
   const { theme } = useTheme();
   const isDarkMode = theme === "dark";
 
-  const { data, isLoading, error } = usePowerRankings(player.athlete_id);
+  const { data, isLoading, error } = usePowerRankings(player.tracking_id);
 
   console.log("playerPR", data);
 
