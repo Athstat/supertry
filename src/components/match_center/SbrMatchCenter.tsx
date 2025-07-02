f3cbf4248231bc41c4152d4ed62aad33d829f04f
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-07-03 00:21:21 +0200

    adds first name to profile screen

--- src/components/match_center/SbrMatchCenter.tsx
+++ src/components/match_center/SbrMatchCenter.tsx
@@ -1,7 +1,32 @@
 import React from 'react'
+import useSWR from 'swr'
+import { sbrService } from '../../services/sbrService';
+import { LoadingState } from '../ui/LoadingState';
+import SbrFixtureCard from '../sbr/SbrFixtureCard';
 
 export default function SbrMatchCenter() {
-  return (
-    <div>SbrMatchCenter</div>
-  )
+
+    const key = 'sbr-fixtures';
+    let {data: fixtures, isLoading} = useSWR(key, () => sbrService.getAllFixtures());
+
+    if (isLoading) {
+        return <LoadingState />
+    }
+
+    fixtures = fixtures ?? [];
+
+    return (
+        <div className='flex flex-col gap-2' >
+            <h1 className='font-bold text-lg' >Sbr Games</h1>
+
+            <div>
+                {fixtures.map((fixture, index) => {
+                    return <SbrFixtureCard 
+                        fixture={fixture}
+                        key={index}
+                    />
+                })}
+            </div>
+        </div>
+    )
 }
