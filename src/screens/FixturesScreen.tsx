91256cd7af8982ec6b05e5f93a71a748e6d4ebb1
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-07-03 03:30:00 +0200

    removes unused code

--- src/screens/FixturesScreen.tsx
+++ src/screens/FixturesScreen.tsx
@@ -1,23 +1,4 @@
-import React from 'react';
 import PageView from './PageView';
-import TabView, { TabViewHeaderItem, TabViewPage } from '../components/shared/tabs/TabView';
-import { ScopeProvider } from 'jotai-scope';
-import {
-  allSbrWeekFixturesAtom,
-  sbrFixturesPivotDateAtom,
-  sbrFixturesWeekEndAtom,
-  sbrFixturesWeekStartAtom,
-  sbrWeekFeatureGamesAtom,
-  sbrWeekFixturesAtom,
-} from '../state/sbrFixtures.atoms';
-import {
-  fixturesDateRangeAtom,
-  fixturesSelectedMonthIndexAtom,
-} from '../components/fixtures/calendar/fixtures_calendar.atoms';
-import SbrScreenDataProvider from '../components/sbr/SbrScreenDataProvider';
-import SbrFixturesTab from '../components/sbr/fixtures/SbrFixturesTab';
-import { useAtomValue } from 'jotai';
-import ProFixturesTab from './ProFixturesTab';
 import { Calendar } from 'lucide-react';
 import ToggleSwitch from '../components/shared/buttons/ToggleSwitch';
 import { useQueryState } from '../hooks/useQueryState';
@@ -25,32 +6,11 @@ import ProMatchCenter from '../components/match_center/ProMatchCenter';
 import SbrMatchCenter from '../components/match_center/SbrMatchCenter';
 
 export default function FixturesScreen() {
-  const sbrAtoms = [
-    sbrFixturesPivotDateAtom,
-    sbrFixturesWeekEndAtom,
-    sbrFixturesWeekStartAtom,
-    allSbrWeekFixturesAtom,
-    sbrWeekFixturesAtom,
-    sbrWeekFeatureGamesAtom,
-  ];
 
   const [fixtureContext, setFixtureContext] = useQueryState('sc', { init: 'Pros' })
 
-  const tabItems: TabViewHeaderItem[] = [
-    {
-      label: 'Professional',
-      tabKey: 'professional',
-      className: 'flex-1 text-sm lg:text-base',
-    },
-    {
-      label: 'School Boy Rugby',
-      tabKey: 'sbr',
-      className: 'flex-1 text-sm lg:text-base',
-    },
-  ];
-
   return (
-    <PageView className="dark:text-white p-4 md:p-6 flex flex-col gap-4">
+    <PageView className="dark:text-white lg:w-[60%] p-4 md:p-6 flex flex-col gap-4">
 
       <div className='flex flex-row justify-between' >
         <div className='flex flex-row items-center gap-2' >
@@ -68,16 +28,11 @@ export default function FixturesScreen() {
         </div>
       </div>
 
-      <div className="w-full lg:w-3/4 mx-auto">
+      <div className="w-full mx-auto">
         {fixtureContext === "Pros" && <ProMatchCenter />}
         {fixtureContext === "Sbr" && <SbrMatchCenter />}
       </div>
 
     </PageView>
   );
-}
-
-function SbrFixturesContent() {
-  const weekGames = useAtomValue(sbrWeekFixturesAtom);
-  return <SbrFixturesTab fixtures={weekGames} />;
 }
--- src/screens/PageView.tsx
+++ src/screens/PageView.tsx
@@ -41,31 +41,33 @@ export function TopicPageView({ children, title, statsCards, className }: TopicP
 
     return (
         <div>
-            <BlueGradientCard className="rounded-none p-4 min-h-32 items-start justify-center flex flex-col gap-2" >
-                <button
-                    onClick={handleGoBack}
-                    className="flex items-center text-primary-100 hover:text-white transition-colors"
-                >
-                    <ChevronLeft size={20} />
-                    <span>
-                        {"Go Back"}
-                    </span>
-                </button>
-                <p className="text-xl font-extrabold" >{title}</p>
+            <BlueGradientCard className="rounded-none p-4 py-8 lg:py-10 min-h-32 items-center justify-center flex flex-col gap-2" >
+                <div className="lg:w-1/2 w-full flex flex-col gap-4" >
+                    <button
+                        onClick={handleGoBack}
+                        className="flex items-center text-primary-100 hover:text-white transition-colors"
+                    >
+                        <ChevronLeft size={20} />
+                        <span>
+                            {"Go Back"}
+                        </span>
+                    </button>
+                    <p className="text-xl font-extrabold" >{title}</p>
 
-                {statsCards && <div className="grid grid-cols-2 w-full gap-2" >
-                    
-                    {statsCards.map((card, index) => {
-                        return (
-                            <TransparentStatCard 
-                                label={card.title}
-                                value={card.value}
-                                key={index}
-                            />
-                        )
-                    })}
+                    {statsCards && <div className="grid grid-cols-2 w-full gap-2" >
 
-                </div>}
+                        {statsCards.map((card, index) => {
+                            return (
+                                <TransparentStatCard
+                                    label={card.title}
+                                    value={card.value}
+                                    key={index}
+                                />
+                            )
+                        })}
+
+                    </div>}
+                </div>
             </BlueGradientCard>
 
             <PageView className={className} >
