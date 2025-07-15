6164cc65acabefdaad759c50d83ec1da01b48660
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-07-15 11:13:49 +0200

    adds input field compoent

---
+++ src/components/shared/InputField.tsx
@@ -0,0 +1,53 @@
+import { ReactNode } from 'react'
+import { twMerge } from 'tailwind-merge'
+
+type Props = {
+    value?: string,
+    onChange?: (newVal: string) => void,
+    label?: string,
+    type?: string,
+    id?: string,
+    inputCn?: string,
+    labelCn?: string,
+    className?: string,
+    icon?: ReactNode
+}
+
+export default function InputField({value, onChange, label, type, id, inputCn, labelCn, className, icon} : Props) {
+
+    const handleInputChange = (newVal?: string) => {
+        if (onChange && newVal) {
+            onChange(newVal)
+        }
+    }
+
+    return (
+        <div className={twMerge(className)} >
+            {label && <label
+                htmlFor={id}
+                className={twMerge(
+                    "block text-sm font-medium text-gray-700 dark:text-gray-300 mb-1",
+                    labelCn
+                )}
+            >
+                {label}
+
+            </label>}
+
+            <div className="relative">
+                <input
+                    id={id}
+                    type={type}
+                    required
+                    className={twMerge(
+                        "w-full px-4 py-3 bg-white dark:bg-dark-800/40 border border-gray-300 dark:border-dark-600 rounded-xl focus:ring-2 focus:ring-primary-500 dark:focus:ring-primary-400 focus:border-transparent dark:text-gray-100",
+                        inputCn
+                    )}
+                    value={value}
+                    onChange={e => handleInputChange(e.target.value)}
+                />
+                {icon}
+            </div>
+        </div>
+    )
+}
