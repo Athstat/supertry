f11fcf848edbd396abb7db36f544e9f8e6cebd69
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-07-18 10:49:22 +0200

    adds fxies to use fixture votes

--- src/hooks/useFxitureVotes.tsx
+++ src/hooks/useFxitureVotes.tsx
@@ -1,27 +1,33 @@
+import useSWR from "swr";
 import { sbrService } from "../services/sbrService";
 import { ISbrFixture, ISbrFixtureVote } from "../types/sbr";
 import { useAuthUser } from "./useAuthUser";
-import { useFetch } from "./useFetch";
+import { useEffect, useState, useTransition } from "react";
 
 export function useSbrFixtureVotes(fixture: ISbrFixture) {
 
     const user = useAuthUser();
 
-    const { data, isLoading, error } = useFetch(
-        "fixture-votes",
-        fixture.fixture_id,
-        sbrService.getFixtureVotes
-    );
+    const key = `sbr/${fixture.fixture_id}/votes`;
+    const { data, isLoading, error } = useSWR(key, () => sbrService.getFixtureVotes(fixture.fixture_id));
 
+    const [userVote, setUserVote] = useState<ISbrFixtureVote>();
+
+    const [_, startTransition] = useTransition();
     const votes = data ?? [];
 
-    let userVote: ISbrFixtureVote | undefined;
 
-    votes.forEach((v) => {
-        if (v.user_id === user.id) {
-            userVote = v;
-        }
-    });
+    useEffect(() => {
+
+        startTransition(() => {
+            votes.forEach((v) => {
+                if (v.user_id === user.kc_id) {
+                    setUserVote(v);
+                }
+            })
+        });
+
+    }, [votes]);
 
     const homeVotes = votes.filter((v) => {
         return v.vote_for === "home_team";
