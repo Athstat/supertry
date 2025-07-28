156f97f198ac67c90765456d1a408b23145ff579
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-07-28 22:12:28 +0200

    removes banner

--- src/screens/DashboardScreen.tsx
+++ src/screens/DashboardScreen.tsx
@@ -17,24 +17,6 @@ export function DashboardScreen() {
   return (
     <PageView className="flex flex-col space-y-6 p-4">
 
-      <div className='flex flex-col gap-2' >
-        <HeroImageBanner
-          link='/images/africa_cup.jpeg'
-        />
-        <p className="font-bold text-lg">Rugby Africa Cup 2025 is here!</p>
-        
-        <SecondaryText className="text-xs">
-          Stay updated with all the latest Africa Cup fixtures. Check out upcoming matches and follow your favorite teams throughout the tournament.
-        </SecondaryText>
-
-        <button
-          className="mt-2 px-4 text-sm py-2 border border-blue-400 bg-primary-600 text-white rounded-xl hover:bg-primary-700 transition"
-          onClick={() => goToFixtures()}
-        >
-          View Fixtures
-        </button>
-      </div>
-
       {/* <ActionList /> */}
       <UpcomingFixturesSection />
       <FeaturedPlayersCarousel />
