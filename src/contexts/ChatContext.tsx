e35f2cd22c683956d8fb48bdf4d68a6b7b9018d2
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-08-07 17:27:32 +0200

    removes unused improt

--- src/contexts/ChatContext.tsx
+++ src/contexts/ChatContext.tsx
@@ -1,6 +1,5 @@
 import { SendBirdProvider } from "@sendbird/uikit-react";
 import { SEND_BIRD_APP_ID } from "../data/messaging/send_bird.init";
-import { authService } from "../services/authService";
 import { useTheme } from "./ThemeContext";
 import { ReactNode } from "react";
 import { useAuth } from "./AuthContext";
