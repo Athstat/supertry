7325a56ed921190393d26beee48816fc87ddbf6a
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-08-07 17:21:10 +0200

    adds useGuestLogin hook

---
+++ src/hooks/auth/useGuestLogin.ts
@@ -0,0 +1,55 @@
+import { useCallback, useState } from "react";
+import { useDeviceId } from "../useDeviceId";
+import { authService } from "../../services/authService";
+import { useAuth } from "../../contexts/AuthContext";
+import { useNavigate } from "react-router-dom";
+
+/** Hook that provides functionality to handle guest login */
+export function useGuestLogin() {
+
+    const [error, setError] = useState<string>();
+    const [isLoading, setLoading] = useState<boolean>(false);
+    const { deviceId } = useDeviceId();
+
+    const navigate = useNavigate();
+
+    const { setAuth } = useAuth();
+
+    const handleGuestLogin = useCallback(async () => {
+
+        setError(undefined);
+
+        if (!deviceId) return;
+
+        try {
+
+            setLoading(true);
+            const { data, error } = await authService.authenticateAsGuestUser(deviceId);
+
+            if (data) {
+
+                const accessToken = data.token;
+                const authUser = data.user;
+
+                setAuth(accessToken, authUser);
+
+                navigate('/dashboard');
+            } else {
+                setError(error?.message);
+            }
+
+        } catch (err) {
+            console.error('Guest login error:', err);
+            setError('Failed to sign in as guest. Please try again.');
+        } finally {
+            setLoading(false);
+        }
+    }, [deviceId, setAuth])
+
+    return {
+        handleGuestLogin,
+        isLoading,
+        error,
+        deviceId
+    }
+}
