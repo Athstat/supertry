47210dd88b789bad973e69dfb6c63207b37578e8
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-08-10 03:42:30 +0200

    adds service method to get my created leagues

--- src/services/fantasy/fantasyLeagueGroupsService.ts
+++ src/services/fantasy/fantasyLeagueGroupsService.ts
@@ -22,6 +22,25 @@ export const fantasyLeagueGroupsService = {
         return [];
     },
 
+    getMyCreatedLeagues: async (): Promise<FantasyLeagueGroup[]> => {
+        try {
+
+            const uri = getUri(`/api/v1/fantasy-league-groups/mine`);
+            const res = await fetch(uri, {
+                headers: getAuthHeader()
+            });
+
+            if (res.ok) {
+                return (await res.json()) as FantasyLeagueGroup[];
+            }
+
+        } catch (err) {
+            console.log("Error fetching user's fantasy league groups ", err);
+        }
+
+        return [];
+    },
+
     getGroupById: async (leagueId: string) => {
         try {
             const uri = getUri(`/api/v1/fantasy-league-groups/${leagueId}`);
@@ -43,7 +62,7 @@ export const fantasyLeagueGroupsService = {
         try {
 
             const uri = getUri(`/api/v1/fantasy-league-groups/${leagueId}/members`);
-            
+
             const res = await fetch(uri, {
                 headers: getAuthHeader()
             });
