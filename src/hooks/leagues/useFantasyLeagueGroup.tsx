b7d4b41509016ea1df7bacd3dc7e1b59f7aa7cf3
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-08-10 14:07:00 +0200

    add hooks

--- src/hooks/leagues/useFantasyLeagueGroup.tsx
+++ src/hooks/leagues/useFantasyLeagueGroup.tsx
@@ -1,3 +1,15 @@
+import { useAtomValue } from "jotai";
+import { fantasyLeagueGroupAtom, fantasyLeagueGroupMembersAtom, fantasyLeagueGroupRoundsAtom } from "../../state/fantasy/fantasyLeagueGroup.atoms";
+
+/** Hook that provides fantasy league group info. Should be used with in the fantasy league group provider */
 export function useFantasyLeagueGroup() {
-    
+    const league = useAtomValue(fantasyLeagueGroupAtom);
+    const members = useAtomValue(fantasyLeagueGroupMembersAtom);
+    const rounds = useAtomValue(fantasyLeagueGroupRoundsAtom);
+
+    return {
+        league,
+        members,
+        rounds
+    }
 }
