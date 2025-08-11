591f1b445ea63d60b9982480a81e04f9b92eecdc
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-08-11 12:00:58 +0200

    adds empty rosters message

--- src/components/fixtures/FixtureRosters.tsx
+++ src/components/fixtures/FixtureRosters.tsx
@@ -7,6 +7,8 @@ import { gamesService } from "../../services/gamesService";
 import { useState } from "react";
 import { IProAthlete } from "../../types/athletes";
 import PlayerProfileModal from "../player/PlayerProfileModal";
+import NoContentCard from "../shared/NoContentMessage";
+import { fixtureSumary } from "../../utils/fixtureUtils";
 
 type Props = {
     fixture: IFixture,
@@ -20,6 +22,9 @@ export default function FixtureRosters({ fixture }: Props) {
 
     const [selectedPlayer, setSelectedPlayer] = useState<IProAthlete>();
     const [showModal, setShowModal] = useState<boolean>(false);
+
+    const {game_status} = fixtureSumary(fixture);
+    const gameComplete = game_status === "completed";
 
     const toggleModal = () => {
         setShowModal(prev => !prev);
@@ -58,6 +63,12 @@ export default function FixtureRosters({ fixture }: Props) {
     const handleClickPlayer = (player: IProAthlete) => {
         setSelectedPlayer(player);
         setShowModal(true);
+    }
+
+    if (rosters.length === 0) {
+        return <NoContentCard 
+            message={gameComplete ? "Rosters were not available for this match" : "Rosters are not yet available for this match"}
+        />
     }
 
     return (
