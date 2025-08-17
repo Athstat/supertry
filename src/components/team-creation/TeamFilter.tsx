aae6cd4e859438565fc9d4a7ef2ba96e9befb898
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-08-18 01:27:14 +0200

    fixes team logo issue

--- src/components/team-creation/TeamFilter.tsx
+++ src/components/team-creation/TeamFilter.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { alphabeticalComparator } from "../../utils/stringUtils";
+import TeamLogo from "../team/TeamLogo";
 
 interface Team {
   id: string;
@@ -42,7 +43,7 @@ const TeamFilter: React.FC<TeamFilterProps> = ({
                 key={team.id}
                 onClick={() => toggleTeamFilter(team.id)}
                 className={`
-                px-3 py-1.5 rounded-full text-xs font-medium transition shrink-0
+                px-3 py-1.5 flex flex-row items-center gap-2 rounded-full text-xs font-medium transition shrink-0
                 ${
                   teamFilter.includes(team.id)
                     ? "bg-green-100 dark:bg-green-900/30 text-green-800 dark:text-green-400 border border-green-500 shadow-sm"
@@ -51,12 +52,13 @@ const TeamFilter: React.FC<TeamFilterProps> = ({
               `}
               >
                 {team.logo && (
-                  <img
-                    src={team.logo}
-                    alt={team.name}
-                    className="w-4 h-4 inline mr-1"
+                  <TeamLogo 
+                    url={team.logo}
+                    teamName={team.name}
+                    className="w-5 h-5"
                   />
                 )}
+
                 {team.name}
               </button>
             ))}
