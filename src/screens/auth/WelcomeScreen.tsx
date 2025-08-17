b90de8713b84180d62354c14372feea4bb076ea7
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-08-17 23:57:01 +0200

    adds rugby pitch in the background

--- src/screens/auth/WelcomeScreen.tsx
+++ src/screens/auth/WelcomeScreen.tsx
@@ -7,6 +7,7 @@ import PrimaryButton from '../../components/shared/buttons/PrimaryButton';
 import SecondaryText from '../../components/shared/SecondaryText';
 import { useGuestLogin } from '../../hooks/auth/useGuestLogin';
 import { ErrorMessage } from '../../components/ui/ErrorState';
+import MovingRugbyPitch from '../../components/shared/MovingRugbyPitch';
 
 export function WelcomeScreen() {
   const navigate = useNavigate();
@@ -102,66 +103,70 @@ export function WelcomeScreen() {
   }
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50 dark:bg-dark-850 px-4">
-      <motion.div
-        initial={{ opacity: 0, y: -20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.6 }}
-        className="w-full max-w-md flex flex-col gap-2 items-center"
-      >
-        <ScrummyLogo className="w-60 h-60 md:w-60 md:h-60 -mb-5" />
 
+    <MovingRugbyPitch className='h-[100vh] overflow-y-auto flex flex-col items-center justify-center' >
+      <div className="flex z-50 bg-green-950/70 flex-col items-center justify-center h-full px-4">
         <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ delay: 0.3, duration: 0.6 }}
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6 }}
+          className="w-full max-w-md flex flex-col gap-2 items-center"
         >
-          <h1 className="text-3xl md:text-4xl font-bold text-gray-900 dark:text-white text-center">
-            Welcome to SCRUMMY
-          </h1>
-        </motion.div>
+          <ScrummyLogo className="w-60 h-60 md:w-60 md:h-60 -mb-5" />
 
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ delay: 0.5, duration: 0.6 }}
-        >
-          <p className="mt-3 lg:text-xl text-gray-600 dark:text-gray-300 text-center">
-            You've officially joined the scrum! Don't worry, it's less bruises and more bragging
-            rights from here.
-          </p>
-        </motion.div>
-
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ delay: 0.5, duration: 0.6 }}
-          className='flex flex-1 flex-col gap-4 items-center justify-center p-4 w-full' >
-          
-          {deviceId && <PrimaryButton
-            className='w-full py-3 animate-glow'
-            onClick={handleGuestLogin}
-            isLoading={isLoading}
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.3, duration: 0.6 }}
           >
-            Get Started!
-          </PrimaryButton>}
+            <h1 className="text-3xl md:text-4xl font-bold text-gray-900 dark:text-white text-center">
+              Welcome to SCRUMMY
+            </h1>
+          </motion.div>
 
-          {error && <ErrorMessage message={error} />}
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.5, duration: 0.6 }}
+          >
+            <p className="mt-3 lg:text-xl text-gray-600 dark:text-gray-300 text-center">
+              You've officially joined the scrum! Don't worry, it's less bruises and more bragging
+              rights from here.
+            </p>
+          </motion.div>
 
-          {!deviceId && <PrimaryButton onClick={navigateToSignin} className='w-full py-3 animate-glow' >
-            Sign In
-          </PrimaryButton>}
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.5, duration: 0.6 }}
+            className='flex flex-1 flex-col gap-4 items-center justify-center p-4 w-full' >
 
-          {deviceId && (
-            <div onClick={navigateToSignin} >
-              <SecondaryText className='underline cursor-pointer hover:text-slate-800 dark:text-slate-300' >
-                Already Part of The Scrum? Sign In
-              </SecondaryText>
-            </div>
-          )}
+            {deviceId && <PrimaryButton
+              className='w-full py-3 animate-glow'
+              onClick={handleGuestLogin}
+              isLoading={isLoading}
+            >
+              Get Started!
+            </PrimaryButton>}
 
+            {error && <ErrorMessage message={error} />}
+
+            {!deviceId && <PrimaryButton onClick={navigateToSignin} className='w-full py-3 animate-glow' >
+              Sign In
+            </PrimaryButton>}
+
+            {deviceId && (
+              <div onClick={navigateToSignin} >
+                <SecondaryText className='underline cursor-pointer hover:text-slate-800 dark:text-slate-300' >
+                  Already Part of The Scrum? Sign In
+                </SecondaryText>
+              </div>
+            )}
+
+          </motion.div>
         </motion.div>
-      </motion.div>
-    </div>
+      </div>
+    </MovingRugbyPitch>
+
   );
 }
