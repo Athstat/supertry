f5afd136d629413c491abd270da07bd8698a99d6
Your GitHub Username <jabulani.kunene@athstat.com>
2025-08-18 19:36:37 +0200

    latest

--- src/components/player/PlayerProfileModal.tsx
+++ src/components/player/PlayerProfileModal.tsx
@@ -1,9 +1,9 @@
-import PlayerProfileBanner from "./profile-modal-components/PlayerProfileBanner";
-import PlayerNameAndPosition from "./profile-modal-components/PlayerNameAndPosition";
-import PlayerProfileModalTabContent from "./profile-modal-components/PlayerProfileModalTabContent";
-import PlayerDataProvider from "./provider/PlayerDataProvider";
-import DialogModal from "../shared/DialogModal";
-import { IProAthlete } from "../../types/athletes";
+import PlayerProfileBanner from './profile-modal-components/PlayerProfileBanner';
+import PlayerNameAndPosition from './profile-modal-components/PlayerNameAndPosition';
+import PlayerProfileModalTabContent from './profile-modal-components/PlayerProfileModalTabContent';
+import PlayerDataProvider from './provider/PlayerDataProvider';
+import DialogModal from '../shared/DialogModal';
+import { IProAthlete } from '../../types/athletes';
 
 interface Props {
   player: IProAthlete;
@@ -13,9 +13,10 @@ interface Props {
 }
 
 export default function PlayerProfileModal({ player, isOpen, onClose }: Props) {
+  console.log('Player: ', player);
 
   return (
-    <PlayerDataProvider player={player} >
+    <PlayerDataProvider player={player}>
       <DialogModal
         open={isOpen}
         className="p-0 flex flex-col gap-2 "
@@ -23,7 +24,6 @@ export default function PlayerProfileModal({ player, isOpen, onClose }: Props) {
         outerCon="p-4 no-scrollbar"
         onClose={onClose}
         hw="w-[96%] max-h-[96vh] min-h-[96vh] md:w-[60%] lg:w-[40%]"
-        
       >
         {/* Modal header with player image and close button */}
         <PlayerProfileBanner player={player} />
@@ -32,13 +32,8 @@ export default function PlayerProfileModal({ player, isOpen, onClose }: Props) {
         <PlayerNameAndPosition player={player} />
 
         <div className="flex-1 ">
-          
-          <PlayerProfileModalTabContent
-            player={player}
-          />
-
+          <PlayerProfileModalTabContent player={player} />
         </div>
-
       </DialogModal>
     </PlayerDataProvider>
   );
