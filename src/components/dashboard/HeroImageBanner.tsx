17bbdafd35d0dab3bd81086647b4f5a57cadeb52
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-08-19 19:57:09 +0200

    fixes join weekly league card

---
+++ src/components/dashboard/HeroImageBanner.tsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+type BannerProps = {
+  link?: string
+}
+
+export default function HeroImageBanner({ link = "/images/africa_banner.jpg" }: BannerProps) {
+
+  const [error, setError] = useState<boolean>(false);
+  const [isLoading, setLoading] = useState<boolean>(true);
+
+  if (error) return;
+
+  return (
+    <div className="w-full overflow-clip bg-white dark:border-slate-800 rounded-xl relative">
+
+      {isLoading && (
+        <div className="w-full h-44 overflow-clip bg-slate-300 dark:bg-slate-800/60 animate-pulse dark:border-slate-800 rounded-xl relative">
+        </div>
+      )}
+
+      {!error && <img
+        src={link}
+        alt="Africa Cup Banner"
+        className="w-full h-full object-center"
+        onAbort={() => setLoading(false)}
+        onLoad={() => setLoading(false)}
+        onError={() => {
+          setLoading(false);
+          setError(true);
+        }}
+        loading={'lazy'}
+      />}
+      {/* <div className="absolute bottom-0 left-0 w-full bg-gradient-to-tpx-6 py-4 flex flex-col items-start">
+        <h3
+          className="text-white text-lg font-bold drop-shadow-xl mb-2"
+          style={{
+            textShadow:
+              '0 4px 24px #000, 0 2px 8px #000, 0 1px 0 #000, 2px 2px 8px #000, -2px 2px 8px #000'
+          }}
+        >
+        </h3>
+      </div> */}
+    </div>
+  )
+}
--- src/screens/DashboardScreen.tsx
+++ src/screens/DashboardScreen.tsx
@@ -1,20 +1,25 @@
 import PageView from './PageView';
-import MyWeekPanel from '../components/dashboard/MyWeekPanel';
 import UpcomingFixturesSection from '../components/dashboard/UpcomingFixturesSection';
 import FeaturedPlayersCarousel from '../components/dashboard/FeaturedPlayersCarousel';
 import ComparePlayersPanel from '../components/dashboard/ComparePlayersPanel';
 import { Home } from 'lucide-react';
-import { HeroSection } from '../components/dashboard';
+import { HeroImageBanner } from '../components/dashboard/JoinWeeklyLeagueCard';
 
 export function DashboardScreen() {
   return (
     <PageView className="flex flex-col space-y-6 p-4">
+      
       <div className="flex flex-row items-center gap-2">
         <Home />
         <p className="text-xl font-extrabold">Dashboard</p>
       </div>
 
-      <HeroSection />
+      {/* <HeroSection /> */}
+
+      <HeroImageBanner 
+        link={'/public/images/wwc_2025_banner.jpg'}
+      />
+
       <FeaturedPlayersCarousel />
 
       {/* <ActionList /> */}
