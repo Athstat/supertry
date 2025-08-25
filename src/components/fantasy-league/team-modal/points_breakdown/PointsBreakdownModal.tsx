96338104101c70fa0afdaf5b6fb70f253ebd8f26
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-08-25 21:15:45 +0200

    adds points breakdown

---
+++ src/components/fantasy-league/team-modal/points_breakdown/PointsBreakdownModal.tsx
@@ -0,0 +1,32 @@
+import { IProAthlete } from "../../../../types/athletes"
+import { IFantasyLeagueTeam, FantasyLeagueTeamWithAthletes, IFantasyLeagueRound } from "../../../../types/fantasyLeague"
+import DialogModal from "../../../shared/DialogModal"
+import PlayerPointsBreakdownView from "./PlayerPointsBreakdownView"
+
+type Props = {
+    athlete: IProAthlete,
+    team: IFantasyLeagueTeam | FantasyLeagueTeamWithAthletes,
+    round: IFantasyLeagueRound,
+    onClose?: () => void,
+    isOpen?: boolean
+}
+
+/** Renders a points breakdown modal */
+export default function PointsBreakdownModal({athlete, team, round, onClose, isOpen} : Props) {
+  return (
+    <DialogModal
+        open={isOpen}
+        title={athlete.player_name}
+        onClose={onClose}
+        hw="min-h-[90vh] max-h-[90vh]"
+    >
+
+        <PlayerPointsBreakdownView 
+            athlete={athlete}
+            team={team}
+            round={round}
+        />
+
+    </DialogModal>
+  )
+}
