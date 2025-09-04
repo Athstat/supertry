bc1884ecee97a9c01ccd61f6f5304d7c05972653
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-09-04 22:22:21 +0200

    adds conditional rendering for betal tag

--- src/components/branding/BetaTag.tsx
+++ src/components/branding/BetaTag.tsx
@@ -1,4 +1,5 @@
 import { twMerge } from "tailwind-merge"
+import { isInProduction } from "../../utils/webUtils"
 
 type Props = {
     className?: string
@@ -6,6 +7,11 @@ type Props = {
 
 /** Render a beta tag */
 export default function BetaTag({ className } : Props) {
+
+  const inProd = isInProduction();
+
+  if (inProd) return;
+
   return (
     <div className={twMerge(
       "rounded-xl px-2 w-fit font-medium py-0.5 text-xs border border-green-500 text-green-600 bg-green-500/30 dark:bg-green-900/40",
