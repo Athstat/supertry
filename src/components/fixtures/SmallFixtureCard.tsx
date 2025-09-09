eb7319ed2d503ceac927e86d0140946589607c86
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-09-09 14:36:35 +0200

    makes fixture card clickable

--- src/components/fixtures/SmallFixtureCard.tsx
+++ src/components/fixtures/SmallFixtureCard.tsx
@@ -3,6 +3,11 @@ import { IFixture } from "../../types/games"
 import SecondaryText from "../shared/SecondaryText"
 import TeamLogo from "../team/TeamLogo"
 import { fixtureSumary } from "../../utils/fixtureUtils"
+import { Fragment, useState } from "react"
+import { analytics } from "../../services/analytics/anayticsService"
+import { FixtureCardModal } from "./FixtureCard"
+import { twMerge } from "tailwind-merge"
+import ProFixtureVotingBox from "./voting/ProFixtureVotingBox"
 
 type Props = {
     fixture: IFixture
@@ -10,59 +15,83 @@ type Props = {
 
 export default function SmallFixtureCard({ fixture }: Props) {
 
-    const {hasNotStarted, matchFinal} = fixtureSumary(fixture);
+    const { hasNotStarted, matchFinal } = fixtureSumary(fixture);
+    const [showModal, setShowModal] = useState(false);
+
+    const toggle = () => setShowModal(prev => !prev);
+
+    const handleClick = () => {
+        analytics.trackFixtureCardClicked(fixture);
+        toggle();
+    }
 
     return (
-        <div className={'border dark:border-slate-700  rounded-xl p-6 flex flex-col gap-4'} >
-            <div>
-                <SecondaryText>{fixture.competition_name} - Week {fixture.round}</SecondaryText>
-            </div>
+        <Fragment>
+            <div
+                onClick={handleClick}
+                className={twMerge(
+                    'border dark:border-slate-700 cursor-pointer dark:bg-slate-800/20  rounded-xl p-6 flex flex-col gap-4',
+                    'hover:bg-white dark:hover:bg-slate-800/40'
+                )}
+            >
+                <div>
+                    <SecondaryText>{fixture.competition_name} - Week {fixture.round}</SecondaryText>
+                </div>
 
-            <div className="flex flex-row items-center gap-2 divide-x dark:divide-slate-700" >
+                <div className="flex flex-row items-center gap-2 divide-x dark:divide-slate-700" >
 
-                <div className="flex flex-col gap-2 w-2/3" >
-                    <div className="flex flex-row items-center justify-between" >
-                        <div className="flex flex-row items-center gap-2" >
+                    <div className="flex flex-col gap-2 w-2/3" >
+                        <div className="flex flex-row items-center justify-between" >
+                            <div className="flex flex-row items-center gap-2" >
 
-                            <TeamLogo
-                                url={fixture.team?.image_url}
-                                teamName={fixture.team?.athstat_name}
-                                className="w-8 h-8"
-                            />
-                            <p className="text-sm" >{fixture.team?.athstat_name}</p>
+                                <TeamLogo
+                                    url={fixture.team?.image_url}
+                                    teamName={fixture.team?.athstat_name}
+                                    className="w-8 h-8"
+                                />
+                                <p className="text-sm" >{fixture.team?.athstat_name}</p>
+                            </div>
+
+                            <div>
+                                {!hasNotStarted ? <p>{fixture.team_score}</p> : ''}
+                            </div>
                         </div>
 
-                        <div>
-                            {!hasNotStarted ? <p>{fixture.team_score}</p> : ''}
+                        <div className="flex flex-row items-center justify-between" >
+                            <div className="flex flex-row items-center gap-2" >
+
+                                <TeamLogo
+                                    url={fixture.opposition_team?.image_url}
+                                    teamName={fixture?.opposition_team?.athstat_name}
+                                    className="w-8 h-8"
+                                />
+                                <p className="text-sm" >{fixture?.opposition_team?.athstat_name}</p>
+                            </div>
+
+
+
+                            <div>
+                                {!hasNotStarted ? <p>{fixture.opposition_score}</p> : ''}
+                            </div>
                         </div>
                     </div>
 
-                    <div className="flex flex-row items-center justify-between" >
-                        <div className="flex flex-row items-center gap-2" >
-
-                            <TeamLogo
-                                url={fixture.opposition_team?.image_url}
-                                teamName={fixture?.opposition_team?.athstat_name}
-                                className="w-8 h-8"
-                            />
-                            <p className="text-sm" >{fixture?.opposition_team?.athstat_name}</p>
-                        </div>
-
-                        
-
-                        <div>
-                            {!hasNotStarted ? <p>{fixture.opposition_score}</p> : ''}
-                        </div>
+                    <div className="flex text-xs flex-col w-1/3 p-2 items-center justify-center" >
+                        {!matchFinal && fixture.kickoff_time && <SecondaryText className="text-xs" >{format(fixture.kickoff_time, 'HH:mm')}</SecondaryText>}
+                        {matchFinal && <SecondaryText className="text-xs" >Final</SecondaryText>}
+                        {fixture.kickoff_time && <SecondaryText className="text-xs" >{format(fixture.kickoff_time, 'dd MMM yyy')}</SecondaryText>}
                     </div>
+
                 </div>
 
-                <div className="flex text-xs flex-col w-1/3 p-2 items-center justify-center" >
-                    {!matchFinal && fixture.kickoff_time && <SecondaryText className="text-xs" >{format(fixture.kickoff_time, 'HH:mm')}</SecondaryText>}
-                    {matchFinal && <SecondaryText className="text-xs" >Final</SecondaryText>}
-                    {fixture.kickoff_time && <SecondaryText className="text-xs" >{format(fixture.kickoff_time, 'dd MMM yyy')}</SecondaryText>}
-                </div>
-
+                <ProFixtureVotingBox fixture={fixture} />
             </div>
-        </div>
+
+            <FixtureCardModal 
+                fixture={fixture}
+                onClose={toggle}
+                showModal={showModal}
+            />
+        </Fragment>
     )
 }
