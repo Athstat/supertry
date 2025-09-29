469414bc12a809a1dc3abbcc632e77ca0188f5db
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-09-29 19:47:41 +0200

    starts all over again

--- src/components/dashboard/stories/GameStoriesCarrousel.tsx
+++ src/components/dashboard/stories/GameStoriesCarrousel.tsx
@@ -1,29 +1,23 @@
-import { Fragment, useState } from "react";
 import { useDashboard } from "../../../hooks/dashboard/useDashboard"
 import { useRoundGames } from "../../../hooks/fixtures/useRoundGames";
 import RoundedCard from "../../shared/RoundedCard";
 import SecondaryText from "../../shared/SecondaryText";
-import GameStoryItem from "./GameStoryItem";
-import { IFixture } from "../../../types/games";
-import DialogModal from "../../shared/DialogModal";
-import GameStoryBoard from "./GameStoryBoard";
-import StoryModal from "./StoryModal";
 
 /** Renders an instagram like game stories carrousel component */
 export default function GameStoriesCarrousel() {
 
   const { currentRound } = useDashboard();
-  const { games, isLoading } = useRoundGames(currentRound);
+  const { isLoading } = useRoundGames(currentRound);
 
-  const [currentGame, setCurrentGame] = useState<IFixture>();
-  const [showModal, setShowModal] = useState<boolean>(false);
+  // const [currentGame, setCurrentGame] = useState<IFixture>();
+  // const [showModal, setShowModal] = useState<boolean>(false);
 
-  const toggle = () => setShowModal(prev => !prev);
+  // const toggle = () => setShowModal(prev => !prev);
 
-  const onClickStoryItem = (game: IFixture) => {
-    setCurrentGame(game);
-    toggle();
-  }
+  // const onClickStoryItem = (game: IFixture) => {
+  //   setCurrentGame(game);
+  //   toggle();
+  // }
 
   if (isLoading) {
     return (
@@ -62,18 +56,10 @@ export default function GameStoriesCarrousel() {
       </div>
 
       <div className="flex flex-row w-full items-center gap-2 no-scrollbar overflow-x-auto" >
-        {games.map((game) => {
-          return (
-            <GameStoryItem
-              key={game.game_id}
-              game={game}
-              onClick={onClickStoryItem}
-            />
-          )
-        })}
+        
       </div>
 
-      {showModal && (
+      {/* {showModal && (
         <Fragment>
           {currentGame && (
             <GameStoryBoard
@@ -83,7 +69,7 @@ export default function GameStoriesCarrousel() {
             />
           )}
         </Fragment>
-      )}
+      )} */}
 
     </div>
   )
