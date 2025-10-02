4d56ae52974f49e12de2a7ae58b4fc5246a95ed3
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-10-02 06:31:33 +0200

    adds fab button for user to go back top of the page

--- src/components/player-picker/PlayerPickerV2.tsx
+++ src/components/player-picker/PlayerPickerV2.tsx
@@ -3,7 +3,6 @@ import { playerPickerAtoms } from "../../state/playerPicker/playerPicker"
 import { IProAthlete, PositionClass } from "../../types/athletes"
 import { IFantasyTeamAthlete } from "../../types/fantasyTeamAthlete"
 import { IFantasyAthlete } from "../../types/rugbyPlayer"
-import DialogModal from "../shared/DialogModal"
 import { IFantasyLeagueRound } from "../../types/fantasyLeague"
 import { fantasyLeagueAtom } from "../../state/fantasy/fantasyLeague.atoms"
 import PlayerPickerHeader from "./PlayerPickerHeader"
@@ -11,6 +10,10 @@ import PlayerPickerPlayerList from "./PlayerPickerPlayerList"
 import PlayerPickerDataProvider from "./PlayerPickerDataProvider"
 import { usePlayerPicker } from "../../hooks/playerPicker/usePlayerPicker"
 import { formatPosition } from "../../utils/athleteUtils"
+import { useInView } from "react-intersection-observer"
+import PlayerPickerFAB from "./PlayerPickerFAB"
+import { useRef } from "react"
+import DialogModal from "../shared/DialogModal"
 
 type Props = {
   playerToBeReplaced?: IProAthlete | IFantasyAthlete | IFantasyTeamAthlete,
@@ -20,18 +23,18 @@ type Props = {
   isOpen?: boolean,
   title?: string,
   remainingBudget?: number,
-  excludePlayers?: (IProAthlete | IFantasyAthlete | IFantasyTeamAthlete | {tracking_id: string})[],
+  excludePlayers?: (IProAthlete | IFantasyAthlete | IFantasyTeamAthlete | { tracking_id: string })[],
   onClose?: () => void
 }
 
 /** Renders player picker version 2, with improved stability */
-export default function PlayerPickerV2({ 
+export default function PlayerPickerV2({
   playerToBeReplaced, positionPool,
   onSelectPlayer, targetLeagueRound,
   isOpen, title, remainingBudget,
   excludePlayers, onClose
 }: Props) {
-  
+
   const atoms = [
     playerPickerAtoms.availableTeamsAtom,
     playerPickerAtoms.filterTeamsAtom,
@@ -43,7 +46,7 @@ export default function PlayerPickerV2({
     playerPickerAtoms.maxPlayerPriceAtom,
     fantasyLeagueAtom
   ]
-  
+
   return (
     <ScopeProvider atoms={atoms} >
       <PlayerPickerDataProvider
@@ -57,7 +60,7 @@ export default function PlayerPickerV2({
         remainingBudget={remainingBudget}
         onClose={onClose}
       >
-        <InnerPlayerPicker 
+        <InnerPlayerPicker
           title={title}
           onClose={onClose}
         />
@@ -71,11 +74,26 @@ type InnerPlayerPickerProps = {
   onClose?: () => void
 }
 
-function InnerPlayerPicker({title, onClose}: InnerPlayerPickerProps) {
+function InnerPlayerPicker({ title, onClose }: InnerPlayerPickerProps) {
 
-  const {positionPool} = usePlayerPicker();
+  const { positionPool } = usePlayerPicker();
   const positionName = formatPosition(positionPool);
-  const dialogTitle = title ? title : positionName ? `Select a ${positionName} Player` : 'Select A Player'; 
+  const dialogTitle = title ? title : positionName ? `Select a ${positionName} Player` : 'Select A Player';
+
+  const topRef = useRef<HTMLDivElement | null>(null);
+  const { ref: inViewRef, inView } = useInView();
+  // actual DOM ref
+
+  const setRefs = (node: HTMLDivElement | null) => {
+    topRef.current = node;
+    inViewRef(node);
+  };
+
+  const handleClickFAB = () => {
+    if (!inView && topRef) {
+      topRef?.current?.scrollIntoView({ behavior: "smooth", block: "start" });
+    }
+  }
 
   return (
     <DialogModal
@@ -83,12 +101,17 @@ function InnerPlayerPicker({title, onClose}: InnerPlayerPickerProps) {
       title={dialogTitle}
       onClose={onClose}
       hw="min-h-[95vh] lg:w-[60vh]"
-      className="flex flex-col gap-2"
+      className="flex flex-col gap-2 relative"
       outerCon="no-scrollbar"
+      ref={setRefs}
     >
       <PlayerPickerHeader />
-      
+
       <PlayerPickerPlayerList />
+      <PlayerPickerFAB
+        onClick={handleClickFAB}
+        show={!inView}
+      />
     </DialogModal>
   )
 }
