ff792d1f22f62e2a14980daaa8654827d11ffdfc
Your GitHub Username <jabulani.kunene@athstat.com>
2025-10-03 13:35:50 +0200

    updated availability types

--- src/types/fantasyTeamAthlete.ts
+++ src/types/fantasyTeamAthlete.ts
@@ -1,52 +1,52 @@
-import { Gender } from "./athletes";
-import { IProTeam } from "./team";
+import { Gender } from './athletes';
+import { IProTeam } from './team';
 
 export type IFantasyTeamAthlete = {
-  id: number,
-  team_id: number | string,
-  athlete_id: string,
-  purchase_price: number,
-  purchase_date?: Date,
-  is_starting?: boolean,
-  slot: number,
-  score?: number,
-  tracking_id: string,
-  player_name: string,
-  nick_name?: string,
-  birth_country?: string,
-  date_of_birth?: Date,
-  abbr?: string,
-  athstat_name?: string,
-  athstat_firstname?: string,
-  athstat_lastname?: string,
-  athstat_middleinitial?: string,
-  athlete_team_id?: string,
-  height?: number,
-  weight?: number,
-  hidden?: boolean,
-  gender?: Gender,
-  price?: number,
-  power_rank_rating?: number,
-  region?: string,
-  position_class?: string,
-  position?: string,
-  image_url?: string,
-  team_name?: string,
-  team_logo?: string,
-  incrowed_team_id?: string,
-  incrowed_season_id?: string,
-  incrowed_competition_id?: string,
-  points_kicking?: number,
-  tackling?: number,
-  infield_kicking?: number,
-  strength?: number,
-  playmaking?: number,
-  ball_carrying?: number,
-  total_points?: number,
-  is_captain?: boolean,
-  is_super_sub?: boolean
+  id: number;
+  team_id: number | string;
+  athlete_id: string;
+  purchase_price: number;
+  purchase_date?: Date;
+  is_starting?: boolean;
+  slot: number;
+  score?: number;
+  tracking_id: string;
+  player_name: string;
+  nick_name?: string;
+  birth_country?: string;
+  date_of_birth?: Date;
+  abbr?: string;
+  athstat_name?: string;
+  athstat_firstname?: string;
+  athstat_lastname?: string;
+  athstat_middleinitial?: string;
+  athlete_team_id?: string;
+  height?: number;
+  weight?: number;
+  hidden?: boolean;
+  gender?: Gender;
+  price?: number;
+  power_rank_rating?: number;
+  region?: string;
+  position_class?: string;
+  position?: string;
+  image_url?: string;
+  team_name?: string;
+  team_logo?: string;
+  incrowed_team_id?: string;
+  incrowed_season_id?: string;
+  incrowed_competition_id?: string;
+  points_kicking?: number;
+  tackling?: number;
+  infield_kicking?: number;
+  strength?: number;
+  playmaking?: number;
+  ball_carrying?: number;
+  total_points?: number;
+  is_captain?: boolean;
+  is_super_sub?: boolean;
   team?: IProTeam;
-}
+};
 
 export interface IFantasyTeamSubmission {
   clubId: string;
@@ -69,8 +69,6 @@ export interface IFantasyClubTeam {
   balance?: number; // Team's available budget
 }
 
-
-
 export type IUpdateFantasyTeamAthleteItem = {
   id?: string;
   team_id: string;
@@ -80,7 +78,7 @@ export type IUpdateFantasyTeamAthleteItem = {
   is_starting: boolean;
   slot: number;
   score: number;
-}
+};
 
 export type ICreateFantasyTeamAthleteItem = {
   id?: string;
@@ -97,34 +95,39 @@ export type ICreateFantasyTeamAthleteItem = {
   athlete_team_id?: string;
   player_name?: string;
   is_captain?: boolean;
-}
+};
 
 export type AthleteWithTrackingId = {
-  tracking_id?: string
-}
+  tracking_id?: string;
+};
 
 /** Represents an athlete points breakdown item */
 export type FantasyAthletePointsBreakdownItem = {
-  pk?: string,
-  action: string,
-  action_count: number,
-  score?: number,
-  round: number,
-  game: string,
-  athlete: string
-}
+  pk?: string;
+  action: string;
+  action_count: number;
+  score?: number;
+  round: number;
+  game: string;
+  athlete: string;
+};
 
-type PlayerSquadAvailability = "AVAILABLE" | "TEAM_NOT_PLAYING";
+type PlayerSquadAvailability =
+  | 'AVAILABLE'
+  | 'TEAM_NOT_PLAYING'
+  | 'STARTING'
+  | 'BENCH'
+  | 'NOT_IN_SQUAD';
 
 export type SquadReportItem = {
-  athlete_id: string,
-  player_name: string,
-  image_url: string,
-  team_id: string,
-  team_name: string,
-  team_image_url: string,
-  game_id: string,
-  home_team_name: string,
-  away_team_name: string,
-  availability: PlayerSquadAvailability
-}
+  athlete_id: string;
+  player_name: string;
+  image_url: string;
+  team_id: string;
+  team_name: string;
+  team_image_url: string;
+  game_id: string;
+  home_team_name: string;
+  away_team_name: string;
+  availability: PlayerSquadAvailability;
+};
