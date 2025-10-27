08eb883950e25130124a1f090264584c43481ed7
Your GitHub Username <jabulani.kunene@athstat.com>
2025-10-27 15:51:55 +0200

    filtering athlete stats by current urc season id

--- src/services/powerRankingsService.ts
+++ src/services/powerRankingsService.ts
@@ -1,25 +1,29 @@
-import { SingleMatchPowerRanking } from "../types/powerRankings";
-import { getAuthHeader, getUri } from "../utils/backendUtils";
-import { logger } from "./logger"
+import { SingleMatchPowerRanking } from '../types/powerRankings';
+import { getAuthHeader, getUri } from '../utils/backendUtils';
+import { logger } from './logger';
 
 export const powerRankingsService = {
-    getPastMatchsPowerRankings: async (athleteId: string, limit?: number) => {
-        try {
-            const queryParam = limit ? `?limit=${limit}` : '';
-            const uri = getUri(`/api/v1/games/match-prs/athletes/${athleteId}${queryParam}`);
-            
-            const res = await fetch(uri, {
-                headers: getAuthHeader()
-            });
+  getPastMatchsPowerRankings: async (athleteId: string, limit?: number, seasonId?: string) => {
+    try {
+      const queryParams = [];
+      if (limit) queryParams.push(`limit=${limit}`);
+      if (seasonId) queryParams.push(`season_id=${seasonId}`);
 
-            const json = await res.json() as SingleMatchPowerRanking[];
-            console.log("Past Matches ", json);
+      const queryString = queryParams.length > 0 ? `?${queryParams.join('&')}` : '';
+      const uri = getUri(`/api/v1/games/match-prs/athletes/${athleteId}${queryString}`);
 
-            return json;
-        } catch (error) {
-            console.log("Error fetching past match prs ", error);
-            logger.error("Error fetching athlete matchs power rankings ", error);
-            return [];
-        }
+      const res = await fetch(uri, {
+        headers: getAuthHeader(),
+      });
+
+      const json = (await res.json()) as SingleMatchPowerRanking[];
+      console.log('Past Matches ', json);
+
+      return json;
+    } catch (error) {
+      console.log('Error fetching past match prs ', error);
+      logger.error('Error fetching athlete matchs power rankings ', error);
+      return [];
     }
-}
+  },
+};
