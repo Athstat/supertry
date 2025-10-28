fc25595dd7d4351245af7335ec787d6e6241b7c1
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-10-28 07:47:50 +0200

    adds hooks to fetch the data

---
+++ src/hooks/fantasy/useSportActionRanking.ts
@@ -0,0 +1,61 @@
+import useSWR from "swr";
+import { fantasySeasonsService } from "../../services/fantasy/fantasySeasonsService";
+import { useMemo } from "react";
+
+/** Hook for returning rankings based of a sports action */
+export function useSportActionRanking(seasonId: string, actionName: string, limit: number = 5) {
+    const key = `/fantasy-seasons/${seasonId}/sports-action-rankings/${actionName}?limit=${limit}`;
+    const { data, isLoading, mutate } = useSWR(key, () => fantasySeasonsService.getSportActionRankings(
+        seasonId,
+        actionName,
+        limit
+    ));
+
+    const rankings = useMemo(() => {
+        return (data ?? []);
+    }, [data]);
+
+    return {
+        rankings,
+        isLoading,
+        refresh: mutate
+    }
+}
+
+/** Hook for most selected player rankings based of a sports action */
+export function useMostSelectedPlayers(seasonId: string, limit: number = 5) {
+    const key = `/fantasy-seasons/${seasonId}/most-selected-players?limit=${limit}`;
+    const { data, isLoading, mutate } = useSWR(key, () => fantasySeasonsService.getMostSelectedPlayers(
+        seasonId,
+        limit
+    ));
+
+    const rankings = useMemo(() => {
+        return (data ?? []);
+    }, [data]);
+
+    return {
+        rankings,
+        isLoading,
+        refresh: mutate
+    }
+}
+
+/** Hook for most selected player rankings based of a sports action */
+export function useFantasyPointsRankings(seasonId: string, limit: number = 5) {
+    const key = `/fantasy-seasons/${seasonId}/fantasy-points-scored-rankings?limit=${limit}`;
+    const { data, isLoading, mutate } = useSWR(key, () => fantasySeasonsService.getFantasyPointsScoredRankings(
+        seasonId,
+        limit
+    ));
+
+    const rankings = useMemo(() => {
+        return (data ?? []);
+    }, [data]);
+
+    return {
+        rankings,
+        isLoading,
+        refresh: mutate
+    }
+}
