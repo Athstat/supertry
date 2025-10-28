7b6256ae256d52b0be6a08a501fd628089032b8f
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-10-28 07:42:19 +0200

    adds service items

--- src/services/fantasy/fantasySeasonsService.ts
+++ src/services/fantasy/fantasySeasonsService.ts
@@ -1,5 +1,5 @@
 import { IFantasySeason } from "../../types/fantasy/fantasySeason";
-import { FantasyLeagueGroup } from "../../types/fantasyLeagueGroups";
+import { FantasyLeagueGroup, FantasyPointsScoredRankingItem, MostSelectedRankingItem, PlayerSportActionRankingItem } from "../../types/fantasyLeagueGroups";
 import { getAuthHeader, getUri } from "../../utils/backendUtils";
 import { logger } from "../logger";
 
@@ -53,6 +53,62 @@ export const fantasySeasonsService = {
             }
         } catch (err) {
             logger.error('Error fetching featured fantasy league groups ', err);
+        }
+
+        return [];
+    },
+
+    /** Returns ranking of most selected players */
+    getMostSelectedPlayers: async (id: string, limit: number = 5) : Promise<MostSelectedRankingItem[]> => {
+        try {
+            const uri = getUri(`/api/v1/fantasy-seasons/${id}/player-rankings/by-most-selected?limit=${limit}`);
+            const res = await fetch(uri, {
+                headers: getAuthHeader()
+            })
+
+            if (res.ok) {
+                return (await res.json()) as MostSelectedRankingItem[];
+            }
+
+        } catch (err) {
+            logger.error('error fetching most seleted players ', err);
+        }
+
+        return [];
+    },
+
+    /** returns a ranking of players based on fantasy points scored */
+    getFantasyPointsScoredRankings: async (id: string, limit: number = 5) : Promise<FantasyPointsScoredRankingItem[]> => {
+        try {
+            const uri = getUri(`/api/v1/fantasy-seasons/${id}/player-rankings/by-points-scored?limit=${limit}`);
+            const res = await fetch(uri, {
+                headers: getAuthHeader()
+            })
+
+            if (res.ok) {
+                return (await res.json()) as FantasyPointsScoredRankingItem[];
+            }
+
+        } catch (err) {
+            logger.error('error fetching fantasy points score rankings ', err);
+        }
+
+        return [];
+    },
+
+    /** Returns player rankings for a specific sport action */
+    getSportActionRankings: async (id: string, actionName: string, limit: number = 5) : Promise<PlayerSportActionRankingItem[]> => {
+        try {
+            const uri = getUri(`/api/v1/fantasy-seasons/${id}/player-rankings/by-sports-action/${actionName}?limit=${limit}`);
+            const res = await fetch(uri, {
+                headers: getAuthHeader()
+            });
+
+            if (res.ok) {
+                return (await res.json()) as PlayerSportActionRankingItem[];
+            }
+        } catch (err) {
+            logger.error("Error fetching sport action ranking for ", actionName, err);
         }
 
         return [];
