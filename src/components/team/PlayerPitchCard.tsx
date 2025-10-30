3af0a8709aa11c59ae1763b86c1b008d8d53196f
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-10-30 20:51:56 +0200

    adds seperate player pitch card

---
+++ src/components/team/PlayerPitchCard.tsx
@@ -0,0 +1,72 @@
+import { twMerge } from "tailwind-merge";
+import { useAthleteRoundScore } from "../../hooks/useAthletePointsBreakdown";
+import { IFantasyLeagueRound } from "../../types/fantasyLeague";
+import { IFantasyTeamAthlete } from "../../types/fantasyTeamAthlete";
+import { formatPosition } from "../../utils/athleteUtils";
+import { isLeagueRoundLocked } from "../../utils/leaguesUtils";
+import PlayerMugshot from "../shared/PlayerMugshot";
+import SecondaryText from "../shared/SecondaryText";
+import { Activity } from "react";
+
+type PlayerPitchCardProps = {
+  player: IFantasyTeamAthlete,
+  onClick?: (player: IFantasyTeamAthlete) => void,
+  round: IFantasyLeagueRound
+}
+
+export function PlayerPitchCard({ player, onClick, round }: PlayerPitchCardProps) {
+
+  const { position_class } = player;
+  const isLocked = isLeagueRoundLocked(round);
+  const { isLoading, score } = useAthleteRoundScore(player.tracking_id, round.season_id, round.start_round ?? 0);
+
+  const handleClick = () => {
+    if (onClick) {
+      onClick(player);
+    }
+  }
+
+  return (
+    <div className='flex flex-col items-center justify-center gap-1' >
+      <div
+        className={twMerge(
+          'overflow-hidden rounded-xl min-h-[150px] max-h-[150px] bg-gradient-to-br from-green-500/30 to-green-500/60',
+          'min-w-[120px] max-w-[120px] flex flex-col'
+        )}
+        onClick={handleClick}
+      >
+
+        <div className='flex-3 flex overflow-clip flex-col items-center justify-center w-full' >
+          <PlayerMugshot
+            url={player.image_url}
+            className='border-none rounded-none w-[100px] h-[100px] bg-transparent hover:bg-transparent'
+            showPrBackground={false}
+          />
+
+          {/* <TeamJersey 
+          teamId={player.athlete_team_id}
+          className='border-none rounded-none w-[100px] h-[100px] bg-transparent hover:bg-transparent'
+        /> */}
+        </div>
+
+        <div className='flex-1 p-2 w-full items-center justify-center  min-h-[30%] rounded-xl bg-gradient-to-br from-white to-slate-200 dark:from-slate-800 dark:to-dark-900' >
+
+          <div className='flex flex-col items-center justify-center' >
+            <p className='text-slate-800 dark:text-white text-[11px] font-semibold' >{player.athstat_firstname}</p>
+          </div>
+
+          <div className='flex flex-col items-center justify-center' >
+            <SecondaryText className=' text-[10px]' >{position_class ? formatPosition(position_class) : ""}</SecondaryText>
+          </div>
+        </div>
+      </div>
+
+      <Activity mode={isLoading && isLocked ? 'visible' : 'hidden'}  >
+        <div>
+          <p className='font-bold' >{score.toFixed(1)}</p>
+        </div>
+      </Activity>
+      
+    </div>
+  )
+}
