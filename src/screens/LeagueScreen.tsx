5d2711287c2e4d0d1e1ee749f43400f7b5e9fbbb
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-10-30 13:57:47 +0200

    puts fantasy screen header into one file

--- src/screens/LeagueScreen.tsx
+++ src/screens/LeagueScreen.tsx
@@ -3,16 +3,12 @@ import FantasyLeagueGroupDataProvider from '../components/fantasy-league/provide
 import { useFantasyLeagueGroup } from '../hooks/leagues/useFantasyLeagueGroup';
 import PageView from './PageView';
 import { ErrorState } from '../components/ui/ErrorState';
-import { Globe, Share2, Trophy } from 'lucide-react';
 import { TabViewHeaderItem, TabViewPage } from '../components/shared/tabs/TabView';
 import { LeagueStandings } from '../components/fantasy-league/LeagueStandings';
 import LeagueInfoTab from '../components/fantasy-league/LeagueInfoTab';
 import LeagueFixturesTab from '../components/fantasy-league/LeagueFixturesTab';
-import JoinLeagueButton from '../components/fantasy-league/buttons/JoinLeagueButton';
 import LeagueCommissionerTab from '../components/fantasy-league/commissioner/LeagueCommissionerTab';
 import MyTeams from '../components/fantasy-leagues/MyTeams';
-import PrimaryButton from '../components/shared/buttons/PrimaryButton';
-import { useShareLeague } from '../hooks/leagues/useShareLeague';
 import { useQueryState } from '../hooks/useQueryState';
 import { useEffect, useState } from 'react';
 import LeagueOverviewTab from '../components/fantasy-league/overview/LeagueOverviewTab';
@@ -20,6 +16,7 @@ import PilledTabView from '../components/shared/tabs/PilledTabView';
 import LearnScrummyNoticeCard from '../components/branding/help/LearnScrummyNoticeCard';
 import { fantasyAnalytics } from '../services/analytics/fantasyAnalytics';
 import { useHideTopNavBar } from '../hooks/navigation/useNavigationBars';
+import LeagueGroupScreenHeader from '../components/fantasy-league/LeagueGroupScreenHeader';
 
 export function FantasyLeagueScreen() {
   const { leagueId } = useParams();
@@ -33,10 +30,10 @@ export function FantasyLeagueScreen() {
 
 function Content() {
 
+  /** Auto Hides Top Bar to Maximise screen space */
   useHideTopNavBar();
-  const { league, userMemberRecord, isMember, isOfficialLeague, currentRound } =
-    useFantasyLeagueGroup();
-  const { handleShare } = useShareLeague(league);
+
+  const { league, userMemberRecord } = useFantasyLeagueGroup();
 
   const [journey] = useQueryState('journey');
 
@@ -48,7 +45,7 @@ function Content() {
 
   useEffect(() => {
     fantasyAnalytics.trackVisitedLeagueScreen(league?.id);
-  }, []);
+  }, [league?.id]);
 
   if (!league && !league) {
     return <ErrorState error="Whoops" message="Fantasy League was not found" />;
@@ -86,36 +83,12 @@ function Content() {
     },
   ];
 
-  console.log('current round: ', currentRound);
-
   return (
     <PageView className="dark:text-white p-4 flex flex-col gap-4">
-      <div className="flex flex-col ">
-        <div className="flex flex-row items-center justify-between gap-2">
-          <div className="flex flex-col items-start gap-2">
-            <div className="flex flex-row items-center gap-2">
-              {isOfficialLeague ? <Globe /> : <Trophy />}
-              <p className="font-bold text-xl">{league?.title}</p>
-            </div>
-            <p className="text-sm text-gray-500 dark:text-gray-400 tracking-wide font-medium truncate">
-              {currentRound?.title}
-            </p>
-          </div>
-          {!isEditing && (
-            <div>
-              {!isMember && <JoinLeagueButton league={league} />}
-
-              {isMember && (
-                <PrimaryButton onClick={handleShare}>
-                  {/* <Plus className="w-4 h-4" /> */}
-                  <Share2 className="w-4 h-4" />
-                  Invite
-                </PrimaryButton>
-              )}
-            </div>
-          )}
-        </div>
-      </div>
+      
+      <LeagueGroupScreenHeader 
+        isEditing={isEditing}
+      />
 
       <LearnScrummyNoticeCard />
 
