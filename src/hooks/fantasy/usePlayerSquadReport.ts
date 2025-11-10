ef0a0db0980aeeb72df5e289cdd7e577a3218338
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-11-10 20:35:16 +0200

    adds hook to get player availability

--- src/hooks/fantasy/usePlayerSquadReport.ts
+++ src/hooks/fantasy/usePlayerSquadReport.ts
@@ -83,12 +83,87 @@ export function useGeneralPlayerAvailability(athleteId: string) {
     return firstReport?.status === "TEAM_NOT_PLAYING";
   }, [firstReport]);
 
+  const isPastGame = useMemo<boolean>(() => {
+    if (nextMatch) {
+      const dateNow = new Date();
+      const { kickoff_time } = nextMatch;
+
+      if (kickoff_time) {
+        const gameHours = 1000 * 60 * 60 * 3;
+        const gameAvgEnd = dateNow.valueOf() - gameHours;
+
+        return gameAvgEnd > new Date(kickoff_time).valueOf();
+      }
+    }
+
+    return false;
+  }, [nextMatch])
+
   return {
     report: firstReport,
     isLoading,
     nextMatch,
     isPending,
     isNotAvailable,
-    isTeamNotPlaying
+    isTeamNotPlaying,
+    isPastGame
+  };
+}
+
+
+/** Gets Player Specific Round Availability outside rosters */
+export function usePlayerRoundAvailability(athleteId: string, seasonId: string, roundNumber: number) {
+
+  const key = `/athlete/${athleteId}/general-availabilityby-season/${seasonId}/${roundNumber}`;
+  const { data, isLoading } = useSWR(key, () => djangoAthleteService.getRoundAvailabilityReport(
+    athleteId,
+    seasonId,
+    roundNumber
+  ));
+
+  const firstReport = useMemo(() => {
+    return data;
+  }, [data]);
+
+  const nextMatch = useMemo<IFixture | undefined>(() => {
+    return firstReport?.game;
+  }, [firstReport]);
+
+  const isPending = useMemo(() => {
+    return firstReport?.status === "PENDING";
+  }, [firstReport]);
+
+  const isNotAvailable = useMemo(() => {
+    return firstReport?.status === "NOT_AVAILABLE";
+  }, [firstReport]);
+
+  const isTeamNotPlaying = useMemo(() => {
+    return firstReport?.status === "TEAM_NOT_PLAYING";
+  }, [firstReport]);
+
+  const isPastGame = useMemo<boolean>(() => {
+    if (nextMatch) {
+      const dateNow = new Date();
+      const { kickoff_time } = nextMatch;
+
+      if (kickoff_time) {
+        const gameHours = 1000 * 60 * 60 * 3;
+        const gameAvgEnd = dateNow.valueOf() - gameHours;
+
+        return gameAvgEnd > new Date(kickoff_time).valueOf();
+      }
+    }
+
+    return false;
+  }, [nextMatch])
+
+  return {
+    report: firstReport,
+    isLoading,
+    nextMatch,
+    isPending,
+    isNotAvailable,
+    isTeamNotPlaying,
+    isPastGame
   };
 }
