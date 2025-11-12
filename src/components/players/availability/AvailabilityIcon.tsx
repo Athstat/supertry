676208e899bf8452534a3d228c7d5aa99745feb0
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-11-12 08:13:37 +0200

    adds report text

--- src/components/players/availability/AvailabilityIcon.tsx
+++ src/components/players/availability/AvailabilityIcon.tsx
@@ -8,6 +8,9 @@ import { useMemo } from "react"
 import { IProTeam } from "../../../types/team"
 import { isPastFixture } from "../../../utils/fixtureUtils"
 import { twMerge } from "tailwind-merge"
+import { IFantasyLeagueRound } from "../../../types/fantasyLeague"
+import { usePlayerRoundAvailability } from "../../../hooks/fantasy/usePlayerRoundAvailability"
+import { useFantasyLeagueGroup } from "../../../hooks/leagues/useFantasyLeagueGroup"
 
 type Props = {
     athlete: IProAthlete | IFantasyAthlete | IFantasyTeamAthlete,
@@ -101,7 +104,7 @@ export function AvailabilityText({ athlete, className }: Props) {
             className
         )} >
             <TriangleAlert className="min-w-5  min-h-5" />
-            
+
             {report.status == "NOT_AVAILABLE" && (<p className="text-xs" >
                 {athlete.player_name} {isPast ? 'was' : 'is'} not on the team roster for the match
                 against <a href={`/fixtures/${report.game?.game_id}`} className="underline cursor-pointer text-primary-500" >
@@ -112,10 +115,82 @@ export function AvailabilityText({ athlete, className }: Props) {
             </p>)}
 
             {report.status == "TEAM_NOT_PLAYING" && (<p className="text-xs" >
-                {athlete.player_name}'s {isPast ? 'was' : 'is'} team is not playing in this round 
+                {athlete.player_name}'s {isPast ? 'was' : 'is'} team is not playing in this round
                 {!isPast && ' Consider taking action if he is in your team'}
             </p>)}
         </WarningCard>
     )
 }
 
+type RoundProps = Props & {
+    round: IFantasyLeagueRound
+}
+
+
+export function RoundAvailabilityText({ athlete, className, round }: RoundProps) {
+
+    const { league } = useFantasyLeagueGroup();
+    const { report, isLoading } = usePlayerRoundAvailability(
+        athlete.tracking_id,
+        league?.season_id ?? "",
+        round.start_round ?? 0
+    );
+
+    const opposition = useMemo<IProTeam | undefined>(() => {
+
+        if (!report) return undefined;
+
+        const { game } = report;
+        const athelteTeamId = athlete.team_id;
+
+        if (athelteTeamId !== game?.team?.athstat_id) {
+            return game?.team;
+        }
+
+        return game?.opposition_team;
+    }, [report, athlete]);
+
+    if (isLoading) {
+        return;
+    }
+
+    if (!report) {
+        return;
+    }
+
+    const notAvailable = Boolean(report.game) && report?.status === "NOT_AVAILABLE";
+    const isPast = isPastFixture(report.game)
+
+    if (!notAvailable) {
+        return;
+    }
+
+    // const handleViewGame = () => {
+    //     navigate(`/fixtures/${report.game?.game_id}`);
+    // }
+
+    return (
+        <WarningCard className={twMerge(
+            'text-xs',
+            className,
+        )} >
+            <TriangleAlert className="min-w-5  min-h-5" />
+
+            {report.status == "NOT_AVAILABLE" && (<p className="text-xs" >
+                {athlete.player_name} {isPast ? 'was' : 'is'} not on the team roster for the match
+                against <a href={`/fixtures/${report.game?.game_id}`} className="underline cursor-pointer text-primary-500" >
+                    <strong>{opposition?.athstat_name}</strong>
+                </a>
+                .
+                {!isPast && 'Consider taking action if he is in your team'}
+            </p>)}
+
+
+
+            {report.status == "TEAM_NOT_PLAYING" && (<p className="text-xs" >
+                {athlete.player_name}'s {isPast ? 'was' : 'is'} team is not playing in this round
+                {!isPast && ' Consider taking action if he is in your team'}
+            </p>)}
+        </WarningCard>
+    )
+}
