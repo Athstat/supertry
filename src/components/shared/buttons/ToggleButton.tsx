1750606501275d8789bae30ede6cbeadac9ff7fb
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-11-20 01:45:19 +0200

    adds UI

--- src/components/shared/buttons/ToggleButton.tsx
+++ src/components/shared/buttons/ToggleButton.tsx
@@ -5,7 +5,7 @@ type Props = {
     onChange?: (newVal: boolean) => void
 }
 
-/** Renders Toogle Button */
+/** Renders Toggle Button */
 export default function ToggleButton({ value, onChange }: Props) {
 
     const handleOnClick = () => {
@@ -17,16 +17,18 @@ export default function ToggleButton({ value, onChange }: Props) {
     return (
         <button
             onClick={handleOnClick}
-            className="border-[3px] w-[60px] h-[30px] dark:border-slate-300 rounded-xl overflow-clip"
+            className={twMerge(
+                "w-[60px] h-[30px] rounded-full transition-colors duration-300 border border-slate-100 dark:border-slate-500 flex flex-row items-center",
+                value ? "bg-blue-500 dark:border-blue-400 " : "bg-gray-300 dark:bg-slate-600"
+            )}
         >
             {/* Handle */}
             <div
                 className={twMerge(
-                    'w-[30px] bg-blue-500 h-[30px] rounded-xl'
+                    'w-[30px] h-[25px] bg-white  rounded-full transition-transform duration-300',
+                    value ? "translate-x-[25px]" : "translate-x-[2px]"
                 )}
-            >
-
-            </div>
+            />
         </button>
     )
 }
--- src/screens/myaccount/NotificationPreferencesScreen.tsx
+++ src/screens/myaccount/NotificationPreferencesScreen.tsx
@@ -29,10 +29,32 @@ export default function NotificationPreferencesScreen() {
                 </div>
             </div>
 
-            <div className="flex flex-col w-full gap-2" >
-                <TogglableSettingCard 
+            <div className="flex flex-col w-full gap-4" >
+                <TogglableSettingCard
                     title="Receive Notifications"
-                    description="Allow SCRUMMY to send notifications to you"
+                    description="Allow SCRUMMY to send notifications"
+                />
+
+                <TogglableSettingCard
+                    title="Game Updates"
+                    description="Receive updates e.g scores, tries when a game is live"
+                    value={true}
+                />
+
+                <TogglableSettingCard
+                    title="Roster Updates"
+                    description={
+                        "Receive updates when game rosters become available, before kickoff"
+                    }
+                    value={true}
+                />
+
+                <TogglableSettingCard
+                    title="News"
+                    description={
+                        "Get news updates from the world of rugby"
+                    }
+                    value={false}
                 />
             </div>
         </PageView>
@@ -46,16 +68,16 @@ type SettingProp = {
     onChange?: (newVal: boolean) => void
 }
 
-function TogglableSettingCard({title, description, value, onChange} : SettingProp) {
+function TogglableSettingCard({ title, description, value, onChange }: SettingProp) {
     return (
         <div className="flex flex-row items-center gap-2 justify-between" >
             <div>
                 <p>{title}</p>
-                <SecondaryText className="text-xs text-wrap" >{description}</SecondaryText>
+                <SecondaryText className="text-xs text-wrap max-w-[95%]" >{description}</SecondaryText>
             </div>
 
             <div>
-                <ToggleButton 
+                <ToggleButton
                     value={value}
                     onChange={onChange}
                 />
