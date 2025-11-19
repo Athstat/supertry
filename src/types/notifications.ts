4f4d9406843c9e91a874a1ce2591f4d5a465b122
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-11-20 01:09:58 +0200

    adds service methods for notification settings

--- src/types/notifications.ts
+++ src/types/notifications.ts
@@ -6,3 +6,26 @@ export const gameUpdatesPreferenceOptions: GameUpdatesPreference[] = [
     "all-updates",
     "results-only"
 ];
+
+export type NotificationProfile = {
+    user_id: string,
+    receive_notifications_enabled?: boolean,
+    game_updates_enabled?: boolean,
+    game_roster_updates_enabled?: boolean,
+    news_updates_enabled?: boolean,
+    my_team_updates_enabled?: boolean,
+    email_updates_enabled?: boolean,
+    created_at: Date,
+    updated_at: Date,
+    game_updates_preference: GameUpdatesPreference
+}
+
+export type UpdateNotificationProfileReq = {
+    receive_notifications_enabled: boolean,
+    game_updates_enabled: boolean,
+    game_roster_updates_enabled: boolean,
+    news_updates_enabled: boolean,
+    my_team_updates_enabled: boolean,
+    email_updates_enabled: boolean,
+    game_updates_preference: GameUpdatesPreference
+}
