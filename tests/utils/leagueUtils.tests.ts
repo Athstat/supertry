fa1b61336f3bfe2d43b38e17214b202a415f8863
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-11-24 20:03:52 +0200

    removes server work around

--- tests/utils/leagueUtils.tests.ts
+++ tests/utils/leagueUtils.tests.ts
@@ -1,4 +1,4 @@
-import { add, addDays, addHours, addMinutes } from "date-fns";
+import { add, addDays, addMinutes } from "date-fns";
 import { isLeagueLocked } from "../../src/utils/leaguesUtils";
 
 test('test is league locked when join deadline has passed', () => {
@@ -23,27 +23,6 @@ test('test is league locked when join deadline in equal to time now', () => {
     const joinDeadline = addMinutes(now, 1);
 
     expect(isLeagueLocked(joinDeadline)).toEqual(false);
-
-});
-
-test('test is league locked when its 30 minutes before the first match', () => {
-    // This problem is something we eventually want to fix, but this test
-    // serves to make sure the work around isn't broken
-    // sincerely Tadiwa
-
-    /** 
-        The Problem here is that the join deadline is actually set two hours
-        before the game on the backend but on the front-end we lock the leagues
-        30 minutes before the first match
-    */
-
-    const now = new Date();
-    const joinDeadline = add(now, {
-        minutes: 30,
-        hours: 1
-    });
-
-    expect(isLeagueLocked(joinDeadline)).toEqual(true);
 
 });
 
