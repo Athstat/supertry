6a0a7fc45560729ffc99e141f38f0fb6c054f96e
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-11-26 12:17:16 +0200

    fixes typescript errros

--- src/state/season.atoms.ts
+++ src/state/season.atoms.ts
@@ -1,10 +1,11 @@
 /** Atoms for a season */
 
 import { atom } from "jotai";
-import { IFixture, ISeason, ITeam } from "../types/games";
+import { IFixture, ITeam } from "../types/games";
 import { IProAthlete } from "../types/athletes";
+import { IProSeason } from "../types/season";
 
-export const seasonAtom = atom<ISeason>();
+export const seasonAtom = atom<IProSeason>();
 export const seasonFixtutesAtoms = atom<IFixture[]>([]);
 export const seasonAthletesAtoms = atom<IProAthlete[]>([]);
 export const seasonTeamsAtoms = atom<ITeam[]>([])
