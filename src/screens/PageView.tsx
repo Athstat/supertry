2d436fda67daf502a0b3340ea4bc16de49df58ca
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-11-28 17:27:38 +0200

    fixes page view

--- src/screens/PageView.tsx
+++ src/screens/PageView.tsx
@@ -27,50 +27,46 @@ type TopicPageViewProps = Props & {
 
 export function TopicPageView({ children, title, statsCards, className }: TopicPageViewProps) {
 
-    const {hardPop} = useNavigateBack();
+  const { hardPop } = useNavigateBack();
 
-    const handleGoBack = () => {
-        hardPop("/dashboard");
-    }
+  const handleGoBack = () => {
+    hardPop("/dashboard");
+  }
 
-    return (
-        <div>
-            <BlueGradientCard className="rounded-none p-4 py-8 lg:py-10 min-h-32 items-center justify-center flex flex-col gap-2" >
-                <div className="lg:w-1/2 w-full flex flex-col gap-4" >
-                    <button
-                        onClick={handleGoBack}
-                        className="flex items-center text-primary-100 hover:text-white transition-colors"
-                    >
-                        <ChevronLeft size={20} />
-                        <span>
-                            {"Go Back"}
-                        </span>
-                    </button>
-                    <p className="text-xl font-extrabold" >{title}</p>
+  return (
+    <div>
+      <BlueGradientCard className="rounded-none p-4 py-8 lg:py-10 min-h-32 items-center justify-center flex flex-col gap-2" >
+        <div className="lg:w-1/2 w-full flex flex-col gap-4" >
+          <button
+            onClick={handleGoBack}
+            className="flex items-center text-primary-100 hover:text-white transition-colors"
+          >
+            <ChevronLeft size={20} />
+            <span>
+              {"Go Back"}
+            </span>
+          </button>
+          <p className="text-xl font-extrabold" >{title}</p>
 
-                    {statsCards && <div className="grid grid-cols-2 w-full gap-2" >
+          {statsCards && <div className="grid grid-cols-2 w-full gap-2" >
 
-                        {statsCards.map((card, index) => {
-                            return (
-                                <TransparentStatCard
-                                    label={card.title}
-                                    value={card.value}
-                                    key={index}
-                                />
-                            )
-                        })}
+            {statsCards.map((card, index) => {
+              return (
+                <TransparentStatCard
+                  label={card.title}
+                  value={card.value}
+                  key={index}
+                />
+              )
+            })}
 
-                    </div>}
-                </div>
-            </BlueGradientCard>
-
-            <PageView className={className} >
-                {children}
-            </PageView>
+          </div>}
         </div>
       </BlueGradientCard>
 
-      <PageView className={className}>{children}</PageView>
+      <PageView className={className} >
+        {children}
+      </PageView>
     </div>
   );
 }
