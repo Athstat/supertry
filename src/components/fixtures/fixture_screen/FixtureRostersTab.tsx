7af9270146abbdda1bcc5a28e80d2376a3b6a31a
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-11-28 08:19:20 +0200

    adds fixes to roster tab

--- src/components/fixtures/fixture_screen/FixtureRostersTab.tsx
+++ src/components/fixtures/fixture_screen/FixtureRostersTab.tsx
@@ -6,6 +6,7 @@ import { RugbyPitch3D } from "../../shared/RugbyPitch"
 import { useGameRosters } from "../../../hooks/fixtures/useGameRosters"
 import PlayerMugshot from "../../shared/PlayerMugshot"
 import TeamJersey from "../../player/TeamJersey"
+import { twMerge } from "tailwind-merge"
 
 type Props = {
     fixture: IFixture
@@ -60,6 +61,8 @@ function TeamRosterPitchView({ team, fixture }: TeamRosterProps) {
         return teamRoster.find((r) => r.player_number === jersey);
     }, [teamRoster]);
 
+    const hasRosterItems = teamRoster.length >= 15;
+
     if (isLoading) {
         return (
             <div className="mt-4 relative animate-pulse" >
@@ -70,87 +73,100 @@ function TeamRosterPitchView({ team, fixture }: TeamRosterProps) {
 
     return (
         <div className="mt-4 relative" >
-            
-            <RugbyPitch3D className="max-h-[500px]" />
 
-            <div className="absolute mt-2 w-full  top-0 left-0 flex flex-col gap-2" >
-                <div className="flex w-full flex-row items-center justify-center gap-3" >
-                    <RosterItem
-                        item={getByJersey(1)}
-                    />
+            <RugbyPitch3D className={twMerge(
+                "max-h-[500px]",
+                !hasRosterItems && "opacity-20"
+            )} />
 
-                    <RosterItem
-                        item={getByJersey(2)}
-                    />
-
-                    <RosterItem
-                        item={getByJersey(3)}
-                    />
-                </div>
-
-                <div className="flex w-full flex-row items-center justify-center gap-4" >
-                    <RosterItem
-                        item={getByJersey(6)}
-                    />
-
-                    <RosterItem
-                        item={getByJersey(4)}
-                    />
-
-                    <div className="mt-4" >
+            <Activity mode={hasRosterItems ? "visible" : "hidden"} >
+                <div className="absolute mt-2 w-full  top-0 left-0 flex flex-col gap-2" >
+                    <div className="flex w-full flex-row items-center justify-center gap-3" >
                         <RosterItem
-                            item={getByJersey(8)}
+                            item={getByJersey(1)}
+                        />
+
+                        <RosterItem
+                            item={getByJersey(2)}
+                        />
+
+                        <RosterItem
+                            item={getByJersey(3)}
                         />
                     </div>
 
-                    <RosterItem
-                        item={getByJersey(5)}
-                    />
-
-
-                    <RosterItem
-                        item={getByJersey(7)}
-                    />
-                </div>
-
-                <div className="flex w-full flex-row items-center justify-center gap-4" >
-
-
-                    <RosterItem
-                        item={getByJersey(10)}
-                    />
-
-                    <div className="mb-6" >
+                    <div className="flex w-full flex-row items-center justify-center gap-4" >
                         <RosterItem
-                            item={getByJersey(9)}
+                            item={getByJersey(6)}
+                        />
+
+                        <RosterItem
+                            item={getByJersey(4)}
+                        />
+
+                        <div className="mt-4" >
+                            <RosterItem
+                                item={getByJersey(8)}
+                            />
+                        </div>
+
+                        <RosterItem
+                            item={getByJersey(5)}
+                        />
+
+
+                        <RosterItem
+                            item={getByJersey(7)}
                         />
                     </div>
 
-                    <RosterItem
-                        item={getByJersey(12)}
-                    />
-                </div>
+                    <div className="flex w-full flex-row items-center justify-center gap-4" >
 
-                <div className="flex w-full flex-row items-center justify-center gap-4" >
-                    <RosterItem
-                        item={getByJersey(11)}
-                    />
 
-                    <RosterItem
-                        item={getByJersey(13)}
-                    />
-
-                    <div className="mt-6" >
                         <RosterItem
-                            item={getByJersey(15)}
+                            item={getByJersey(10)}
+                        />
+
+                        <div className="mb-6" >
+                            <RosterItem
+                                item={getByJersey(9)}
+                            />
+                        </div>
+
+                        <RosterItem
+                            item={getByJersey(12)}
                         />
                     </div>
 
-                    <RosterItem
-                        item={getByJersey(14)}
-                    />
+                    <div className="flex w-full flex-row items-center justify-center gap-4" >
+                        <RosterItem
+                            item={getByJersey(11)}
+                        />
+
+                        <RosterItem
+                            item={getByJersey(13)}
+                        />
+
+                        <div className="mt-6" >
+                            <RosterItem
+                                item={getByJersey(15)}
+                            />
+                        </div>
+
+                        <RosterItem
+                            item={getByJersey(14)}
+                        />
+                    </div>
                 </div>
-            </div>
+            </Activity>
+
+            <Activity mode={hasRosterItems ? "hidden" : "visible"} >
+                <div className="w-full h-[300px] flex flex-col items-center justify-center absolute top-0 left-0" >
+                    <div>
+                        <p>Rosters for {team.athstat_name} are not yet available</p>
+                    </div>
+                </div>
+            </Activity>
         </div>
     )
 }
@@ -161,13 +177,16 @@ type RosterItemProps = {
 
 function RosterItem({ item }: RosterItemProps) {
     return (
-        <div>
+        <div
+            key={item?.athlete.tracking_id}
+        >
             {item && <div className="flex flex-col items-center justify-center" >
 
                 <Activity mode={item.athlete.image_url ? "visible" : "hidden"} >
                     <PlayerMugshot
                         url={item.athlete.image_url}
                         teamId={item.team_id}
+                        className="border-2 border-green-500 dark:border-green-500/40 bg-green-600 w-16 h-16 hover:bg-green-400"
                     />
                 </Activity>
 
@@ -175,7 +194,7 @@ function RosterItem({ item }: RosterItemProps) {
                     <div className="max-w-16 bg-green-600 max-h-16 min-w-16 min-h-16 flex flex-col items-center rounded-full overflow-hidden border-2 dark:border-green-500/20 " >
                         <TeamJersey
                             teamId={item.athlete.team_id}
-                            className="w-full max-h-15 max-w-15 min-h-15 min-w-15 object-center mt-3 h-full"
+                            className="w-full max-h-[60px] max-w-[60px] min-h-[60px] min-w-[60px] object-center mt-3 h-full"
                             hideFade
                         />
                     </div>
