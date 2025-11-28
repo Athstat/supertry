cafb621719dd14f9adad147cbc232de13d5d6fd9
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-11-28 12:47:09 +0200

    adds hook for fixture screen

---
+++ src/hooks/fixtures/useFixture.ts
@@ -0,0 +1,38 @@
+import { useAtom, useAtomValue } from "jotai";
+import { fixtureAtom, fixtureSelectedPlayerAtom, showPlayerMatchModalAtom, showPlayerProfileAtom } from "../../state/fixtures/fixture.atoms";
+import { IProAthlete } from "../../types/athletes";
+
+export function useFixtureScreen() {
+    const fixture = useAtomValue(fixtureAtom);
+    const [selectedPlayer, setSelectedPlayer] = useAtom(fixtureSelectedPlayerAtom);
+    const [showProfileModal, setShowProfileModal] = useAtom(showPlayerProfileAtom);
+    const [showPlayerMatchModal, setShowPlayerMatchModal] = useAtom(showPlayerMatchModalAtom);
+
+    const openPlayerMatchModal = (player: IProAthlete) => {
+        setShowProfileModal(false);
+        setSelectedPlayer(player);
+        setShowPlayerMatchModal(true);
+    }
+
+    const closePlayerMatchModal = () => {
+        setShowPlayerMatchModal(false);
+        setSelectedPlayer(undefined);
+        setShowPlayerMatchModal(false);
+    }
+
+    const openPlayerProfileModal = (player: IProAthlete) => {
+        setSelectedPlayer(player);
+        setShowProfileModal(true);
+    }
+
+
+    const closePlayerProfileModal = () => {
+        setShowProfileModal(true);
+    }
+
+    return {
+        fixture, selectedPlayer, showPlayerMatchModal, openPlayerProfileModal,
+        closePlayerProfileModal, closePlayerMatchModal, openPlayerMatchModal,
+        showProfileModal
+    }
+}
--- src/state/fixtures/fixture.atoms.ts
+++ src/state/fixtures/fixture.atoms.ts
@@ -7,3 +7,9 @@ export const fixtureAtom = atom<IFixture>();
 
 /* Holds the selected athlete on fixture screen */
 export const fixtureSelectedPlayerAtom = atom<IProAthlete>();
+
+/** Holds a true or false value whether the player profile modal should be shown or not */
+export const showPlayerProfileAtom = atom<boolean>(false);
+
+/** Holds a true or false value whether the player match modal should be shown */
+export const showPlayerMatchModalAtom = atom<boolean>(false);
