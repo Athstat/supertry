d8a0b4b0ec56389aa617f6624be641be8e72bb40
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-12-02 22:04:48 +0200

    adds player iem on top position player cards

--- src/components/players/positioning/TopPositionPlayers.tsx
+++ src/components/players/positioning/TopPositionPlayers.tsx
@@ -1,6 +1,10 @@
 import { useMemo } from "react";
 import { useSupportedAthletes } from "../../../hooks/athletes/useSupportedAthletes"
-import { PositionClass } from "../../../types/athletes";
+import { IProAthlete, PositionClass } from "../../../types/athletes";
+import SecondaryText from "../../shared/SecondaryText";
+import { ArrowRight } from "lucide-react";
+import RoundedCard from "../../shared/RoundedCard";
+import { PlayerGameCard } from "../../player/PlayerGameCard";
 
 type Props = {
     positionClass?: PositionClass,
@@ -11,10 +15,10 @@ type Props = {
 }
 
 /** Renders a card that shows the top players in that position, using the athlete provider */
-export default function TopPositionPlayers({ positionClass, positionName }: Props) {
+export default function TopPositionPlayers({ positionClass, positionName, title, description, showViewMoreButton }: Props) {
 
-    const {athletes} = useSupportedAthletes();
-    
+    const { athletes } = useSupportedAthletes();
+
     const sortedPlayers = useMemo(() => {
         const positionPlayers = athletes.filter((a) => {
             return a.position_class === positionClass || a.position === positionName;
@@ -31,20 +35,47 @@ export default function TopPositionPlayers({ positionClass, positionName }: Prop
     }, [sortedPlayers]);
 
     return (
-        <div>
-            <p>{positionName}</p>
-            <div>
+        <div className="flex flex-col  gap-2" >
+            <div className="flex flex-row items-center justify-between" >
+                <div>
+                    <p>{title}</p>
+                    <SecondaryText>{description}</SecondaryText>
+                </div>
+
+                {showViewMoreButton && (
+                    <div className="flex text-slate-600 dark:text-slate-400 flex-row items-center gap-2" >
+                        <SecondaryText>View All</SecondaryText>
+                        <ArrowRight className="w-4 h-4" />
+                    </div>
+                )}
+
+            </div>
+            <div className="flex flex-row items-center gap-2 overflow-y-auto no-scrollbar" >
                 {top5.map((a) => {
                     return (
-                        <div
+                        <PlayerItem
+                            player={a}
                             key={a.tracking_id}
-                        >
-                            <p>{a.player_name}</p>
-                            <p>{a.power_rank_rating}</p>
-                        </div>
+                        />
                     )
                 })}
             </div>
+        </div>
+    )
+}
+
+type PlayerItemProps = {
+    player: IProAthlete
+}
+
+function PlayerItem({ player }: PlayerItemProps) {
+    return (
+        <div className=" dark:border-none flex flex-col items-center" >
+            {/* <PlayerGameCard 
+                player={player}
+            /> */}
+
+            
         </div>
     )
 }
--- src/screens/players/PlayersOverviewScreen.tsx
+++ src/screens/players/PlayersOverviewScreen.tsx
@@ -2,6 +2,7 @@ import { ArrowRight, Users } from 'lucide-react'
 import PageView from '../PageView'
 import SearchInput from '../../components/shared/forms/SearchInput'
 import RoundedCard from '../../components/shared/RoundedCard'
+import TopPositionPlayers from '../../components/players/positioning/TopPositionPlayers'
 // import { useNavigate } from 'react-router-dom'
 
 export default function PlayersOverviewScreen() {
@@ -26,8 +27,8 @@ export default function PlayersOverviewScreen() {
       </div>
 
       <div>
-        <RoundedCard 
-          className='flex cursor-pointer py-2 px-4 dark:border-none flex-row items-center gap-2 justify-between' 
+        <RoundedCard
+          className='flex cursor-pointer py-2 px-4 dark:border-none flex-row items-center gap-2 justify-between'
           onClick={handleViewAll}
         >
           <p className='text-sm' >View All Players</p>
@@ -37,6 +38,25 @@ export default function PlayersOverviewScreen() {
         </RoundedCard>
       </div>
 
+      <div className='flex flex-col gap-4' >
+        <TopPositionPlayers
+          positionClass='front-row'
+          title='Front Row'
+          showViewMoreButton
+        />
+
+        <TopPositionPlayers
+          positionClass='back-row'
+          title='Back Row'
+          showViewMoreButton
+        />
+
+        <TopPositionPlayers
+          positionClass='half-back'
+          title='Half Back'
+          showViewMoreButton
+        />
+      </div>
 
     </PageView>
   )
