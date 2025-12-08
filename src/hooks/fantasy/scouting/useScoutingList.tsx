a743a834a5047f10a6cb29a7e83649e20c3867c8
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-12-08 14:24:41 +0200

    adds scouting list api

---
+++ src/hooks/fantasy/scouting/useScoutingList.tsx
@@ -0,0 +1,84 @@
+import { useCallback, useMemo, useState } from "react";
+import { logger } from "../../../services/logger";
+import { scoutingService } from "../../../services/fantasy/scoutingService";
+import useSWR from "swr";
+
+/** API for getting a users scouting list and manipulating it */
+export function useScoutingList() {
+
+    const key = `/fantasy/scouting/my-list`;
+    const {data, isLoading: loadingList} = useSWR(key, () => scoutingService.getUserList());
+
+    const [isAdding, setIsAdding] = useState(false);
+    const [isRemoving, setIsRemoving] = useState(false);
+
+    const [error, setError] = useState<string>();
+    const [message, setMessage] = useState<string>();
+
+    const list = useMemo(() => {
+        return data ?? [];
+    }, [data]);
+
+    const addPlayer = useCallback(async (athleteId: string, callback?: () => void) => {
+
+        setIsAdding(true);
+        setMessage(undefined);
+        setError(undefined);
+
+        try {
+            const res = await scoutingService.addPlayer(athleteId);
+
+            if (res) {
+                setMessage("Player Added to Scouting List");
+                if (callback) callback();
+            }
+
+        } catch (err) {
+            logger.error("Error adding player ", err);
+            setError("Something wen't wrong");
+        } finally {
+            setIsAdding(false);
+        }
+
+    }, [setIsAdding, setMessage, setError]);
+
+    const removePlayer = useCallback(async (athleteId: string, callback?: () => void) => {
+
+        setIsRemoving(true);
+        setMessage(undefined);
+        setError(undefined)
+
+        try {
+            await scoutingService.removePlayer(athleteId);
+            setMessage("Removed Player from scouting list");
+
+            if (callback) callback();
+        } catch (err) {
+            logger.error("Error adding player ", err);
+            setError("Something wen't wrong");
+        } finally {
+            setIsAdding(false);
+        }
+
+    }, [setIsAdding, setMessage, setError]);
+
+    const clearError = () => {
+        setError(undefined);
+    }
+
+    const clearMessage = () => {
+        setMessage(undefined);
+    }
+
+    return {
+        addPlayer,
+        removePlayer,
+        isAdding,
+        isRemoving,
+        error,
+        message,
+        clearError,
+        clearMessage,
+        list,loadingList
+    }
+}
