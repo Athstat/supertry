514f81087a9d8f47278959694cbcaed9d454934e
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-12-10 15:21:07 +0200

    works on the colour picker

--- src/components/player-picker/PlayerPickerPlayerList.tsx
+++ src/components/player-picker/PlayerPickerPlayerList.tsx
@@ -1,4 +1,4 @@
-import { Fragment, useCallback, useMemo, useState } from "react";
+import { Activity, useCallback, useMemo, useState } from "react";
 import { usePlayerPicker } from "../../hooks/playerPicker/usePlayerPicker";
 import { IProAthlete } from "../../types/athletes";
 import RoundedCard from "../shared/RoundedCard";
@@ -9,10 +9,12 @@ import { seasonService } from "../../services/seasonsService";
 import TeamJersey from "../player/TeamJersey";
 import { twMerge } from "tailwind-merge";
 import { useInView } from "react-intersection-observer";
-import { BadgeInfo, ChevronsUpDown } from "lucide-react";
+import { ChevronsUpDown, Plus } from "lucide-react";
 import WarningCard from "../shared/WarningCard";
 import PlayerProfileModal from "../player/PlayerProfileModal";
 import AvailabilityIcon from "../players/availability/AvailabilityIcon";
+import MatchPrCard from "../rankings/MatchPrCard";
+import PrimaryButton from "../shared/buttons/PrimaryButton";
 
 
 type SortField = 'power_rank_rating' | 'price' | null;
@@ -171,53 +173,54 @@ export default function PlayerPickerPlayerList({ onSelect }: Props) {
     }
 
     return (
-        <div className="" >
+        <div className="overflow-hidden" >
 
             <WarningCard className="text-xs" >
                 <ChevronsUpDown className="w-4 h-4" />
                 <p>Players Sorted By <strong>{sortField === "power_rank_rating" ? "Power Ranking" : "Price"}</strong> in <strong>{sortDirection === "asc" ? "from Lowest to Highest" : "Highest to Lowest"}</strong>.</p>
             </WarningCard>
 
-            <div className="flex font-semibold py-2 flex-row items-center justify-between" >
+            <table className="w-full table-fixed" >
+                <thead>
+                    <tr className="cursor-pointer" >
+                        <th className="w-[60%]" >
+                            <div className="flex flex-row items-center gap-1" >
+                                <SecondaryText className="text-xs" >Player</SecondaryText>
+                            </div>
+                        </th>
+                        <th onClick={() => handleSort("price")} className="w-[14%]" >
 
-                <div className="flex flex-row  items-center justify-start gap-2 min-w-[230px]" >
-                    <SecondaryText className="max-w-[30px]" >Info</SecondaryText>
-                    <SecondaryText>Player</SecondaryText>
-                </div>
+                            <div className="flex flex-row items-center gap-1" >
+                                <SecondaryText className="text-xs" >Price</SecondaryText>
+                                <ChevronsUpDown className="w-3 h-3" />
+                            </div>
 
-                <div className="grid grid-cols-2 w-[150px] gap-4" >
-                    <div
-                        className="flex items-center gap-1 cursor-pointer hover:opacity-70 transition-opacity"
-                        onClick={() => handleSort('power_rank_rating')}
-                    >
-                        <SecondaryText>
-                            PR
-                        </SecondaryText>
-                        <ChevronsUpDown className="w-4 h-4 text-slate-700 dark:text-slate-400" />
-                    </div>
+                        </th>
 
-                    <div
-                        className="flex items-center gap-1 cursor-pointer hover:opacity-70 transition-opacity"
-                        onClick={() => handleSort('price')}
-                    >
-                        <SecondaryText>Price</SecondaryText>
-                        <ChevronsUpDown className="w-4 h-4 text-slate-700 dark:text-slate-400" />
-                    </div>
-                </div>
-            </div>
+                        <th onClick={() => handleSort("power_rank_rating")} className="w-[11%]" >
+                            <div className="flex flex-row items-center gap-1" >
+                                <SecondaryText className="text-xs" >PR</SecondaryText>
+                                <ChevronsUpDown className="w-3 h-3" />
+                            </div>
+                        </th>
+                        <th className="w-[13%]" ><SecondaryText className="text-xs" >Add</SecondaryText></th>
+                    </tr>
+                </thead>
 
-            <div className="flex flex-col divide-y dark:divide-slate-700" >
-                {filteredAthletes.map((a) => {
-                    return (
-                        <PlayerListItem
-                            player={a}
-                            key={a.tracking_id}
-                            onViewPlayerProfile={handleViewPlayerProfile}
-                            onSelectPlayer={onSelect}
-                        />
-                    )
-                })}
-            </div>
+                <tbody>
+                    {filteredAthletes.map((a) => {
+                        return (
+                            <PlayerListItem
+                                player={a}
+                                key={a.tracking_id}
+                                onViewPlayerProfile={handleViewPlayerProfile}
+                                onSelectPlayer={onSelect}
+                            />
+                        )
+                    })}
+                </tbody>
+
+            </table>
 
             {showModal && profileModalPlayer && (
                 <PlayerProfileModal
@@ -263,67 +266,62 @@ function PlayerListItem({ player, onViewPlayerProfile, onSelectPlayer }: PlayerL
     }, [onSelectPlayer, player, isAffordable]);
 
     return (
-        <div ref={ref} className={twMerge(
-            "flex hover:bg-slate-50 flex-row items-center",
+        <tr ref={ref} className={twMerge(
+            "hover:bg-slate-50 items-center w-[100%]",
             !isAffordable && 'opacity-50',
             "dark:hover:bg-slate-800/50 hover:bg-slate-200"
         )} >
+            <Activity mode={inView ? "visible" : "hidden"} >
+                <td onClick={handleViewPlayerProfile} className="overflow-clip"  >
 
-            {inView && (
-                <Fragment>
+                    <div className="flex cursor-pointer flex-row items-center gap-2 w-full" >
+                        <TeamJersey
+                            teamId={player.team_id}
+                            className={twMerge(
+                                "min-h-10 max-h-10 min-w-10 max-w-10",
+                                "lg:min-h-10 lg:max-h-10 lg:min-w-10 lg:max-w-10"
+                            )}
+                            key={player.tracking_id}
+                            hideFade
+                        />
 
-                    <div
-                        onClick={handleViewPlayerProfile}
-                        className="w-[30px] cursor-pointer"
-                    >
-                        <BadgeInfo className="w-4 h-4 text-slate-500 hover:text-blue-600" />
-                    </div>
-
-                    <div
-                        onClick={handleSelectPlayer}
-                        className="flex cursor-pointer  flex-row py-2 items-center justify-between gap-2 w-full"
-                    >
-
-                        <div className="flex flex-row items-center gap-2 min-w-[200px]" >
-
-                            <TeamJersey
-                                teamId={player.team_id}
-                                className={twMerge(
-                                    "min-h-10 max-h-10 min-w-10 max-w-10",
-                                    "lg:min-h-10 lg:max-h-10 lg:min-w-10 lg:max-w-10"
-                                )}
-                                key={player.tracking_id}
-                                hideFade
-                            />
-
-                            <div className="flex flex-col" >
-                                <div className="flex flex-row items-center gap-1" >
-                                    <p className="text-sm dark:text-white truncate" >{player.player_name}</p>
-                                    <AvailabilityIcon 
-                                        athlete={player}
-                                        iconClassName="w-2 h-2"
-                                        className="w-4 h-4 rounded-md"
-                                    />
-                                </div>
-                                
-                                {isAffordable && (<SecondaryText className="text-[10px]" >
-                                    {player?.team?.athstat_name ?? player.position_class}
-                                </SecondaryText>)}
-
-                                {!isAffordable && (
-                                    <p className="text-red-500 text-[10px] font-medium" >Can't Afford this Player</p>
-                                )}
+                        <div className="flex flex-col w-fit" >
+                            <div className="flex flex-row items-center gap-1" >
+                                <p className="text-sm dark:text-white truncate" >{player.player_name}</p>
+                                <AvailabilityIcon
+                                    athlete={player}
+                                    iconClassName="w-2 h-2"
+                                    className="w-4 h-4 rounded-md"
+                                />
                             </div>
-                        </div>
 
-                        <div className="grid grid-cols-2 gap-4 w-[150px]" >
-                            <SecondaryText>{player.power_rank_rating ? Math.floor(player.power_rank_rating) : '-'}</SecondaryText>
-                            <SecondaryText>{player.price}</SecondaryText>
+                            {isAffordable && (<SecondaryText className="text-[10px]" >
+                                {player?.team?.athstat_name ?? player.position_class}
+                            </SecondaryText>)}
+
+                            {!isAffordable && (
+                                <p className="text-red-500 text-[10px] font-medium" >Can't Afford this Player</p>
+                            )}
                         </div>
                     </div>
 
-                </Fragment>
-            )}
-        </div>
+                </td>
+
+                <td className="" >
+                    <SecondaryText>{player.price}</SecondaryText>
+                </td>
+
+                <td className="" >
+                    <MatchPrCard pr={player.power_rank_rating} />
+                </td>
+
+                <td className="" >
+                    <PrimaryButton onClick={handleSelectPlayer} className="w-fit px-2" >
+                        <Plus className="w-4 h-4 " />
+                    </PrimaryButton>
+                </td>
+            </Activity>
+
+        </tr>
     )
 }
