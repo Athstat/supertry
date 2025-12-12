de6c3ab44de4445dbff736dbc84ab89f86767941
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-12-12 11:57:55 +0200

    sorts the teams

--- src/hooks/seasons/useSeasonTeams.ts
+++ src/hooks/seasons/useSeasonTeams.ts
@@ -1,12 +1,19 @@
 import useSWR from "swr";
 import { seasonService } from "../../services/seasonsService";
+import { useMemo } from "react";
 
 export function useSeasonTeams(seasonId?: string) {
     const seasonTeamsKey = seasonId ? `seasons-teams/${seasonId}` : null;
-    const { data: teams, isLoading, mutate, error} = useSWR(seasonTeamsKey, () => seasonService.getSeasonTeams(seasonId ?? ""));
+    const { data, isLoading, mutate, error} = useSWR(seasonTeamsKey, () => seasonService.getSeasonTeams(seasonId ?? ""));
+
+    const sortedTeams = useMemo(() => {
+        return (data || []).sort((a, b) => {
+            return a.athstat_name.localeCompare(b.athstat_name)
+        })
+    },[data]);
 
     return {
-        teams,
+        teams: sortedTeams,
         isLoading,
         mutate,
         error
