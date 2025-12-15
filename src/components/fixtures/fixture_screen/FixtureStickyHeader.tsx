c20ec7d9bc2774dd1c6725ac79f697722d2a39a6
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-12-15 14:30:54 +0200

    hides score on sticky header if game hasn't kicked off

--- src/components/fixtures/fixture_screen/FixtureStickyHeader.tsx
+++ src/components/fixtures/fixture_screen/FixtureStickyHeader.tsx
@@ -13,19 +13,19 @@ type Props = {
 export function FixtureStickyHeader({ fixture }: Props) {
     const { isSticky, sentinelRef } = useSticky<HTMLDivElement>();
 
-    const {gameKickedOff, game_status} = fixtureSummary(fixture);
+    const { gameKickedOff, game_status } = fixtureSummary(fixture);
     const gameIsFinal = (game_status === "completed") && gameKickedOff;
 
     return (
         <>
             <div ref={sentinelRef} />
             {isSticky && <Sticky className={"dark:bg-transparent"}  >
-                
-                <div className="flex z-30 flex-row w-full items-center shadow-lg shadow-black/10 dark:shadow-black justify-center py-2 lg:px-[15%] bg-white dark:bg-[#101010] h-16 px-4" >
-                    
-                <div className="flex-1 flex flex-row items-center justify-between "  >
+
+                <div className="flex z-30 flex-row w-full items-center shadow-lg shadow-black/10 dark:shadow-black justify-between py-2 lg:px-[15%] bg-white dark:bg-[#101010] h-16 px-4" >
+
+                    <div className="flex-1 flex flex-row items-center justify-start gap-3"  >
                         <TeamLogo className="h-10 w-10" url={fixture?.team?.image_url} teamName={fixture?.team?.athstat_name} />
-                        <p  className="text-md font-bold"  >{fixture.team_score}</p>
+                        {gameKickedOff && <p className="text-md font-bold"  >{fixture.team_score}</p>}
                     </div>
 
                     <div className="flex-[3] dark:text-slate-300 text-slate-700 flex flex-row items-center justify-center " >
@@ -33,8 +33,8 @@ export function FixtureStickyHeader({ fixture }: Props) {
                         {!gameKickedOff && fixture.kickoff_time && <p>{format(fixture.kickoff_time, "hh:mm a")}</p>}
                     </div>
 
-                    <div className="flex-1 flex flex-row items-center justify-between " >
-                        <p className="text-md font-bold" >{fixture.opposition_score}</p>
+                    <div className="flex-1 flex flex-row items-center justify-end gap-3" >
+                        {gameKickedOff && <p className="text-md font-bold" >{fixture.opposition_score}</p>}
                         <TeamLogo className="h-10 w-10" url={fixture?.opposition_team?.image_url} teamName={fixture?.opposition_team?.athstat_name} />
                     </div>
                 </div>
