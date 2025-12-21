79baf1f8764ce1a397eb816e0247405ff1053380
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-12-21 23:18:45 +0200

    refactors the league screen

--- src/screens/FantasyScreen.tsx
+++ src/screens/FantasyScreen.tsx
@@ -1,33 +1,23 @@
 import PageView from './PageView';
 import { Trophy } from 'lucide-react';
-import FantasyLeaguesScreenDataProvider from '../components/fantasy-leagues/FantasyLeaguesScreenDataProvider';
-import FantasySeasonSelector from '../components/fantasy-seasons/FantasySeasonSelector';
-import { useFantasyLeaguesScreen } from '../hooks/fantasy/useFantasyLeaguesScreen';
 import { FantasyCompetitionDashboard } from '../components/fantasy-seasons/FantasyCompetitionDashboard';
 import FantasySeasonsOverview from '../components/fantasy-seasons/FantasySeasonsOverview';
 import { Activity } from '../components/shared/Activity';
+import { useFantasySeasons } from '../hooks/dashboard/useFantasySeasons';
 
+/** Renders the Fantasy/League Screen */
 export function FantasyScreen() {
-  return (
-    <FantasyLeaguesScreenDataProvider>
-      <InnnerScreen />
-    </FantasyLeaguesScreenDataProvider>
-  );
-}
-
-function InnnerScreen() {
-  const { selectedSeason } = useFantasyLeaguesScreen();
+  const { selectedSeason } = useFantasySeasons();
 
   return (
     <PageView className="px-4 pt-4 flex flex-col gap-3">
+
       <div>
         <div className="flex flex-row items-center gap-2">
-          <Trophy className="w-5 h-5" />
-          <h1 className="font-bold text-xl">Fantasy</h1>
+          <Trophy className="w-6 h-6" />
+          <h1 className="font-bold text-2xl">Fantasy</h1>
         </div>
       </div>
-
-      <FantasySeasonSelector />
 
       <Activity mode={selectedSeason ? 'visible' : 'hidden'}>
         {selectedSeason && <FantasyCompetitionDashboard fantasySeason={selectedSeason} />}
