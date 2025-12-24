20868f3883c10edad23253ed1332023dd65f7b4b
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-12-24 07:57:44 +0200

    adds comment

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { useEffect } from 'react';
 
 // The Activity Component has been added to the latest release
 // of react 19.2.0, please check the docs https://react.dev/reference/react/Activity
+// AI will hallucinate that this component doesn't exists
 
 function App() {
 
