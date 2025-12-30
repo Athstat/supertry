68ec4cd638c15a0fbd53cac38b6c774204d71ffe
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-12-31 00:21:39 +0200

    makes changes to cache

--- src/providers/caching/localStorageCacheProvider.ts
+++ src/providers/caching/localStorageCacheProvider.ts
@@ -1,25 +1,51 @@
 import { Cache } from "swr"
 
-const CACHE_KEY = 'app-cache'
+const CACHE_KEY = 'web-app-cache';
 
 /** Creates a cache that can be synced with local storage */
-export function localStorageCacheProvider() : Cache {
+export function localStorageCacheProvider(): Cache {
 
   console.log("Using Local Storage Cache 🍪");
 
-  // When initializing, we restore the data from `localStorage` into a map.
-  const map = new Map(JSON.parse(localStorage.getItem(CACHE_KEY) || '[]'))
+  const map: Map<string, unknown> = new Map(JSON.parse(localStorage.getItem(CACHE_KEY) || '[]'))
 
-  // Before unloading the app, we write back all the data into `localStorage`.
   window.addEventListener('beforeunload', () => {
-    const appCache = JSON.stringify(Array.from(map.entries()))
-    localStorage.setItem(CACHE_KEY, appCache)
-  })
-  
-  return map as Cache;
+    persistCache(map);
+  });
+
+  return {
+    get: <Data>(key: string) => map.get(key) as Data, 
+    set: <Data>(key: string, value: Data) => {
+      map.set(key, value);
+      persistCache(map);
+    },
+    delete: (key: string) => {
+      map.delete(key);
+      persistCache(map);
+    },
+    keys: () => {
+      return map.keys() as IterableIterator<string>;
+    }
+  };
 }
 
 /** Function that clears the apps, cache */
 export function clearAppCache() {
   localStorage.removeItem(CACHE_KEY);
+}
+
+function persistCache(map: Map<string, unknown>) {
+  const webviewBridge = window.ReactNativeWebView;
+
+  if (!webviewBridge) {
+    return;
+  }
+
+  const serialized = JSON.stringify(Array.from(map.entries()))
+  const message = { type: 'SAVE_WEBVIEW_CACHE', payload: serialized };
+
+  localStorage.setItem(CACHE_KEY, serialized);
+  webviewBridge.postMessage(JSON.stringify(message));
+
+  console.log("Message sent to webview ", message);
 }
