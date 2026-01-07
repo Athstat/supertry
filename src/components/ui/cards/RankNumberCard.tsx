7255b4f8c06334873c3353fe752b91b7405025ee
Tadiwanashe Shangwa <tshxng@gmail.com>
2026-01-07 08:46:22 +0200

    makes rank num responsive

--- src/components/ui/cards/RankNumberCard.tsx
+++ src/components/ui/cards/RankNumberCard.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from "react"
 import { twMerge } from "tailwind-merge"
 
 type Props = {
@@ -5,13 +6,37 @@ type Props = {
     className?: string
 }
 
-export default function RankNumberCard({value, className} : Props) {
+export default function RankNumberCard({ value, className }: Props) {
+
+    const textSize = useMemo(() => {
+
+        const charLen = value ? `${value}`.length : 0;
+
+        if (charLen <= 3) {
+            return '12px'
+        }
+
+        if (charLen >= 4 && charLen <= 5) {
+            return '11px';
+        }
+
+        if (charLen >= 6) {
+            return '10px';
+        }
+
+    }, [value])
+
     return (
         <div className={twMerge(
-            "flex-shrink-0 w-8 h-8 bg-[#DDE5ED] dark:bg-gray-700 rounded-md flex items-center justify-center",
+            "flex-shrink-0 w-8 h-8 bg-[#DDE5ED] dark:bg-gray-700 rounded-md flex flex-row items-center justify-center",
             className
         )}>
-            <p className="text-sm font-semibold">{value}</p>
+            <p 
+                className="font-semibold text-wrap truncate max-w-8"
+                style={{
+                    fontSize: textSize
+                }}
+            >{value}</p>
         </div>
     )
 }
