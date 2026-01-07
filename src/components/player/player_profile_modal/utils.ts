12889bf95f2565fbda522b33ca8f9f62d1f1bd51
Tadiwanashe Shangwa <tshxng@gmail.com>
2026-01-07 11:52:05 +0200

    cleans up code

--- src/components/player/player_profile_modal/utils.ts
+++ src/components/player/player_profile_modal/utils.ts
@@ -1,3 +1,5 @@
+import { logger } from "../../../services/logger";
+
 /**
  * Formats a date string into a more readable format
  * 
@@ -10,6 +12,7 @@ export const formatDate = (dateString: Date): string => {
     const date = new Date(dateString);
     return `${String(date.getDate()).padStart(2, '0')} ${['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'][date.getMonth()]} ${date.getFullYear()}`;
   } catch (e) {
+    logger.error("Error ", e)
     return 'Unknown';
   }
 };
