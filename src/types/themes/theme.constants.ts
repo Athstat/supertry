6ff07e16c7c0b109b0e1fe77dcd4ee361141c19d
Tadiwanashe Shangwa <tshxng@gmail.com>
2026-01-13 11:34:10 +0200

    changes app BG to white

--- src/types/themes/theme.constants.ts
+++ src/types/themes/theme.constants.ts
@@ -8,7 +8,7 @@ export const appTheme1: AppTheme = {
 
 /** Prem Style Light Theme for Scrummy */
 export const appTheme2: AppTheme = {
-    backgroundCN: 'bg-slate-50 dark:bg-[#15202b]',
+    backgroundCN: 'bg-white dark:bg-[#15202b]',
     cardBackgroundCN: 'bg-white dark:bg-slate-700/50 dark:hover:bg-slate-700/80'
 }
 
