f8c8129e7866fb95054a923fd944270f87c98a7d
Tadiwanashe Shangwa <tshxng@gmail.com>
2026-01-15 00:20:21 +0200

    adds crown

--- src/components/fantasy_league/standings/LeagueStandingsTableRow.tsx
+++ src/components/fantasy_league/standings/LeagueStandingsTableRow.tsx
@@ -1,70 +1,108 @@
-import { User } from "lucide-react";
+import { Crown, User } from "lucide-react";
 import { twMerge } from "tailwind-merge";
 import { FantasySeasonRankingItem } from "../../../types/fantasyLeagueGroups";
 import { smartRoundUp } from "../../../utils/intUtils";
 import SecondaryText from "../../ui/typography/SecondaryText";
 
 type StandingsProps = {
-  ranking: FantasySeasonRankingItem;
-  index: number;
-  isUser?: boolean;
-  hideUserScore?: boolean;
-  onClick?: (member: FantasySeasonRankingItem) => void
+    ranking: FantasySeasonRankingItem;
+    index: number;
+    isUser?: boolean;
+    hideUserScore?: boolean;
+    onClick?: (member: FantasySeasonRankingItem) => void
 };
 
 /** Renders a league standing table row */
 export function LeagueStandingsTableRow({ ranking, isUser, hideUserScore, index, onClick }: StandingsProps) {
 
-  const rank = ranking.league_rank ?? index + 1;
-  const shouldHideScore = (isUser && hideUserScore) || !ranking.total_score
-  const pointsDisplay = shouldHideScore ? '-' : smartRoundUp(ranking.total_score);
+    const rank = ranking.league_rank ?? index + 1;
+    const shouldHideScore = (isUser && hideUserScore) || !ranking.total_score
+    const pointsDisplay = shouldHideScore ? '-' : smartRoundUp(ranking.total_score);
 
-  const handleClick = () => {
-    if (onClick && ranking) {
-      onClick(ranking);
+    const isPointsKing = ranking.league_rank === 1;
+
+    const handleClick = () => {
+        if (onClick && ranking) {
+            onClick(ranking);
+        }
     }
-  }
 
-  return (
-    <div
-      className={twMerge(
-        'flex flex-row  cursor-pointer hover:bg-slate-200 hover:dark:bg-slate-800/60  p-3 items-center gap-2 justify-between',
-        isUser && 'bg-blue-500 text-white'
-      )}
+    return (
+        <div
+            className={twMerge(
+                'cursor-pointer flex flex-col gap-2 hover:bg-slate-200 hover:dark:bg-slate-800/60 p-2',
+                isUser && 'bg-blue-500 text-white && hover:bg-blue-500 hover:dark:bg-blue-500',
+                isPointsKing && ' rounded-xl'
+            )}
 
-      onClick={handleClick}
-    >
-      <div className="flex flex-row items-center gap-2">
-        <div className="flex flex-row">
-          {/* {badge && <div className='text-sm' >{badge}</div>} */}
-          <SecondaryText className={twMerge(
-            "w-10",
-            isUser && "text-white dark:text-white"
-          )}>
-            {/* {rank} {badge}{' '} */}
-            {rank}
-          </SecondaryText>
+            onClick={handleClick}
+        >
+
+
+            <div className="flex flex-row items-center gap-2 justify-between px-2" >
+                <div className="flex flex-row items-center gap-2">
+
+                    <div className="flex flex-row">
+                        <SecondaryText className={twMerge(
+                            "w-10",
+                            isUser && "text-white dark:text-white"
+                        )}>
+                            {/* {rank} {badge}{' '} */}
+                            {rank}
+                        </SecondaryText>
+
+                    </div>
+
+                    {isUser && (
+                        <div className=" w-6 h-6 bg-blue-500 rounded-xl flex flex-col items-center justify-center">
+                            <User className="w-4 h-4 text-white" />
+                        </div>
+                    )}
+
+                    <div className="flex flex-col">
+                        <p>{ranking.username ?? ranking.first_name}</p>
+                        {isUser && hideUserScore && (
+                            <p className={twMerge('text-xs', isUser ? 'text-white/80' : 'text-gray-500')}>
+                                Claim account to see your points
+                            </p>
+                        )}
+                    </div>
+
+                    <RankingCrown isUser={isUser} ranking={ranking} />
+                </div>
+
+                <div className="text-right">
+                    <p>{pointsDisplay}</p>
+                </div>
+            </div>
         </div>
+    );
+}
 
-        {isUser && (
-          <div className=" w-6 h-6 bg-blue-500 rounded-xl flex flex-col items-center justify-center">
-            <User className="w-4 h-4 text-white" />
-          </div>
-        )}
+type RankingCrownProps = {
+    isUser?: boolean,
+    ranking: FantasySeasonRankingItem
+}
 
-        <div className="flex flex-col">
-          <p>{ranking.username ?? ranking.first_name}</p>
-          {isUser && hideUserScore && (
-            <p className={twMerge('text-xs', isUser ? 'text-white/80' : 'text-gray-500')}>
-              Claim account to see your points
-            </p>
-          )}
+function RankingCrown({ isUser, ranking }: RankingCrownProps) {
+
+    const rankType = ranking.league_rank === 1 ? 'gold' :
+        ranking.league_rank === 2 ? 'silver' :
+            ranking.league_rank === 3 ? 'bronze' : 'none';
+
+    if (rankType === "none") {
+        return null;
+    }
+
+    return (
+        <div className={twMerge(
+            "w-fit px-2 py-0.5 bg-gradient-to-r from-[#1196F5] to-[#1196F5] text-white dark:text-white rounded-full flex flex-row items-center gap-2",
+            isUser && 'from-white to-white text-[#1196F5] dark:text-[#1196F5]',
+            rankType === 'gold' && 'from-[#FFC603] to-[#d29402] text-black dark:text-black',
+            rankType === 'silver' && 'from-[#a8a8a8] to-[#848484] text-white dark:text-white',
+            rankType === 'bronze' && 'from-[#8c5304] to-[#e66305] text-white dark:text-white',
+        )} >
+            <Crown className="w-4 h-4" />
         </div>
-      </div>
-
-      <div className="text-right">
-        <p>{pointsDisplay}</p>
-      </div>
-    </div>
-  );
+    )
 }
