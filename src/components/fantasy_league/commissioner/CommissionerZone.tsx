846f68dda64090b60be5bc629596835c0092f626
Tadiwanashe Shangwa <tshxng@gmail.com>
2026-01-15 11:49:19 +0200

    adds modal to upload a league logo

--- src/components/fantasy_league/commissioner/CommissionerZone.tsx
+++ src/components/fantasy_league/commissioner/CommissionerZone.tsx
@@ -1,23 +1,14 @@
-import { ChevronRight, Pencil, X } from 'lucide-react'
-import LeagueVisibilityInput from '../../fantasy-leagues/create_league_modal/LeagueVisibilityInput'
-import PrimaryButton from '../../ui/buttons/PrimaryButton'
-import { ErrorState } from '../../ui/ErrorState'
-import InputField, { TextField } from '../../ui/forms/InputField'
+import { ChevronRight, Pencil } from 'lucide-react'
 import SecondaryText from '../../ui/typography/SecondaryText'
 import { useState } from 'react'
 import { useFantasyLeagueGroup } from '../../../hooks/leagues/useFantasyLeagueGroup'
-import { fantasyLeagueGroupsService } from '../../../services/fantasy/fantasyLeagueGroupsService'
-import { logger } from '../../../services/logger'
-import { useInView } from 'react-intersection-observer'
-import BottomSheetView from '../../ui/modals/BottomSheetView'
-import { FantasyLeagueGroup } from '../../../types/fantasyLeagueGroups'
-import CircleButton from '../../ui/buttons/BackButton'
 import RoundedCard from '../../ui/cards/RoundedCard'
 import { ShieldUser } from 'lucide-react'
 import { useAuth } from '../../../contexts/AuthContext'
 import { EditLeagueBannerModal } from './EditLeagueBannerModal'
 import { Image } from 'lucide-react'
 import { EditLeagueInfoModal } from './EditLeagueInfoModal'
+import EditLeagueLogoModal from './EditLeagueLogoModal'
 
 export default function CommissionerZone() {
 
@@ -28,8 +19,11 @@ export default function CommissionerZone() {
 
     const [showEditInfo, setShowEditInfo] = useState(false);
     const [showEditBanner, setShowEditBanner] = useState(false);
+    const [showEditLogo, setShowEditLogo] = useState(false);
+
     const toggleShowEditInfo = () => setShowEditInfo(prev => !prev);
     const toggleEditBanner = () => setShowEditBanner(prev => !prev);
+    const toggleEditLogo = () => setShowEditLogo(prev => !prev);
 
     if (!isCommissioner) {
         return;
@@ -55,12 +49,16 @@ export default function CommissionerZone() {
             <div className='flex flex-col gap-2' >
                 <RoundedCard
                     className='py-2 px-4 cursor-pointer flex flex-col gap-1'
-                    onClick={toggleShowEditInfo}
+                    onClick={toggleEditBanner}
                 >
                     <div className='flex flex-row items-center gap-2 justify-between' >
-                        <div className='flex flex-row items-center gap-2' >
-                            <Pencil className='w-4 h-4' />
-                            <p>Edit League info</p>
+                        <div>
+                            <div className='flex flex-row items-center gap-2' >
+                                <Pencil className='w-4 h-4' />
+                                <p>Edit League Info</p>
+                            </div>
+
+                            <SecondaryText>Edit your leagues banner</SecondaryText>
                         </div>
 
                         <div>
@@ -68,7 +66,6 @@ export default function CommissionerZone() {
                         </div>
                     </div>
 
-                    <SecondaryText>Edit your leagues name, description and visibility</SecondaryText>
                 </RoundedCard>
 
                 <RoundedCard
@@ -76,9 +73,13 @@ export default function CommissionerZone() {
                     onClick={toggleEditBanner}
                 >
                     <div className='flex flex-row items-center gap-2 justify-between' >
-                        <div className='flex flex-row items-center gap-2' >
-                            <Image className='w-4 h-4' />
-                            <p>Edit Banner</p>
+                        <div>
+                            <div className='flex flex-row items-center gap-2' >
+                                <Image className='w-4 h-4' />
+                                <p>Edit Banner</p>
+                            </div>
+
+                            <SecondaryText>Edit your league's banner</SecondaryText>
                         </div>
 
                         <div>
@@ -86,16 +87,46 @@ export default function CommissionerZone() {
                         </div>
                     </div>
 
-                    <SecondaryText>Edit your leagues banner</SecondaryText>
+                </RoundedCard>
+
+
+                <RoundedCard
+                    className='py-2 px-4 cursor-pointer flex flex-col gap-1'
+                    onClick={toggleEditLogo}
+                >
+                    <div className='flex flex-row items-center gap-2 justify-between' >
+                        <div>
+                            <div className='flex flex-row items-center gap-2' >
+                                <Image className='w-4 h-4' />
+                                <p>Edit League Logo</p>
+                            </div>
+
+                            <SecondaryText>Edit your league's logo</SecondaryText>
+                        </div>
+
+                        <div>
+                            <ChevronRight />
+                        </div>
+                    </div>
+
                 </RoundedCard>
             </div>
+
 
             <EditLeagueInfoModal
                 isOpen={showEditInfo}
                 onClose={toggleShowEditInfo}
             />
 
-            <EditLeagueBannerModal isOpen={showEditBanner} onClose={toggleEditBanner} />
+            <EditLeagueBannerModal 
+                isOpen={showEditBanner}
+                onClose={toggleEditBanner} 
+            />
+
+            <EditLeagueLogoModal 
+                isOpen={showEditLogo}
+                onClose={toggleEditLogo}
+            />
         </div>
     )
 }
---
+++ src/components/fantasy_league/commissioner/EditLeagueLogoModal.tsx
@@ -0,0 +1,106 @@
+import { X, Upload } from "lucide-react";
+import { useState, useCallback } from "react";
+import { useFantasyLeagueGroup } from "../../../hooks/leagues/useFantasyLeagueGroup";
+import { fantasyLeagueGroupsService } from "../../../services/fantasy/fantasyLeagueGroupsService";
+import { logger } from "../../../services/logger";
+import CircleButton from "../../ui/buttons/BackButton";
+import PrimaryButton from "../../ui/buttons/PrimaryButton";
+import ImageFileInput from "../../ui/forms/FileInput";
+import BottomSheetView from "../../ui/modals/BottomSheetView";
+import { Toast } from "../../ui/Toast";
+
+type Props = {
+    isOpen?: boolean,
+    onClose?: () => void
+}
+
+export default function EditLeagueLogoModal({ isOpen, onClose }: Props) {
+const [files, setFiles] = useState<File[]>([]);
+    const [isUploading, setUploading] = useState(false);
+    const [error, setError] = useState<string>();
+
+    const {league, mutateLeague} = useFantasyLeagueGroup();
+
+    const handleClose = () => {
+        if (onClose) {
+            setFiles([]);
+            onClose();
+        }
+    }
+
+    const handleUpload = useCallback(async () => {
+        try {
+
+            
+            if (files.length === 0 || !league?.id) {
+                return;
+            }
+            
+            setUploading(true);
+
+            const logo = files[0];
+            const updatedLeague = await fantasyLeagueGroupsService.updateBannerAndLogo(league?.id || '', undefined, logo);
+
+            if (updatedLeague) {
+                mutateLeague(updatedLeague);
+                setUploading(false);
+                handleClose();
+                return;
+            }
+
+        } catch (err) {
+            logger.error("Error handling upload ", err);
+            setError("Something wen't wrong uploading banner");
+        } finally {
+            setUploading(false);
+        }
+
+    }, [league, files, handleClose]);
+
+    if (!isOpen) {
+        return null;
+    }
+
+    return (
+        <BottomSheetView
+            hideHandle
+            className='max-h-[80vh] p-4 min-h-[40vh] flex flex-col gap-2'
+        >
+            <div className='flex flex-row items-center gap-2 justify-between' >
+                <p className='font-semibold text-lg' >Edit Logo</p>
+
+                <div>
+                    <CircleButton onClick={handleClose}>
+                        <X />
+                    </CircleButton>
+                </div>
+            </div>
+
+            <form>
+                <ImageFileInput 
+                    files={files}
+                    setFiles={setFiles}
+                />
+            </form>
+
+            <PrimaryButton 
+                className="py-3 flex flex-row items-center gap-2"
+                onClick={() => handleUpload()}
+                isLoading={isUploading}
+            >
+                <p>Upload Logo</p>
+                <Upload />
+            </PrimaryButton>
+
+            {error && (
+                <Toast 
+                    message={error}
+                    isVisible={true}
+                    type="error"
+                    onClose={() => setError(undefined)}
+                />
+            )}
+
+        </BottomSheetView>
+    )
+}
