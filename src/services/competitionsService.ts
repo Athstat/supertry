39db6a26cfcd98138555805e9b4b843a4a8bcac9
Tadiwanashe Shangwa <tshxng@gmail.com>
2026-01-15 22:50:43 +0200

    adds competition service

---
+++ src/services/competitionsService.ts
@@ -0,0 +1,26 @@
+import { IProAthlete } from "../types/athletes";
+import { getAuthHeader, getUri } from "../utils/backendUtils";
+import { logger } from "./logger"
+
+export const competitionService = {
+
+    /** Function to fetch for athelte that belong to a competition */
+    getAthletes: async (competitionId: string) : Promise<IProAthlete[]> => {
+        
+        try {
+            const uri = getUri(`/api/v1/competitions/${competitionId}/athletes`);
+            const res = await fetch(uri, {
+                headers: getAuthHeader()
+            })
+
+            if (res.ok) {
+                return (await res.json()) as IProAthlete[];
+            }
+
+        } catch (err) {
+            logger.error("Error fetching competition athletes ", err);
+        }
+
+        return [];
+    }
+}
