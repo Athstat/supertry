5418fc9ab4b4daccd71d68c153f78073daade738
Tadiwanashe Shangwa <tshxng@gmail.com>
2026-01-16 14:13:20 +0200

    adds david feedback

--- src/components/dashboard/SixNationsGameRules.tsx
+++ src/components/dashboard/SixNationsGameRules.tsx
@@ -1,19 +1,19 @@
 import { twMerge } from 'tailwind-merge'
 import RoundedCard from '../ui/cards/RoundedCard'
 import SecondaryText from '../ui/typography/SecondaryText'
-import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
 
 type Props = {
     className?: string
 }
 
+/** Renders siz nations game rules */
 export default function SixNationsGameRules({ className }: Props) {
 
-    const navigate = useNavigate();
+    const [showGameRules, setShowGameRules] = useState(false);
 
-
-    const handleClick = () => {
-        navigate('/leagues');
+    const toggleShowGameRules = () => {
+        setShowGameRules(prev => !prev);
     }
 
     return (
@@ -21,8 +21,11 @@ export default function SixNationsGameRules({ className }: Props) {
             "flex bg-[#F0F3F7] flex-col gap-4 pt-5 pb-5 pl-3 pr-3",
             className
         )}>
-            <h1 className="font-[510] text-[20px] text-[#011E5C] dark:text-white" style={{ fontFamily: 'Oswald, sans-serif' }}>SCRUMMY M6N Fantasy: Game Rules</h1>
-            <div className='flex flex-col gap-4' >
+            <h1 className="font-[510] text-[20px] text-[#011E5C] dark:text-white" style={{ fontFamily: 'Oswald, sans-serif' }}>SCRUMMY M6N Fantasy</h1>
+            
+            <SecondaryText>Compete for glory and exclusive rewards in this year's M6N Fantasy challenge. Build your roster, track the leaderboard, and win big.</SecondaryText>
+            
+            {showGameRules && <div className='flex flex-col gap-4' >
                 <div>
                     <SecondaryText>1. <strong>Entry Deadline:</strong> Feb 21, 2:00 PM BST (Prize Eligibility).</SecondaryText>
                 </div>
@@ -42,13 +45,13 @@ export default function SixNationsGameRules({ className }: Props) {
                     <SecondaryText>c. <strong>4th-10th place:</strong> Shoutouts on SCRUMMY leaderboard</SecondaryText>
                 </div>
 
-            </div>
+            </div>}
 
             <button
-                onClick={handleClick}
+                onClick={toggleShowGameRules}
                 className="px-2 py-2.5 w-fit rounded-md bg-transparent border border-[#011E5C] dark:border-white font-semibold text-xs text-[#011E5C] dark:text-white uppercase shadow-md transition-colors hover:bg-[#011E5C] hover:text-white dark:hover:bg-white dark:hover:text-[#011E5C] whitespace-nowrap flex-shrink-0"
             >
-                Start Competing
+                {showGameRules ? "Hide Game Rules" : "Game Rules" }
             </button>
         </RoundedCard>
     )
