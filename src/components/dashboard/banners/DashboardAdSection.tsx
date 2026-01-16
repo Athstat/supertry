6070397cbdf0eeb55366dc77c49d9b769fa5ae73
Tadiwanashe Shangwa <tshxng@gmail.com>
2026-01-16 09:02:00 +0200

    adds a dashboard ad section

---
+++ src/components/dashboard/banners/DashboardAdSection.tsx
@@ -0,0 +1,81 @@
+import { useNavigate } from 'react-router-dom';
+import { useFantasySeasons } from '../../../hooks/dashboard/useFantasySeasons';
+
+type AdItem = {
+    title?: string,
+    description?: string,
+    ctaButton?: string,
+    ctaLinkButton?: string,
+    imageUrl?: string,
+    titleClassName?: string,
+    descriptionClassName?: string
+}
+
+export default function DashboardAdSection() {
+    const { selectedSeason } = useFantasySeasons();
+    const navigate = useNavigate();
+
+    const sbrAd: AdItem = {
+        title: "SCHOOL RUGBY '26",
+        description: "Games - Data - Players",
+        ctaLinkButton: "See What's Happening",
+        imageUrl: '/images/dashboard/sbr_cta_bg_v4.png',
+        titleClassName: 'text-end font-bold text-md leading-6 text-[#011E5C] w-[80%]',
+        descriptionClassName: "text-end -mt-2 font-semibold text-sm text-gray-800"
+    }
+
+    const sixNationsAd: AdItem = {
+        title: 'M6 NATIONS FANTASY',
+        description: 'Win more than bragging rights!',
+        ctaButton: 'Play Now',
+        imageUrl: '/images/dashboard/6nations_ad.png',
+        titleClassName: 'text-end font-bold text-xl leading-6 text-white w-[80%]',
+        descriptionClassName: "text-end mt-1 font-semibold text-sm text-white"
+    }
+
+    const ad = selectedSeason?.name.includes('Six Nations 2026') ? sixNationsAd : sbrAd;
+
+    return (
+        <div className="relative w-full overflow-hidden shadow-md">
+
+            <div className="w-full h-fit pointer-events-none">
+                <img
+                    src={ad.imageUrl}
+                    alt="Rugby players"
+                    className="w-full h-full"
+                />
+            </div>
+
+            {/* Content */}
+            <div className="absolute z-10 w-full top-0 right-0 flex flex-col items-end justify-end p-6">
+                {/* Title */}
+                <h2
+                    className={ad.titleClassName}
+                    style={{ fontFamily: 'Race Sport, sans-serif' }}
+                >
+                    {ad.title}
+                </h2>
+                <p className={ad.descriptionClassName}>{ad.description}</p>
+
+                {/* Button */}
+                {ad.ctaLinkButton && <p
+                    onClick={() => navigate('/schools')}
+                    className="font-semibold text-sm text-[#011E5C] underline mt-8 cursor-pointer"
+                >
+                    See What's Happening
+                </p>}
+
+                {ad.ctaButton && (
+                    <button
+                        onClick={() => navigate('/leagues')}
+                        className="px-3 py-3.5 mt-5 rounded-md bg-transparent border border-white dark:border-white font-semibold text-xs text-white dark:text-white uppercase shadow-md transition-colors hover:bg-[#011E5C] hover:text-white dark:hover:bg-white dark:hover:text-[#011E5C] whitespace-nowrap flex-shrink-0"
+                    >
+                        Play Now
+                    </button>
+                )}
+            </div>
+
+
+        </div>
+    );
+}
