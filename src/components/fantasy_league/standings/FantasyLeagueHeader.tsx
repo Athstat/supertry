6642e81a325eb143fe7513d841445bd21dc5b2e7
Tadiwanashe Shangwa <tshxng@gmail.com>
2026-01-16 10:03:10 +0200

    removes banner

--- src/components/fantasy_league/standings/FantasyLeagueHeader.tsx
+++ src/components/fantasy_league/standings/FantasyLeagueHeader.tsx
@@ -2,10 +2,8 @@ import { ArrowLeft } from 'lucide-react'
 import CircleButton from '../../ui/buttons/BackButton'
 import { JoinOrInviteButton } from '../buttons/JoinLeagueButton'
 import { useFantasyLeagueGroup } from '../../../hooks/leagues/useFantasyLeagueGroup'
-import LeagueGroupBanner from '../LeagueGroupBanner'
 import { twMerge } from 'tailwind-merge'
 import { AppColours } from '../../../types/constants'
-import LeagueGroupLogo from '../LeagueGroupLogo'
 
 type Props = {
     handleBack?: () => void
@@ -17,9 +15,9 @@ export default function FantasyLeagueHeader({ handleBack }: Props) {
     const { league } = useFantasyLeagueGroup();
 
     return (
-        <div className="flex flex-col relative" >
+        <div className="flex flex-col relative min-h-[40px]" >
 
-            <LeagueGroupBanner league={league} />
+            {/* <LeagueGroupBanner league={league} /> */}
 
             <div className={twMerge(
                 'flex absolute top-0 left-0 w-full pt-2 px-4 flex-row items-center justify-between pb-10',
@@ -41,9 +39,9 @@ export default function FantasyLeagueHeader({ handleBack }: Props) {
                 <JoinOrInviteButton />
             </div>
 
-            <div className='absolute -bottom-3 left-5' >
+            {/* <div className='absolute -bottom-3 left-5' >
                 <LeagueGroupLogo className='w-16 h-16 dark:drop-shadow-[0_5px_5px_rgba(0,0,0,0.7)]' league={league} />
-            </div>
+            </div> */}
         </div>
     )
 }
