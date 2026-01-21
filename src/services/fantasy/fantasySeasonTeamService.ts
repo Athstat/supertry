ae7dd5f48f084e1669e7e2473f159d4ca795fade
Tadiwanashe Shangwa <tshxng@gmail.com>
2026-01-21 14:49:30 +0200

    adds service to interact with the fantasy season team

---
+++ src/services/fantasy/fantasySeasonTeamService.ts
@@ -0,0 +1,65 @@
+import { IFantasyLeagueTeam } from "../../types/fantasyLeague";
+import { UpdateFantasyLeagueTeam } from "../../types/fantasyLeagueTeam";
+import { getAuthHeader, getUri } from "../../utils/backendUtils";
+import { logger } from "../logger"
+
+export const fantasySeasonTeamService = {
+    getRoundTeam: async (seasonId: string, userId: string, roundNumber: number | number) => {
+        try {
+            
+            const uri = getUri(`/api/v1/fantasy-seasons/${seasonId}/users/${userId}/teams/${roundNumber}`);
+            const res = await fetch(uri, {
+                headers: getAuthHeader()
+            });
+
+            if (res.ok) {
+                return (await res.json()) as IFantasyLeagueTeam;
+            }
+
+        } catch (err) {
+            logger.error('Error fetching user round team ', err);
+        }
+
+        return undefined;
+    },
+
+    updateRoundTeam: async (seasonId: string, userId: string, roundNumber: string | number, data: UpdateFantasyLeagueTeam) => {
+        try {
+            const uri = getUri(`/api/v1/fantasy-seasons/${seasonId}/users/${userId}/teams/${roundNumber}`);
+            const res = await fetch(uri, {
+                headers: getAuthHeader(),
+                method: 'PUT',
+                body: JSON.stringify(data)
+            });
+
+            if (res.ok) {
+                return (await res.json()) as IFantasyLeagueTeam
+            }
+
+        } catch (err) {
+            logger.error("Error updating user fantasy round team ", err);
+        }
+
+        return undefined;
+    },
+
+    createRoundTeam: async (seasonId: string, userId: string, roundNumber: string | number, data: UpdateFantasyLeagueTeam) => {
+        try {
+            const uri = getUri(`/api/v1/fantasy-seasons/${seasonId}/users/${userId}/teams/${roundNumber}`);
+            const res = await fetch(uri, {
+                headers: getAuthHeader(),
+                method: 'POST',
+                body: JSON.stringify(data)
+            });
+
+            if (res.ok) {
+                return (await res.json()) as IFantasyLeagueTeam
+            }
+
+        } catch (err) {
+            logger.error("Error updating user fantasy round team ", err);
+        }
+
+        return undefined;
+    }
+}
