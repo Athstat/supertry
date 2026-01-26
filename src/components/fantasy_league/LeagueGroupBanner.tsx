051c41fa5880c7958062d022692e4f6dd4ffa82e
Tadiwanashe Shangwa <tshxng@gmail.com>
2026-01-26 15:00:34 +0200

    adds league group banenr

--- src/components/fantasy_league/LeagueGroupBanner.tsx
+++ src/components/fantasy_league/LeagueGroupBanner.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { FantasyLeagueGroup } from "../../types/fantasyLeagueGroups"
+import { countryFlags } from "../../types/countries";
 
 type Props = {
   league?: FantasyLeagueGroup
@@ -9,13 +10,16 @@ export default function LeagueGroupBanner({league} : Props) {
 
   const [error,setError] = useState(false);
 
-  if (league?.banner && !error) {
+  const defaultCountryBannerUrl = getCountryDefaultBanner(league);
+  const bannerUrl = league?.banner || defaultCountryBannerUrl;
+
+  if (bannerUrl && !error) {
     return (
       <div className="w-full max-h-[250px] min-h-[150px] overflow-clip" >
         <img 
           onError={() => setError(true)}
-          src={league.banner}
-          alt={`${league.title} Banner`}
+          src={bannerUrl}
+          alt={`${league?.title} Banner`}
           className="w-full h-full object-right"
         />
       </div>
@@ -27,4 +31,18 @@ export default function LeagueGroupBanner({league} : Props) {
       
     </div>
   )
+}
+
+function getCountryDefaultBanner(league?: FantasyLeagueGroup) {
+  const country = countryFlags.find((c) => {
+    const matchesWhole = league?.title?.toLowerCase() === c.name.toLowerCase();
+    const matchesFanLeague = league?.title?.endsWith("Fans") && league?.title.toLowerCase().startsWith(c.name.toLowerCase())
+    return matchesWhole || matchesFanLeague;
+  })
+
+  if (country) {
+    return `https://dp7xhssw324ru.cloudfront.net/country_banners_${country.code.toLowerCase()}.png`;
+  }
+
+  return undefined;
 }
