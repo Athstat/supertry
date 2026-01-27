3e7baa4f7ce0cb64eca91309bf7abc221fc6ca74
Tadiwanashe Shangwa <tshxng@gmail.com>
2026-01-27 23:47:24 +0200

    adds view mode switcher to SBR screen

---
+++ src/components/fixtures/FixtureViewModeSwitcher.tsx
@@ -0,0 +1,29 @@
+import { FixtureListViewMode } from "../../types/games";
+import SegmentedControl from "../ui/SegmentedControl";
+
+type ViewModeSwitcherProps = {
+  viewMode: FixtureListViewMode,
+  onChange?: (mode: FixtureListViewMode) => void
+}
+
+/** Renders a fixture view mode switcher */
+export function FixtureViewModeSwitcher({ viewMode, onChange }: ViewModeSwitcherProps) {
+
+  const handleOnChange = (val?: string) => {
+    if (val && onChange) {
+      onChange(val as FixtureListViewMode);
+    }
+  }
+
+  return (
+    <SegmentedControl
+      options={[
+        { value: 'fixtures', label: 'Fixtures' },
+        { value: 'pickem', label: "PICK'EM" },
+      ]}
+      value={viewMode}
+      onChange={handleOnChange}
+    />
+  )
+}
+
--- src/components/ui/containers/RoundedScreenHeader.tsx
+++ src/components/ui/containers/RoundedScreenHeader.tsx
@@ -7,9 +7,10 @@ type Props = {
     leadingIcon?: ReactNode,
     children?: ReactNode,
     trailingSlot?: ReactNode,
-    className?: string
+    className?: string,
+    titleCN?: string
 }
-export default function RoundedScreenHeader({ children, title, leadingIcon, className, trailingSlot }: Props) {
+export default function RoundedScreenHeader({ children, title, leadingIcon, className, trailingSlot, titleCN }: Props) {
     return (
         <div className={twMerge(
             "bg-[#1196F5] text-white p-4 rounded-b-[20px] flex flex-col gap-6",
@@ -18,10 +19,13 @@ export default function RoundedScreenHeader({ children, title, leadingIcon, clas
 
             <div className="flex flex-row items-center gap-2 justify-between" >
                 <div className="flex flex-row items-center gap-3" >
-                    <div className="w-[42px] h-[42px]  bg-[#88CBFA] rounded-full flex flex-col items-center justify-center" >
+                    <div className="w-[42px] h-[42px]  bg-white/50 rounded-full flex flex-col items-center justify-center" >
                         {leadingIcon}
                     </div>
-                    <TextHeading className="text-[24px] font-semibold" >{title}</TextHeading>
+                    <TextHeading className={twMerge(
+                        "text-[24px] font-semibold",
+                        titleCN
+                    )} >{title}</TextHeading>
                 </div>
 
                 {trailingSlot}
