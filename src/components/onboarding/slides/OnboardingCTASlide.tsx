8fb7f9e3b2f52ac6900c30fa90e5d104b221b073
Tadiwanashe Shangwa <tshxng@gmail.com>
2026-01-29 22:59:20 +0200

    makes code put

--- src/components/onboarding/slides/OnboardingCTASlide.tsx
+++ src/components/onboarding/slides/OnboardingCTASlide.tsx
@@ -14,6 +14,7 @@ export function OnboardingCTASlide() {
 
   const {updateProfile, isLoading} = useInternalUserProfile();
   const {favouriteTeams, country} = useOnboarding();
+  
 
   const handleGetStarted = async () => {
 
