e9ee8b3037f4bccbed8d1d3bbd15a5c801d290f2
Tadiwanashe Shangwa <tshxng@gmail.com>
2026-01-30 12:14:09 +0200

    adds members count to fantasy league group type def

--- src/components/fantasy-leagues/card/JoinLeagueCard.tsx
+++ src/components/fantasy-leagues/card/JoinLeagueCard.tsx
@@ -1,9 +1,7 @@
 import { ChevronRight, Users } from 'lucide-react';
 import { FantasyLeagueGroup } from '../../../types/fantasyLeagueGroups';
 import PrimaryButton from '../../ui/buttons/PrimaryButton';
-import useSWR from 'swr';
-import { fantasyLeagueGroupsService } from '../../../services/fantasy/fantasyLeagueGroupsService';
-import { useCallback, useMemo } from 'react';
+import { useCallback } from 'react';
 import { twMerge } from 'tailwind-merge';
 import { useJoinLeague } from '../../../hooks/leagues/useJoinLeague';
 import RoundedCard from '../../ui/cards/RoundedCard';
@@ -15,14 +13,7 @@ interface JoinLeagueCardProps {
 
 export function JoinLeagueCard({ leagueGroup }: JoinLeagueCardProps) {
 
-  const key = `/league-group/${leagueGroup.id}/league-group-members/`;
-  const { data, isLoading } = useSWR(key, () => fantasyLeagueGroupsService.getGroupMembers(leagueGroup.id));
-
   const { handleJoinLeague: onJoin, isLoading: isJoining } = useJoinLeague();
-
-  const memberCount = useMemo(() => {
-    return (data || []).length;
-  }, [data])
 
   const name = leagueGroup.title;
 
@@ -41,7 +32,6 @@ export function JoinLeagueCard({ leagueGroup }: JoinLeagueCardProps) {
 
       <div className={twMerge(
         "flex flex-row items-center justify-between",
-        isLoading && "opacity-0"
       )}>
 
         <div className='flex flex-row items-center gap-2' >
@@ -56,10 +46,10 @@ export function JoinLeagueCard({ leagueGroup }: JoinLeagueCardProps) {
               <h3 className="text-sm">{name}</h3>
             </div>
 
-            <div className="flex items-center gap-1 text-sm text-gray-400">
+            {leagueGroup.members_count && <div className="flex items-center gap-1 text-sm text-gray-400">
               <Users className='w-4 h-4' />
-              <span className='text-xs' >Players {memberCount}</span>
-            </div>
+              <span className='text-xs' >Players {leagueGroup.members_count}</span>
+            </div>}
           </div>
         </div>
 
