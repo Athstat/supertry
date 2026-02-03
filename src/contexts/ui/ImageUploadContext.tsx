40593349bddde55d95aa07e0c0e8e5ecd307deae
Tadiwanashe Shangwa <tshxng@gmail.com>
2026-02-03 11:50:59 +0200

    adds image upload context

---
+++ src/contexts/ui/ImageUploadContext.tsx
@@ -0,0 +1,29 @@
+import { createContext, Dispatch, ReactNode, SetStateAction, useState } from "react"
+
+type ImageUploadContextProps = {
+    file?: File,
+    setFile: Dispatch<SetStateAction<File | undefined>>,
+    error?: string,
+    setError?: Dispatch<SetStateAction<string | undefined>>
+}
+
+export const ImageUploadContext = createContext<ImageUploadContextProps | null>(null);
+
+type Props = {
+    children?: ReactNode
+}
+
+/** Provides context for image uploading */
+export default function ImageUploadProvider({ children }: Props) {
+
+    const [file, setFile] = useState<File>();
+    const [error, setError] = useState<string>();
+
+    return (
+        <ImageUploadContext.Provider
+            value={{file, setFile, error, setError}}
+        >
+            {children}
+        </ImageUploadContext.Provider>
+    )
+}
