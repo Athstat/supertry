dab35d58928abc94117d4c547c2f57e61ddbd2d3
Tadiwanashe Shangwa <tshxng@gmail.com>
2026-02-03 21:50:15 +0200

    reduces height

--- src/components/players/teams/PlayersTeamsGridList.tsx
+++ src/components/players/teams/PlayersTeamsGridList.tsx
@@ -118,7 +118,7 @@ function TeamItem({ team, onClick }: TeamItemProps) {
     <RoundedCard onClick={handleOnClick} className="dark:border-none cursor-pointer p-2 h-[90px] flex flex-col items-center justify-center gap-2" >
       <TeamLogo
         url={team.image_url}
-        className="w-8 h-8"
+        className="w-7 h-7"
       />
       <SecondaryText className="text-[10px] text-center" >{team.athstat_name}</SecondaryText>
     </RoundedCard>
