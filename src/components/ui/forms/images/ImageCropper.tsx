ed22a168d6782f220f7ef9853e793f702c5b726d
Tadiwanashe Shangwa <tshxng@gmail.com>
2026-02-03 12:53:35 +0200

    adds image cropping on file input modal

--- src/components/ui/forms/images/ImageCropper.tsx
+++ src/components/ui/forms/images/ImageCropper.tsx
@@ -1,13 +1,52 @@
+import { SyntheticEvent, useEffect, useState } from 'react';
+import 'react-image-crop/dist/ReactCrop.css'
+import ReactCrop, { makeAspectCrop, type Crop } from 'react-image-crop'
+
 type Props = {
     file: File,
     setFile?: (file: File) => void,
+    aspect?: number,
+    minWidth?: number
+    minHeight?: number
 }
 
-export default function ImageCropper({ file, setFile }: Props) {
+export default function ImageCropper({ file, setFile, aspect = 1, minWidth = 100, minHeight = 100 }: Props) {
+
+    const [crop, setCrop] = useState<Crop>();
+    const imageSrc = URL.createObjectURL(file);
+
+    const handleImageLoad = (e: SyntheticEvent<HTMLImageElement, Event>) => {
+        const width = e.currentTarget.width;
+        const height = e.currentTarget.height;
+
+        const defaultCrop = makeAspectCrop({
+            x: 0,
+            y:0,
+            unit: 'px',
+            width: minWidth,
+            height: minHeight
+        }, aspect, width, height);
+
+        setCrop(defaultCrop);
+    }
+
+    useEffect(() => {
+        return () => {
+            URL.revokeObjectURL(imageSrc)
+        }
+    }, [imageSrc]);
 
     return (
         <div>
-
+            <ReactCrop
+                crop={crop}
+                onChange={(crop) => setCrop(crop)}
+                aspect={aspect}
+                minWidth={minWidth}
+                minHeight={minHeight}
+            >
+                <img src={imageSrc} onLoad={handleImageLoad} />
+            </ReactCrop>
         </div>
     )
 }
