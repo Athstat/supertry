6bcd701ecd976de07acccfa838c9476985b6f23b
Tadiwanashe Shangwa <tshxng@gmail.com>
2026-02-04 09:46:57 +0200

    fixes issues on player score indicator

--- src/components/my_fantasy_team/pitch_card/PitchCardScoreIndicator.tsx
+++ src/components/my_fantasy_team/pitch_card/PitchCardScoreIndicator.tsx
@@ -20,7 +20,7 @@ export function PitchCardScoreIndicator({ player }: PlayerPointsScoreProps) {
     const {leagueRound} = useFantasyTeam();
 
     const isLocked = leagueRound && isSeasonRoundLocked(leagueRound);
-    const { isLoading: loadingScore, score } = useAthleteRoundScore(player.tracking_id, leagueRound?.season || '', leagueRound?.round_number ?? 0);
+    const { isLoading: loadingScore, score } = useAthleteRoundScore(player.tracking_id, leagueRound?.season || '', leagueRound?.round_number ?? 0, !isLocked);
     const { league } = useFantasyLeagueGroup();
 
     const isLoading = loadingScore;
@@ -34,7 +34,7 @@ export function PitchCardScoreIndicator({ player }: PlayerPointsScoreProps) {
     );
 
     const showScore = Boolean(!isLoading && isLocked);
-    const showNextMatchInfo = !showAvailabilityWarning && Boolean(homeOrAway) && Boolean(opponent) && !showScore;
+    const showNextMatchInfo = !isLoading && (!showAvailabilityWarning && Boolean(homeOrAway) && Boolean(opponent) && !showScore);
     return (
         <>
             <div className={twMerge(
@@ -44,26 +44,25 @@ export function PitchCardScoreIndicator({ player }: PlayerPointsScoreProps) {
                 
                 <Activity mode={isLoading ? "visible" : "hidden"} >
                     <div className="w-[60%] h-[10px] bg-white/40 animate-pulse" >
-
                     </div>
                 </Activity>
 
-                <Activity mode={showNextMatchInfo ? "visible" : "hidden"} >
+                {!isLoading && <Activity mode={showNextMatchInfo ? "visible" : "hidden"} >
                     <p className=" text-[8px] md:text-[10px] max-w-[100px] font-medium truncate" >vs {opponent?.athstat_name} {homeOrAway}</p>
-                </Activity>
+                </Activity>}
 
-                <Activity mode={(showAvailabilityWarning && !showScore) ? "visible" : "hidden"} >
+                {!isLoading && <Activity mode={(showAvailabilityWarning && !showScore) ? "visible" : "hidden"} >
                     <div className="w-full flex flex-row gap-1 text-center items-center justify-center" >
                         <p className="text-[8px] md:text-[10px] font-medium" >{reportTitle}</p>
                         <TriangleAlert className="w-3 h-3" />
                     </div>
                 </Activity>
-
-                <Activity mode={showScore ? 'visible' : 'hidden'}  >
+}
+                {!isLoading && <Activity mode={showScore ? 'visible' : 'hidden'}  >
                     <div>
                         <p className='text-[10px] md:text-[10px] font-bold' >{sanitizeStat(score)}</p>
                     </div>
-                </Activity>
+                </Activity>}
 
             </div>
         </>
--- src/hooks/fantasy/useAthleteRoundScore.ts
+++ src/hooks/fantasy/useAthleteRoundScore.ts
@@ -3,10 +3,10 @@ import { fantasyAthleteService } from "../../services/fantasy/fantasyAthleteServ
 import { useMemo } from "react";
 
 /** Hook that returns a players score for a specific season round  */
-export function useAthleteRoundScore(athleteId: string, seasonId: string, round: number | string) {
-    const shouldFetch = Boolean(athleteId) && Boolean(seasonId) && Boolean(round);
+export function useAthleteRoundScore(athleteId: string, seasonId: string, round: number | string, shouldFetch: boolean = true) {
+    const shouldFetchFinal = Boolean(athleteId) && Boolean(seasonId) && Boolean(round) && shouldFetch;
 
-    const key = shouldFetch ? `/fantasy-athletes/${athleteId}/season/${seasonId}/round/${round}` : null;
+    const key = shouldFetchFinal ? `/fantasy-athletes/${athleteId}/season/${seasonId}/round/${round}` : null;
     const {data, isLoading, error} = useSWR(key, () => fantasyAthleteService.getRoundScore(athleteId, seasonId, round), {
         refreshInterval: 1000 * 30 // 30 seconds
     });
