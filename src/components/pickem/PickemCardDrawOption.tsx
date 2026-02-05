192c424fa8404528dfa1f652bc478651ab748d4e
Tadiwanashe Shangwa <tshxng@gmail.com>
2026-02-05 11:54:07 +0200

    only fetches votes when in view

--- src/components/pickem/PickemCardDrawOption.tsx
+++ src/components/pickem/PickemCardDrawOption.tsx
@@ -4,6 +4,7 @@ import { VoteIndicator } from "./VoteIndicator";
 import { useGameVotes } from "../../hooks/useGameVotes";
 import { IFixture, VoteForOption } from "../../types/games";
 import { fixtureSummary } from "../../utils/fixtureUtils";
+import { useInView } from "react-intersection-observer";
 
 type Props = {
     fixture: IFixture,
@@ -16,9 +17,11 @@ type Props = {
 
 export default function PickemCardDrawOption({ fixture, fetchGameVotes, onVote, disabled, clickedButton, isVoting }: Props) {
 
+    const { inView, ref } = useInView({ triggerOnce: true });
+
     const { userVote, percentages } = useGameVotes(
         fixture,
-        fetchGameVotes
+        fetchGameVotes && inView,
     );
 
     const { gameKickedOff } = fixtureSummary(fixture);
@@ -36,27 +39,31 @@ export default function PickemCardDrawOption({ fixture, fetchGameVotes, onVote, 
     const showVote = userVote !== undefined && votePerc !== undefined;
 
     return (
-        <button
-            onClick={handleVote}
-            disabled={disabled}
-            className={twMerge(
-                'flex flex-col relative items-center justify-between w-full gap-2 px-4 py-3 rounded-md h-full transition-all duration-200 justify-self-center',
-                'bg-[#F9FBFD] dark:bg-slate-800 shadow-[0px_0px_3px_rgba(0,0,0,0.25)]',
-                !isLocked && 'hover:bg-slate-100 dark:hover:bg-slate-700/50 active:scale-95 cursor-pointer',
-                votedDraw && !isLocked && 'bg-slate-200 dark:bg-slate-600 ring-2 ring-slate-500 shadow-lg',
-                disabled && 'cursor-not-allowed opacity-60',
-            )}
-        >
-
-            <VoteIndicator isHighlighted={votedDraw} />
+        <>
+            <div ref={ref} ></div>
             
-            <div className="relative">
-                {clickedButton === 'draw' && isVoting ? (
-                    <Loader className="w-5 h-5 animate-spin text-slate-600 dark:text-slate-300" />
-                ) : (
-                    <span className="text-xs font-bold text-slate-600 dark:text-slate-300">DRAW  {showVote ? `(${votePerc}%)` : null}</span>
+            <button
+                onClick={handleVote}
+                disabled={disabled}
+                className={twMerge(
+                    'flex flex-col relative items-center justify-between w-full gap-2 px-4 py-3 rounded-md h-full transition-all duration-200 justify-self-center',
+                    'bg-[#F9FBFD] dark:bg-slate-800 shadow-[0px_0px_3px_rgba(0,0,0,0.25)]',
+                    !isLocked && 'hover:bg-slate-100 dark:hover:bg-slate-700/50 active:scale-95 cursor-pointer',
+                    votedDraw && !isLocked && 'bg-slate-200 dark:bg-slate-600 ring-2 ring-slate-500 shadow-lg',
+                    disabled && 'cursor-not-allowed opacity-60',
                 )}
-            </div>
-        </button>
+            >
+
+                <VoteIndicator isHighlighted={votedDraw} />
+
+                <div className="relative">
+                    {clickedButton === 'draw' && isVoting ? (
+                        <Loader className="w-5 h-5 animate-spin text-slate-600 dark:text-slate-300" />
+                    ) : (
+                        <span className="text-xs font-bold text-slate-600 dark:text-slate-300">DRAW  {showVote ? `(${votePerc}%)` : null}</span>
+                    )}
+                </div>
+            </button>
+        </>
     )
 }
