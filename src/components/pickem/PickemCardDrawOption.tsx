444bcaae0103aa0949e5f3389ac94350c3bd8ea6
Tadiwanashe Shangwa <tshxng@gmail.com>
2026-02-05 11:55:20 +0200

    refactors code

--- src/components/pickem/PickemCardDrawOption.tsx
+++ src/components/pickem/PickemCardDrawOption.tsx
@@ -39,31 +39,30 @@ export default function PickemCardDrawOption({ fixture, fetchGameVotes, onVote, 
     const showVote = userVote !== undefined && votePerc !== undefined;
 
     return (
-        <>
-            <div ref={ref} ></div>
-            
-            <button
-                onClick={handleVote}
-                disabled={disabled}
-                className={twMerge(
-                    'flex flex-col relative items-center justify-between w-full gap-2 px-4 py-3 rounded-md h-full transition-all duration-200 justify-self-center',
-                    'bg-[#F9FBFD] dark:bg-slate-800 shadow-[0px_0px_3px_rgba(0,0,0,0.25)]',
-                    !isLocked && 'hover:bg-slate-100 dark:hover:bg-slate-700/50 active:scale-95 cursor-pointer',
-                    votedDraw && !isLocked && 'bg-slate-200 dark:bg-slate-600 ring-2 ring-slate-500 shadow-lg',
-                    disabled && 'cursor-not-allowed opacity-60',
+
+        <button
+            onClick={handleVote}
+            disabled={disabled}
+            className={twMerge(
+                'flex flex-col relative items-center justify-between w-full gap-2 px-4 py-3 rounded-md h-full transition-all duration-200 justify-self-center',
+                'bg-[#F9FBFD] dark:bg-slate-800 shadow-[0px_0px_3px_rgba(0,0,0,0.25)]',
+                !isLocked && 'hover:bg-slate-100 dark:hover:bg-slate-700/50 active:scale-95 cursor-pointer',
+                votedDraw && !isLocked && 'bg-slate-200 dark:bg-slate-600 ring-2 ring-slate-500 shadow-lg',
+                disabled && 'cursor-not-allowed opacity-60',
+            )}
+
+            ref={ref}
+        >
+
+            <VoteIndicator isHighlighted={votedDraw} />
+
+            <div className="relative">
+                {clickedButton === 'draw' && isVoting ? (
+                    <Loader className="w-5 h-5 animate-spin text-slate-600 dark:text-slate-300" />
+                ) : (
+                    <span className="text-xs font-bold text-slate-600 dark:text-slate-300">DRAW  {showVote ? `(${votePerc}%)` : null}</span>
                 )}
-            >
-
-                <VoteIndicator isHighlighted={votedDraw} />
-
-                <div className="relative">
-                    {clickedButton === 'draw' && isVoting ? (
-                        <Loader className="w-5 h-5 animate-spin text-slate-600 dark:text-slate-300" />
-                    ) : (
-                        <span className="text-xs font-bold text-slate-600 dark:text-slate-300">DRAW  {showVote ? `(${votePerc}%)` : null}</span>
-                    )}
-                </div>
-            </button>
-        </>
+            </div>
+        </button>
     )
 }
