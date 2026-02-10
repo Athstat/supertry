0f2471e3c4a4a5a1a3ea7150080d8592a1d83fb3
Tadiwanashe Shangwa <tshxng@gmail.com>
2026-02-10 17:53:04 +0200

    makes buttons bigger on player action modal

--- src/components/my_fantasy_team/PlayerActionModal.tsx
+++ src/components/my_fantasy_team/PlayerActionModal.tsx
@@ -206,14 +206,14 @@ export function PlayerActionModal({
           isSlotLocked && "opacity-60"
         )} >
           <PrimaryButton
-            className="bg-purple-100 text-purple-700 dark:text-purple-300 dark:bg-purple-900/30 dark:hover:bg-purple-900 dark:border-purple-500/50 border-purple-500 hover:bg-purple-200"
+            className="bg-purple-100 text-purple-700 py-4 dark:text-purple-300 dark:bg-purple-900/30 dark:hover:bg-purple-900 dark:border-purple-500/50 border-purple-500 hover:bg-purple-200"
             onClick={handleInitSwap}
           >
             Swap
             {isSlotLocked && <Lock className="w-4 h-4" />}
           </PrimaryButton>
           <PrimaryButton
-            className="bg-red-100 text-red-700 dark:text-red-300 dark:bg-red-900/30 dark:hover:bg-red-900 dark:border-red-500/40 border-red-500 hover:bg-red-200"
+            className="bg-red-100 text-red-700 py-4 dark:text-red-300 dark:bg-red-900/30 dark:hover:bg-red-900 dark:border-red-500/40 border-red-500 hover:bg-red-200"
             onClick={handleRemovePlayer}
           >
             Remove
@@ -232,7 +232,7 @@ export function PlayerActionModal({
           {!isSub && !isTeamCaptainLocked && (
             <RoundedCard
               className={
-                "border-none hover:dark:text-slate-300 cursor-pointer  bg-slate-200 dark:bg-slate-700 dark:text-slate-200 p-2.5 items-center justify-center flex flex-row gap-1"
+                "border-none hover:dark:text-slate-300 py-4 cursor-pointer  bg-slate-200 dark:bg-slate-700 dark:text-slate-200 p-2.5 items-center justify-center flex flex-row gap-1"
               }
               onClick={handleMakePlayerCaptain}
             >
@@ -245,7 +245,7 @@ export function PlayerActionModal({
           {isSub && subOutCandidate?.athlete && (
             <RoundedCard
               className={
-                "border-none hover:dark:text-slate-300 cursor-pointer  bg-slate-200 dark:bg-slate-700 dark:text-slate-200 p-2.5 items-center justify-center flex flex-row gap-1"
+                "border-none hover:dark:text-slate-300 cursor-pointer  bg-slate-200 dark:bg-slate-700 dark:text-slate-200 p-2.5 py-4 items-center justify-center flex flex-row gap-1"
               }
               onClick={handleSubIn}
             >
