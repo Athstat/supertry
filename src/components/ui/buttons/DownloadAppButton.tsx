1c0f14a0daba5bd916889dd06bd166430ad98ae6
Tadiwanashe Shangwa <tshxng@gmail.com>
2026-02-10 13:21:13 +0200

    removes codex review

--- src/components/ui/buttons/DownloadAppButton.tsx
+++ src/components/ui/buttons/DownloadAppButton.tsx
@@ -1,9 +1,9 @@
 import { Download } from "lucide-react"
-import { Link } from "react-router-dom"
 import PrimaryButton from "./PrimaryButton"
 import { useStoreLinks } from "../../../hooks/marketing/useStoreLinks"
 import { leagueInviteService } from "../../../services/fantasy/leagueInviteService"
 import { LeagueGroupInvite } from "../../../types/fantasyLeague"
+import SecondaryText from "../typography/SecondaryText"
 
 type Props = {
     showIcon?: boolean,
@@ -16,17 +16,27 @@ export default function DownloadAppButton({ showIcon = false, invite }: Props) {
     const { oneLinkUrl } = useStoreLinks(invite?.league);
 
     const handleOnClick = () => {
+
         if (invite) {
             leagueInviteService.registerIntent(invite?.id);
         }
+
+        if (oneLinkUrl) {
+            window.location.assign(oneLinkUrl, );
+        }
+    }
+
+    if (!oneLinkUrl) {
+        return <SecondaryText>One Link was not found</SecondaryText>
     }
 
     return (
-        <Link to={oneLinkUrl || ''} target="blank"  onClick={handleOnClick}>
-            <PrimaryButton className="flex w-fit flex-row items-center gap-4" >
-                <p className="text-nowrap" >Download App</p>
-                {showIcon && <Download />}
-            </PrimaryButton>
-        </Link>
+        <PrimaryButton 
+            className="flex w-fit flex-row items-center gap-4" 
+            onClick={handleOnClick}
+        >
+            <p className="text-nowrap" >Download App</p>
+            {showIcon && <Download />}
+        </PrimaryButton>
     )
 }
