e4ac32a90347526d3c97880c3401275ba75244fa
Tadiwanashe Shangwa <tshxng@gmail.com>
2026-02-12 10:23:59 +0200

    makes competiton switcher easier to see

--- src/components/ui/buttons/PrimaryButton.tsx
+++ src/components/ui/buttons/PrimaryButton.tsx
@@ -25,7 +25,7 @@ export default function PrimaryButton({ children, className, onClick, disbabled,
         <button
             disabled={disbabled || disabled}
             className={twMerge(
-                "bg-primary-600 dark:bg-primary-600 text-white font-medium px-4 py-2 w-full items-center justify-center flex rounded-xl",
+                "bg-primary-600 dark:bg-primary-600 text-white font-medium px-4 py-2 w-full items-center justify-center flex rounded-[5px]",
                 "hover:bg-primary-700 dark:hover:bg-primary-700",
                 "border border-primary-500 text-sm lg:text-base",
                 destroy && "bg-red-600 dark:bg-red-600 hover:bg-red-700 dark:hover:bg-red-700 border-red-500",
