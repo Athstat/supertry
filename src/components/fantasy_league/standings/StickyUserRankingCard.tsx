9881f78b65c8c8de3fe8e2a0cfa5aece52d67419
Tadiwanashe Shangwa <tshxng@gmail.com>
2026-02-13 11:36:27 +0200

    fixes desktop responsiveness

--- src/components/fantasy_league/standings/StickyUserRankingCard.tsx
+++ src/components/fantasy_league/standings/StickyUserRankingCard.tsx
@@ -14,9 +14,9 @@ export default function StickyUserRankingCard({ userRanking, onClick }: Props) {
     const pointsDisplay = smartRoundUp(userRanking?.league_points ?? userRanking?.total_score) || '-';
 
     return (
-        <div className="fixed p-4 bottom-1 left-0 z-[10] w-full" >
+        <div className="fixed p-4 bottom-1 left-0 z-[10] w-full flex flex-col items-center justify-center" >
             <div onClick={onClick} className={twMerge(
-                "w-full rounded-md bg-blue-500 dark:shadow-lg text-white shadow-black dark:bg-blue-600 cursor-pointer p-4 flex flex-col gap-2",
+                "w-full lg:w-1/3 rounded-md bg-blue-500 dark:shadow-lg text-white shadow-black dark:bg-blue-600 cursor-pointer p-4 flex flex-col gap-2",
             )} >
 
 
