a34e7dd3d0a685031fb51676dd4217bdaaeb5ca5
Tadiwanashe Shangwa <tshxng@gmail.com>
2026-02-25 21:26:33 +0200

    adds code to avoid over fetching

--- src/hooks/fantasy/usePlayerRoundAvailability.tsx
+++ src/hooks/fantasy/usePlayerRoundAvailability.tsx
@@ -18,7 +18,7 @@ export function usePlayerRoundAvailability(athleteId: string, seasonId: string, 
     finalSeasonId,
     roundNumber,
     team_id
-  ), {revalidateIfStale: true});
+  ), {revalidateIfStale: true, revalidateOnFocus: false});
 
   const firstReport = data;
 
